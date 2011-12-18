93570a74020b9fe1683349a57d93d6cac4dad6da
Arjen van Bochoven <arjen@bochoven.net>
2011-12-19 00:16:37 +0100

    ADDED custom page not found handler to kissmvc.php

--- system/kissmvc.php
+++ system/kissmvc.php
@@ -6,6 +6,12 @@ require('kissmvc_core.php');
 //===============================================================
 class Engine extends KISS_Engine
 {
+	function request_not_found( $msg='' ) 
+	{
+		header( "HTTP/1.0 404 Not Found" );
+				
+		die( '<html><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL was not found on this server.</p><p>Please go <a href="javascript: history.back( 1 )">back</a> and try again.</p><hr /><p>Powered By: <a href="http://kissmvc.com">KISSMVC</a></p></body></html>' );
+	}
 	
 }
 
@@ -22,7 +28,7 @@ class Controller extends KISS_Controller
 //===============================================================
 class Model extends KISS_Model
 {
-	
+
 }
 
 //===============================================================
