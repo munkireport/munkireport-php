e2510df50a65375536e78c77e768cfac45458044
Arjen van Bochoven <arjen@bochoven.net>
2013-06-12 23:06:25 +0200

    Added language files

---
+++ app/lang/en/lang.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * english language file
+ *
+ * @author     Arjen van Bochoven <arjen@bochoven.net>
+ */
+$lang['year']		= 'year';
+$lang['month']		= 'month';
+$lang['week'] 		= 'week';
+$lang['day']		= 'day';
+$lang['hour']		= 'hour';
+$lang['minute']		= 'minute';
+$lang['second']		= 'second';
+// Plurals	
+$lang['years']		= 'years';
+$lang['months']		= 'months';
+$lang['weeks']		= 'weeks';
+$lang['days']		= 'days';
+$lang['hours']		= 'hours';
+$lang['minutes']	= 'minutes';
+$lang['seconds']	= 'seconds';
---
+++ app/lang/nl/lang.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * dutch language file
+ *
+ * @author Arjen van Bochoven <arjen@bochoven.net>
+ */
+$lang['year']		= 'jaar';
+$lang['month']		= 'maand';
+$lang['week'] 		= 'week';
+$lang['day']		= 'dag';
+$lang['hour']		= 'uur';
+$lang['minute']		= 'minuut';
+$lang['second']		= 'seconde';
+// Plurals	
+$lang['years']		= 'jaren';
+$lang['months']		= 'maanden';
+$lang['weeks']		= 'weken';
+$lang['days']		= 'dagen';
+$lang['hours']		= 'uren';
+$lang['minutes']	= 'minuten';
+$lang['seconds']	= 'seconden';
+
