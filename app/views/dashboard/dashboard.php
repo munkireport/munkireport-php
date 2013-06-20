92b703e1c8d073dafc4f1deb9da98fe0a7a39512
Joe Wollard <joe.wollard@gmail.com>
2013-06-19 22:49:36 -0400

    Cleaning up the dashboard layout a bit.
    Added the table-condensed class to each table, removed the 2nd column from each one and added the pull-right class to each badge so things are consistently aligned, and switched from h2 tags to h3 to help reduce the feeling of clutter on the page.

--- app/views/dashboard/dashboard.php
+++ app/views/dashboard/dashboard.php
@@ -4,19 +4,21 @@
 
 	<div class="row">
 		<div class="span4">
-			<h2>Errors</h2>
+			<h3>Errors</h3>
 
 		</div>
 		<div class="span4">
-			<h2>Hardware breakdown</h2>
+			<h3>Hardware breakdown</h3>
 			<?$machine = new Machine();
 				$sql = "select count(id) as count, machine_desc from machine group by machine_desc";
 				?>
-			<table class="table table-striped">
+			<table class="table table-striped table-condensed">
 				<?foreach($machine->query($sql) as $obj):?>
 				<tr>
-					<td><?=$obj->machine_desc?></td>
-					<td><span class="badge"><?=$obj->count?></span></td>
+					<td>
+						<?=$obj->machine_desc?>
+						<span class="badge pull-right"><?=$obj->count?></span>
+					</td>
 				</tr>
 				<?endforeach?>
 			</table>
@@ -24,7 +26,7 @@
 			
 		</div>
 		<div class="span4">
-			<h2>Network breakdown</h2>
+			<h3>Network breakdown</h3>
 			
 			<p>Differentiate between onsite and offsite</p>
 			
@@ -33,25 +35,27 @@
 	</div> <!-- /row -->
 	<div class="row">
 		<div class="span4">
-			<h2>OS breakdown</h2>
+			<h3>OS breakdown</h3>
 			<? $sql = "select count(id) as count, os_version from machine group by os_version ORDER BY os_version";
 				?>
-			<table class="table table-striped">
+			<table class="table table-striped table-condensed">
 				<?foreach($machine->query($sql) as $obj):?>
 				<tr>
-					<td><?=$obj->os_version?></td>
-					<td><span class="badge"><?=$obj->count?></span></td>
+					<td>
+						<?=$obj->os_version?>
+						<span class="badge pull-right"><?=$obj->count?></span>
+					</td>
 				</tr>
 				<?endforeach?>
 			</table>
 		</div>
 		<div class="span4">
-			<h2>Filevault status</h2>
+			<h3>Filevault status</h3>
 
 			
 		</div>
 		<div class="span4">
-			<h2>Battery status</h2>
+			<h3>Battery status</h3>
 			
 			<p>Replace, ok, etc.</p>
 			
@@ -61,26 +65,30 @@
 
 		<div class="row">
 		<div class="span4">
-			<h2>Warranty status</h2>
+			<h3>Warranty status</h3>
 			<?$warranty = new Warranty();
 				$sql = "select count(id) as count, status from warranty group by status ORDER BY status";
 				?>
-			<table class="table table-striped">
+			<table class="table table-striped table-condensed">
 				<?foreach($warranty->query($sql) as $obj):?>
 				<tr>
-					<td><?=$obj->status?></td>
-					<td><span class="badge"><?=$obj->count?></span></td>
+					<td>
+						<?=$obj->status?>
+						<span class="badge pull-right"><?=$obj->count?></span>
+					</td>
 				</tr>
 				<?endforeach?>
 			</table>
 			<?	$thirtydays = date('Y-m-d', strtotime('+30days'));
 				$sql = "select count(id) as count, status from warranty WHERE end_date < '$thirtydays' AND status != 'Expired' AND end_date != '' group by status ORDER BY status";
 			?>
-			<table class="table table-striped">
+			<table class="table table-striped table-condensed">
 				<?foreach($warranty->query($sql) as $obj):?>
 				<tr class="warning">
-					<td>Expires in 30 days</td>
-					<td><span class="badge"><?=$obj->count?></span></td>
+					<td>
+						Expires in 30 days
+						<span class="badge pull-right"><?=$obj->count?></span>
+					</td>
 				</tr>
 				<?endforeach?>
 			</table>
@@ -88,29 +96,35 @@
 
 		</div>
 		<div class="span4">
-			<h2>Manifest names</h2>
+			<h3>Manifest names</h3>
 
 			
 		</div>
 		<div class="span4">
-			<h2>Munki</h2>
+			<h3>Munki</h3>
 			<?$munkireport = new Munkireport();
 				$sql = "select sum(errors > 0) as errors, sum(warnings>0) as warnings, sum(activity != '') as activity from munkireport;";
 				?>
-			<table class="table table-striped">
+			<table class="table table-striped table-condensed">
 				<?foreach($munkireport->query($sql) as $obj):?>
 				<tr class="error">
-					<td>Clients with errors</td>
-					<td><span class="badge badge-important"><?=$obj->errors?></span></td>
+					<td>
+						Clients with errors
+						<span class="badge badge-important pull-right"><?=$obj->errors?></span>
+					</td>
 				</tr>
 
 				<tr class="warning">
-					<td>Clients with warnings</td>
-					<td><span class="badge badge-warning"><?=$obj->warnings?></span></td>
+					<td>
+						Clients with warnings
+						<span class="badge badge-warning pull-right"><?=$obj->warnings?></span>
+					</td>
 				</tr>
 				<tr class="info">
-					<td>Clients with activiy</td>
-					<td><span class="badge badge-info"><?=$obj->activity?></span></td>
+					<td>
+						Clients with activiy
+						<span class="badge badge-info pull-right"><?=$obj->activity?></span>
+					</td>
 				</tr>
 				<?endforeach?>
 			</table>
@@ -121,26 +135,28 @@
 	</div> <!-- /row -->
 	<div class="row">
 		<div class="span4">
-			<h2>Disk status</h2>
+			<h3>Disk status</h3>
 			<?$diskreport = new DiskReport();
 				$sql = "select count(id) as count from diskreport where Percentage > 85";
 				?>
-			<table class="table">
+			<table class="table table-condensed">
 				<?foreach($machine->query($sql) as $obj):?>
 				<tr>
-					<td>Over 85%</td>
-					<td><span class="badge badge-important"><?=$obj->count?></span></td>
+					<td>
+						Over 85%
+						<span class="badge badge-important pull-right"><?=$obj->count?></span>
+					</td>
 				</tr>
 				<?endforeach?>
 			</table>
 			<a href="<?=url('clients/show/diskstatus')?>">Show all clients</a>
 		</div>
 		<div class="span4">
-			<h2>..</h2>
+			<h3>..</h3>
 
 		</div>
 		<div class="span4">
-			<h2>..</h2>
+			<h3>..</h3>
 						
 		</div>
 		
