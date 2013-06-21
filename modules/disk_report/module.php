f9005e6b756b532f9487f4fba245cfe465d4b440
Arjen van Bochoven <arjen@bochoven.net>
2013-06-21 19:05:39 +0200

    Added initial module support

---
+++ modules/disk_report/module.php
@@ -0,0 +1,21 @@
+<?php 
+
+/**
+ * Disk report module class
+ *
+ * @package munkireport
+ * @author 
+ **/
+class disk_report_module
+{
+	/**
+	 * Get available widgets
+	 *
+	 * @return array widget array
+	 * @author arjen@bochoven.net
+	 **/
+	function get_widgets()
+	{
+		return $this->widgets;
+	}
+} // END class disk_report_module
