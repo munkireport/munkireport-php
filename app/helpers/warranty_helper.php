36a4ae10eaf32856feaa4a6cc36cdcc289011bd8
Arjen van Bochoven <arjen@bochoven.net>
2013-08-09 08:51:31 +0200

    Added warranty helper

---
+++ app/helpers/warranty_helper.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Estimates the week the machine was manfactured based off it's serial
+ * number
+ * Ported from python warranty scripts by Adam Reed <adam.reed@anu.edu.au>
+ *
+ * @return string date
+ * @author Arjen van Bochoven
+ **/
+function estimate_manufactured_date($serial)
+{
+	# See http://www.macrumors.com/2010/04/16/apple-tweaks-serial-number
+    #      -format-with-new-macbook-pro/ for details about serial numbers
+
+	if(strlen($serial) == 11)
+	{
+		$year = $serial[2];
+		$est_year = 2000 + strpos('   3456789012', $year);
+		$week = substr($serial, 3, 2);
+		return  *($est_year, $week);
+	}
+	elseif(strlen($serial) == 12)
+	{
+		$year_code = 'cdfghjklmnpqrstvwxyz';
+		$year = strtolower($serial[3]);
+		$est_year = 2010 + intval(strpos($year_code, $year) / 2);
+		$est_half = strpos($year_code, $year) % 2;
+		$week_code = ' 123456789cdfghjklmnpqrtvwxy';
+		$week = strtolower(substr($serial, 4, 1));
+		$est_week = strpos($week_code, $week) + ($est_half * 26);
+		return formatted_manufactured_date($est_year, $est_week);
+	}
+}
+
+function formatted_manufactured_date($year, $week)
+{
+	$strtime = sprintf('%sW%02s1', $year, $week);
+	return date('Y-m-d', strtotime($strtime));
+}
