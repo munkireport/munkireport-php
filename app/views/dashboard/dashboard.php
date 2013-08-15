545f658ef87e03778cb556429f6d00ff250df8be
Arjen van Bochoven <arjen@bochoven.net>
2013-08-15 17:49:56 +0200

    Cleanup dashboard

--- app/views/dashboard/dashboard.php
+++ app/views/dashboard/dashboard.php
@@ -4,11 +4,11 @@
 
 	<div class="row">
 		<div class="col-lg-4">
-			<h3>Errors</h3>
+			<h2>Errors</h2>
 
 		</div>
 		<div class="col-lg-4">
-			<h3>Hardware breakdown</h3>
+			<h2>Hardware breakdown</h2>
 			<?$machine = new Machine();
 				$sql = "select count(id) as count, machine_desc from machine group by machine_desc";
 				?>
@@ -17,7 +17,7 @@
 				<tr>
 					<td>
 						<?=$obj->machine_desc?>
-						<span class="label pull-right"><?=$obj->count?></span>
+						<span class="badge pull-right"><?=$obj->count?></span>
 					</td>
 				</tr>
 				<?endforeach?>
@@ -26,7 +26,7 @@
 			
 		</div>
 		<div class="col-lg-4">
-			<h3>Network breakdown</h3>
+			<h2>Network breakdown</h2>
 			
 			<p>Differentiate between onsite and offsite</p>
 			
@@ -35,7 +35,7 @@
 	</div> <!-- /row -->
 	<div class="row">
 		<div class="col-lg-4">
-			<h3>OS breakdown</h3>
+			<h2>OS breakdown</h2>
 			<? $sql = "select count(id) as count, os_version from machine group by os_version ORDER BY os_version";
 				?>
 			<table class="table table-striped table-condensed">
@@ -43,19 +43,19 @@
 				<tr>
 					<td>
 						<?=$obj->os_version?>
-						<span class="label pull-right"><?=$obj->count?></span>
+						<span class="badge pull-right"><?=$obj->count?></span>
 					</td>
 				</tr>
 				<?endforeach?>
 			</table>
 		</div>
 		<div class="col-lg-4">
-			<h3>Filevault status</h3>
+			<h2>Filevault status</h2>
 
 			
 		</div>
 		<div class="col-lg-4">
-			<h3>Battery status</h3>
+			<h2>Battery status</h2>
 			
 			<p>Replace, ok, etc.</p>
 			
@@ -65,7 +65,7 @@
 
 		<div class="row">
 		<div class="col-lg-4">
-			<h3>Warranty status</h3>
+			<h2>Warranty status</h2>
 			<?$warranty = new Warranty();
 				$sql = "select count(id) as count, status from warranty group by status ORDER BY status";
 				?>
@@ -96,12 +96,12 @@
 
 		</div>
 		<div class="col-lg-4">
-			<h3>Manifest names</h3>
+			<h2>Manifest names</h2>
 
 			
 		</div>
 		<div class="col-lg-4">
-			<h3>Munki</h3>
+			<h2>Munki</h2>
 			<?$munkireport = new Munkireport();
 				$sql = "select sum(errors > 0) as errors, sum(warnings>0) as warnings, sum(activity != '') as activity from munkireport;";
 				?>
@@ -135,7 +135,7 @@
 	</div> <!-- /row -->
 	<div class="row">
 		<div class="col-lg-4">
-			<h3>Disk status</h3>
+			<h2>Disk status</h2>
 			<?$diskreport = new Disk_report_model();
 				$sql = "select count(id) as count from diskreport where Percentage > 85";
 				?>
@@ -152,11 +152,11 @@
 			<a href="<?=url('clients/show/diskstatus')?>">Show all clients</a>
 		</div>
 		<div class="col-lg-4">
-			<h3>..</h3>
+			<h2>..</h2>
 
 		</div>
 		<div class="col-lg-4">
-			<h3>..</h3>
+			<h2>..</h2>
 						
 		</div>
 		
