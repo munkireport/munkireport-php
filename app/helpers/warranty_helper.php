ba3fb0a52a102496b182c7f00153241dc91148c0
Arjen van Bochoven <arjen@bochoven.net>
2013-08-29 23:43:17 +0200

    Added purchase_date to warranty model, moved checking code to warranty_helper, keep model lightweight.

--- app/helpers/warranty_helper.php
+++ app/helpers/warranty_helper.php
@@ -1,5 +1,113 @@
 <?php
 
+function check_warranty_status(&$warranty_model)
+{
+	// Update needed, check with apple
+	
+	// Unfortunately we have to scrape the page as Apple discontinued the json api
+	$url = 'https://selfsolve.apple.com/wcResults.do';
+	$data = array ('sn' => $warranty_model->serial_number, 'num' => '0');
+	$data = http_build_query($data);
+
+	$context_options = array (
+	        'http' => array (
+	            'method' => 'POST',
+	            'header'=> "Content-type: application/x-www-form-urlencoded\r\n"
+	                . "Content-Length: " . strlen($data) . "\r\n",
+	            'content' => $data
+	            )
+	        );
+
+	$context = stream_context_create($context_options);
+	$result = file_get_contents($url, FALSE, $context);
+	
+	if(preg_match('/invalidserialnumber/', $result))
+	{
+		// Check invalid serial
+		$warranty_model->status = 'Invalid serial number';
+	}
+	elseif(preg_match("/RegisterProduct.do\?productRegister=Y/", $result))
+	{
+		// Check registration
+		$warranty_model->status = 'Unregistered serialnumber';
+	}
+	elseif(preg_match('/warrantyPage.warrantycheck.displayHWSupportInfo\(false/', $result))
+	{
+		// Get expired status
+		$warranty_model->status = 'Expired';
+		//$warranty_model->end_date = '0000-00-00';
+	}
+	elseif(preg_match('/warrantyPage.warrantycheck.displayHWSupportInfo\(true([^\)])+/', $result, $matches))
+	{
+		// Get support status
+
+		if(preg_match('/Limited Warranty\./', $matches[0]))
+		{
+			$warranty_model->status = 'No Applecare';
+		}
+		else
+		{
+			$warranty_model->status = 'Supported';
+		}
+					
+		// Get estimated exp date
+		if(preg_match('/Estimated Expiration Date: ([^<]+)</', $matches[0], $matches))
+		{
+			$exp_time = strtotime($matches[1]);
+			$warranty_model->end_date = date('Y-m-d', $exp_time);
+
+			if($warranty_model->status = 'Supported')
+			{
+				$warranty_model->purchase_date = date('Y-m-d', strtotime('-3 years', $exp_time));
+			}
+			else
+			{
+				$warranty_model->purchase_date = date('Y-m-d', strtotime('-1 year', $exp_time));
+			}
+		}	
+	}
+	else
+	{
+		$warranty_model->status = 'No information found';
+	}
+
+	// No purchase date, use the estimated manufacture date
+	if( ! $warranty_model->purchase_date)
+	{
+		// Get est. manufacture date
+		$warranty_model->purchase_date = estimate_manufactured_date($warranty_model->serial_number);
+		
+	}
+
+	// No expiration date, use the estimated manufacture date + n year
+	if( ! $warranty_model->end_date)
+	{
+		$man_time = strtotime($warranty_model->purchase_date);
+
+		// If we're within 3 years, after man_date we did not have AppleCare
+		// So end_date = man_date + 1 year
+		if(strtotime('+3 years', $man_time) > time())
+		{
+			$warranty_model->end_date = date('Y-m-d', strtotime('+1 year', $man_time));
+		}
+		else // end_date = man_date + 3 yrs (assume we had applecare)
+		{
+			$warranty_model->end_date = date('Y-m-d', strtotime('+3 years', $man_time));
+		}
+
+	}
+	
+	// Get info
+	if(preg_match("/warrantyPage.warrantycheck.displayProductInfo\('([^\']+)', '([^\']+)'/", $result, $matches))
+	{
+		// Save img_url
+		$machine = new Machine($warranty_model->serial_number);
+		$machine->img_url = $matches[1];
+		$machine->machine_desc = $matches[2];
+		$machine->save();
+	}
+}
+
 /**
  * Estimates the week the machine was manfactured based off it's serial
  * number
--- app/models/Warranty.php
+++ app/models/Warranty.php
@@ -8,6 +8,7 @@ class Warranty extends Model {
 		parent::__construct('id', strtolower(get_class($this))); //primary key, tablename
 		$this->rs['id'] = '';
 		$this->rs['serial_number'] = $serial; $this->rt['serial_number'] = 'VARCHAR(255) UNIQUE';
+		$this->rs['purchase_date'] = '';
 		$this->rs['end_date'] = '';
 		$this->rs['status'] = '';
 		
@@ -33,75 +34,10 @@ class Warranty extends Model {
 			return $this;
 		}
 		
-		// Update needed, check with apple
-		
-		// Unfortunately we have to scrape the page as Apple discontinued the json api
-		$url = 'https://selfsolve.apple.com/wcResults.do';
-		$data = array ('sn' => $this->serial_number, 'num' => '0');
-		$data = http_build_query($data);
+		// Load warranty helper
+		require_once(conf('application_path').'helpers/warranty_helper.php');
 
-		$context_options = array (
-		        'http' => array (
-		            'method' => 'POST',
-		            'header'=> "Content-type: application/x-www-form-urlencoded\r\n"
-		                . "Content-Length: " . strlen($data) . "\r\n",
-		            'content' => $data
-		            )
-		        );
-
-		$context = stream_context_create($context_options);
-		$result = file_get_contents($url, FALSE, $context);
-		
-		if(preg_match('/invalidserialnumber/', $result))
-		{
-			// Check invalid serial
-			$this->status = 'Invalid serial number';
-		}
-		elseif(preg_match("/RegisterProduct.do\?productRegister=Y/", $result))
-		{
-			// Check registration
-			$this->status = 'Unregistered serialnumber';
-		}
-		elseif(preg_match('/warrantyPage.warrantycheck.displayHWSupportInfo\(false/', $result))
-		{
-			// Get expired status
-			$this->status = 'Expired';
-			//$this->end_date = '0000-00-00';
-		}
-		elseif(preg_match('/warrantyPage.warrantycheck.displayHWSupportInfo\(true([^\)])+/', $result, $matches))
-		{
-			// Get support status
-
-			if(preg_match('/Limited Warranty\./', $matches[0]))
-			{
-				$this->status = 'No Applecare';
-			}
-			else
-			{
-				$this->status = 'Supported';
-			}
-						
-			// Get estimated exp date
-			if(preg_match('/Estimated Expiration Date: ([^<]+)</', $matches[0], $matches))
-			{
-				$this->end_date = date('Y-m-d', strtotime($matches[1]));
-			}	
-		}
-		else
-		{
-			$this->status = 'No information found';
-		}
-		
-		// Get info
-		if(preg_match("/warrantyPage.warrantycheck.displayProductInfo\('([^\']+)', '([^\']+)'/", $result, $matches))
-		{
-			// Save img_url
-			$machine = new Machine($this->serial_number);
-			$machine->img_url = $matches[1];
-			$machine->machine_desc = $matches[2];
-			$machine->save();
-		}
-		
+		check_warranty_status($this);
 		
 		$this->save();
 		
