a4d9d07c1d218b6eae2613e2d49455a10513e25a
Arjen van Bochoven <arjen@bochoven.net>
2013-09-07 11:14:44 +0200

    Fix in the core for nginx + query ajax calls

--- system/kissmvc_core.php
+++ system/kissmvc_core.php
@@ -194,8 +194,19 @@ abstract class KISS_Engine
 		{
 			$uri = substr($uri, 2);
 		}
+
+		// Spit on ?
 		$parts = preg_split('#\?#i', $uri, 2);
 		$uri = $parts[0];
+
+		// Check if we still have an ampersand in the uri
+		// jquery will remove the second ? in an ajax call
+		if(strpos($uri, '&') !== FALSE)
+		{
+			$parts = preg_split('#&#i', $uri, 2);
+			$uri = $parts[0];
+		}
+		
 		if (isset($parts[1]))
 		{
 			$_SERVER['QUERY_STRING'] = $parts[1];
