556f44043156d7ab7f12e8dfcedb1d6c40d4b575
Arjen van Bochoven <arjen@bochoven.net>
2013-09-14 16:37:31 +0200

    colored badges in warranty widget

--- app/views/widgets/warranty_widget.php
+++ app/views/widgets/warranty_widget.php
@@ -12,11 +12,11 @@
 
 				<?	$warranty = new Warranty(); 
 					$thirtydays = date('Y-m-d', strtotime('+30days'));
-					$class_list = array('Expired' => 'danger');
+					$class_list = array('Supported' => 'warning');
 					$sql = "select count(id) as count, status from warranty WHERE end_date < '$thirtydays' AND status != 'Expired' AND end_date != '' group by status ORDER BY status";
 				?>
 					<?foreach($warranty->query($sql) as $obj):?>
-					<?$status = array_key_exists($obj->status, $class_list) ? $class_list[$obj->status] : 'default'?>
+					<?$status = array_key_exists($obj->status, $class_list) ? $class_list[$obj->status] : 'danger'?>
 					<a href="<?=url('show/listing/warranty#'.$obj->status)?>" class="list-group-item list-group-item-<?=$status?>">
 						<span class="badge"><?=$obj->count?></span>
 						Expires in 30 days (<?=$obj->status?>)
