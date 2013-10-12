e4de66b9427de5296f2f4dff4863b4fffac92cae
Arjen van Bochoven <arjen@bochoven.net>
2013-10-12 13:20:40 +0200

    Added filevault status module, added diskreport install script

---
+++ app/modules/filevault_status/filevault_status_model.php
@@ -0,0 +1,36 @@
+<?php
+class Filevault_status_model extends Model {
+
+	function __construct($serial='')
+	{
+		parent::__construct('id', 'filevault_status'); //primary key, tablename
+		$this->rs['id'] = '';
+		$this->rs['serial_number'] = $serial; $this->rt['serial_number'] = 'VARCHAR(255) UNIQUE';
+		$this->rs['filevault_status'] = '';		   
+		
+		// Create table if it does not exist
+		$this->create_table();
+		
+		if ($serial)
+			$this->retrieve_one('serial_number=?', $serial);
+		
+		$this->serial = $serial;
+		echo 'Filevault_status_model initialized';
+	}
+	
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Process data sent by postflight
+	 *
+	 * @param string data
+	 * @author abn290
+	 **/
+	function process($text)
+	{		
+		$this->filevault_status = $text;
+		$this->save();
+	}
+
+	
+}
