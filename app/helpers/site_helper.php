1ad2cdea6746355bbb5bb23539c66b56db5f2d8a
Arjen van Bochoven <arjen@bochoven.net>
2013-10-19 01:44:02 +0200

    Version number update

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.0.0.320';
+$GLOBALS['version'] = '2.0.0.327';
 
 //===============================================
 // Uncaught Exception Handling
