8cff268fa741263c5636c38262a84f80f63de29c
Arjen van Bochoven <arjen@bochoven.net>
2013-10-22 11:45:10 +0200

    Improved error reporting/checking in install scripts

--- app/modules/filevault_status/scripts/install.sh
+++ app/modules/filevault_status/scripts/install.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-echo 'Install filevault status report'
-
 # filevault_status_controller
 FV_CTL="${BASEURL}/index.php?/module/filevault_status/"
 
 # Get the scripts in the proper directories
-curl --fail --silent "${FV_CTL}get_script/filevault_2_status_check.sh" -o "/usr/local/bin/filevault_2_status_check.sh" \
-	&& curl --fail --silent "${FV_CTL}get_script/filevaultstatus" -o "${MUNKIPATH}preflight.d/filevaultstatus" \
+${CURL}  "${FV_CTL}get_script/filevault_2_status_check.sh" -o "/usr/local/bin/filevault_2_status_check.sh" \
+	&& ${CURL}  "${FV_CTL}get_script/filevaultstatus" -o "${MUNKIPATH}preflight.d/filevaultstatus" \
 
-if [ "${?}" != 0 ]
-then
-	echo "Failed to download all required components!"
+# Check exit status of curl
+if [ $? = 0 ]; then
+	# Make scripts executable
+	chmod a+x /usr/local/bin/filevault_2_status_check.sh "${MUNKIPATH}preflight.d/filevaultstatus"
+
+	# Set preference to include this file in the preflight check
+	defaults write "${PREFPATH}" ReportItems -dict-add filevault_status_model "${MUNKIPATH}preflight.d/cache/filevaultstatus.txt"
+else
+	echo "! Failed to install all required components"
+	echo "! Skipping filevault status report"
 	rm -f /usr/local/bin/filevault_2_status_check.sh \
 		"${MUNKIPATH}preflight.d/filevaultstatus"
-	exit 1
+	# Set the exit status
+	ERR=1
 fi
 
-# Make executable
-chmod a+x /usr/local/bin/filevault_2_status_check.sh "${MUNKIPATH}preflight.d/filevaultstatus"
-
-# Set preference to include this file in the preflight check
-defaults write "${PREFPATH}" ReportItems -dict-add filevault_status_model "${MUNKIPATH}preflight.d/cache/filevaultstatus.txt"
--- app/views/install/install_script.php
+++ app/views/install/install_script.php
@@ -8,6 +8,9 @@ BASEURL="<?php echo
 TPL_BASE="${BASEURL}/assets/client_installer/"
 MUNKIPATH="/usr/local/munki/" # TODO read munkipath from munki config
 PREFPATH="/Library/Preferences/MunkiReport"
+CURL="/usr/bin/curl --fail --silent  --show-error"
+# Exit status
+ERR=0
 VERSION="1.0.0"
 
 echo "BaseURL is ${BASEURL}"
@@ -15,9 +18,9 @@ echo "BaseURL is ${BASEURL}"
 echo "Retrieving munkireport scripts"
 
 cd ${MUNKIPATH}
-curl --fail --silent "${TPL_BASE}{preflight,postflight,report_broken_client}" --remote-name --remote-name --remote-name \
-	&& curl --fail --silent "${TPL_BASE}reportcommon" -o "${MUNKIPATH}munkilib/reportcommon.py" \
-	&& curl --fail --silent "${TPL_BASE}phpserialize" -o "${MUNKIPATH}munkilib/phpserialize.py"
+$CURL "${TPL_BASE}{preflight,postflight,report_broken_client}" --remote-name --remote-name --remote-name \
+	&& $CURL "${TPL_BASE}reportcommon" -o "${MUNKIPATH}munkilib/reportcommon.py" \
+	&& $CURL "${TPL_BASE}phpserialize" -o "${MUNKIPATH}munkilib/phpserialize.py"
 
 if [ "${?}" != 0 ]
 then
@@ -29,14 +32,10 @@ fi
 
 chmod a+x "${MUNKIPATH}"{preflight,postflight,report_broken_client}
 
-# Set munkireport version
-rm "${MUNKIPATH}munkireport-"* 2>/dev/null
-touch "${MUNKIPATH}munkireport-${VERSION}"
-
 # Create preflight.d + download scripts
 mkdir -p "${MUNKIPATH}preflight.d"
 cd "${MUNKIPATH}preflight.d"
-curl --fail --silent "${TPL_BASE}submit.preflight" --remote-name
+${CURL} "${TPL_BASE}submit.preflight" --remote-name
 
 if [ "${?}" != 0 ]
 then
@@ -58,9 +57,24 @@ defaults delete "${PREFPATH}" ReportItems
 <?foreach($scripts AS $scriptname => $filepath):?>
 
 <?="## $scriptname ##"?> 
+echo 'Installing <?=$scriptname?>'
+
 <?=file_get_contents($filepath)?>
 
 <?endforeach?>
 
-echo "Installation of MunkiReport v${VERSION} complete."
-exit 0
+# Remove munkireport version file
+rm -f "${MUNKIPATH}munkireport-"*
+
+if [ $ERR = 0 ]; then
+
+	# Set munkireport version file
+	touch "${MUNKIPATH}munkireport-${VERSION}"
+
+	echo "Installation of MunkiReport v${VERSION} complete."
+
+else
+	echo "! Installation of MunkiReport v${VERSION} incomplete."
+fi
+
+exit $ERR
