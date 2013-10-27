3a48a94e835b6fa24132158526df4aa268684c13
Arjen van Bochoven <arjen@bochoven.net>
2013-10-27 23:10:20 +0100

    Better protection in web.config

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.0.1.355';
+$GLOBALS['version'] = '2.0.1.356';
 
 // Return version without commit count
 function get_version()
