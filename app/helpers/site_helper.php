31ef9d0c05ab5168374ccfbf0a3afb428807e32a
Arjen van Bochoven <arjen@bochoven.net>
2013-11-04 22:39:15 +0100

    Changed network listing and munkireport.js
    Client detail link now points to correct tab

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.0.2.406';
+$GLOBALS['version'] = '2.0.2.407';
 
 // Return version without commit count
 function get_version()
