227dfca11d9d3323bfdf27d944b2249ced172112
Arjen van Bochoven <arjen@bochoven.net>
2013-11-04 20:09:09 +0100

    Changed core
    Remove multiple slashes in request

--- system/kissmvc.php
+++ system/kissmvc.php
@@ -18,7 +18,7 @@ class Engine extends KISS_Engine
 	{
 		header( "HTTP/1.0 404 Not Found" );
 				
-		die( '<html><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL was not found on this server.</p><p>Please go <a href="javascript: history.back( 1 )">back</a> and try again.</p><hr /><p>Powered By: <a href="http://kissmvc.com">KISSMVC</a></p></body></html>' );
+		die( '<html><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>'.$msg.'</p><p>The requested URL was not found on this server.</p><p>Please go <a href="javascript: history.back( 1 )">back</a> and try again.</p><hr /><p>Powered By: <a href="http://kissmvc.com">KISSMVC</a></p></body></html>' );
 	}
 
 	function get_uri_string()
