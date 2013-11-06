1ef8d014dab8c0ecb3f02f7ecb97945ebb130999
Arjen van Bochoven <arjen@bochoven.net>
2013-11-06 17:29:45 +0100

    Added import sys to reportcommon resolves #20

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.0.2.424';
+$GLOBALS['version'] = '2.0.2.426';
 
 // Return version without commit count
 function get_version()
