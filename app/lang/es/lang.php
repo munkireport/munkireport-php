89fcba23ad3b3ceac81cd9f7d73c96b96a72f8b2
Noel B.A <nbalonso@gmail.com>
2013-11-06 19:23:15 +0300

    added spanish lang.php

---
+++ app/lang/es/lang.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * spanish language file
+ *
+ * @author Noel B. A. <contact@nbalonso.com>
+ */
+$lang['year']		= 'año';
+$lang['month']		= 'mes';
+$lang['week'] 		= 'semana';
+$lang['day']		= 'día';
+$lang['hour']		= 'hora';
+$lang['minute']		= 'minuto';
+$lang['second']		= 'segundo';
+// Plurals	
+$lang['years']		= 'años';
+$lang['months']		= 'meses';
+$lang['weeks']		= 'semanas';
+$lang['days']		= 'días';
+$lang['hours']		= 'horas';
+$lang['minutes']	= 'minutos';
+$lang['seconds']	= 'segundos';
+
