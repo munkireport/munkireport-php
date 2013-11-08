d75741a463dd687224a36d44aad8391ab6799bbc
Noel B.A <nbalonso@gmail.com>
2013-11-08 18:48:55 +0300

    two new widgets with pending installs

---
+++ app/views/widgets/pending_3rd_widget.php
@@ -0,0 +1,48 @@
+ 		<div class="col-lg-4 col-md-6">
+
+			<div class="panel panel-default">
+
+				<div class="panel-heading">
+
+					<h3 class="panel-title"><i class="icon-shopping-cart"></i> Pending 3rd Party Installations</h3>
+				
+				</div>
+
+				<div class="list-group scroll-box">
+
+				<?php
+					$mr = new Munkireport; 
+					$sql = "SELECT serial_number, report_plist FROM munkireport where itemstoinstall > 0";
+					// Get compression (fixme: we should be able to read this from the model) 
+					$compress = function_exists('gzdeflate');
+					
+					//loop through all the plists
+					foreach($mr->query($sql) as $obj){
+		
+            			$report_plist = unserialize( $compress ? gzinflate( $obj->report_plist ) : $obj->report_plist );
+            			
+            			//loop inside the plist to get the pending install and fill the pendinginstalls_array with the display names
+                		foreach($report_plist['ItemsToInstall'] AS $pendinginstall){
+                			$pendinginstalls_array[] = $pendinginstall['display_name'] . ' ' . $pendinginstall['version_to_install'];
+                		}
+                		
+					}
+					//group the updates by count now that the loops are done
+					$pendinginstalls_array = array_count_values($pendinginstalls_array);
+					arsort($pendinginstalls_array);
+				?> 
+				<?foreach(array_keys($pendinginstalls_array) as $obj):?>
+
+
+					<a href="<?=url('show/listing/munki#pendinginstalls')?>" class="list-group-item">
+					<!--//echo first the key names (update name) and then their values (count) -->
+                	<?=$obj?>
+                	<span class="badge pull-right"><?=$pendinginstalls_array[$obj]?></span>
+            		</a>
+
+				<?endforeach?>
+				</div>
+
+			</div><!-- /panel -->
+
+		</div><!-- /col -->
