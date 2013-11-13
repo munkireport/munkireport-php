e2a3fd3a1498d6c47189c0a3290c94bf8466e5cb
Noel B.A <nbalonso@gmail.com>
2013-11-13 19:17:13 +0300

    margin added. tittle is now standard. indented

--- app/views/inventory/inventoryitem_detail.php
+++ app/views/inventory/inventoryitem_detail.php
@@ -6,59 +6,64 @@
 
 <div class="container">
 
-    <div class="row">
-
-<script type="text/javascript" charset="utf-8">
-    $(document).ready(function() {
-        $('.table').dataTable({
-            "iDisplayLength": 25,
-            "aLengthMenu": [[25, 50, -1], [25, 50, "All"]],
-            "bStateSave": true,
-            "aaSorting": [[1,'asc']]
-        });
-    } );
-</script>
-
-<h1><?=$name?></h1>
-
-<? if (count($inventory_items)): ?>
-    <h2>Machines (<?=count($inventory_items)?>)</h2>
-    <table class='table table-striped table-condensed table-bordered'>
-        <thead>
-            <tr>
-              <th>Hostname</th>
-              <th>Username</th>
-              <th>Version</th>
-              <th>BundleID</th>
-              <th>CFBundleName</th>
-              <th>Path</th>
-            </tr>
-        </thead>
-        <tbody>
-        <? foreach($inventory_items as $item): ?>
-        <? $url=url('clients/detail/' . $item['serial'] . '#tab_inventory-items') ?>
-            <tr>
-                <td>
-                  <a href='<?= $url ?>'>
-                    <?= $item['hostname'] ?>
-                  </a>
-                </td>
-                <td><?= $item['username'] ?></td>
-                <td><?= $item['version'] ?></td>
-                <td><?= $item['bundleid'] ?></td>
-                <td><?= $item['bundlename'] ?></td>
-                <td><?= $item['path'] ?></td>
-            </tr>
-        <? endforeach ?>
-        </tbody>
-    </table>
-<? else: ?>
-    <h2>Machines</h2>
-    <p><i>No machines.</i></p>
-<? endif ?>
-
-    </div> <!-- /row -->
-
+  <div class="row">
+    
+    <div class="col-lg-12">
+    
+      <script type="text/javascript" charset="utf-8">
+          $(document).ready(function() {
+              $('.table').dataTable({
+                  "iDisplayLength": 25,
+                  "aLengthMenu": [[25, 50, -1], [25, 50, "All"]],
+                  "bStateSave": true,
+                  "aaSorting": [[1,'asc']]
+              });
+          } );
+      </script>
+      
+      <h3><?=$name?> <span id="inv-count" class='label label-primary'><?=count($inventory_items)?></span></h3>
+      
+      <? if (count($inventory_items)): ?>
+          <!--
+          <h2>Machines (<?=count($inventory_items)?>)</h2> 
+          -->
+          <table class='table table-striped table-condensed table-bordered'>
+              <thead>
+                  <tr>
+                    <th>Hostname</th>
+                    <th>Username</th>
+                    <th>Version</th>
+                    <th>BundleID</th>
+                    <th>CFBundleName</th>
+                    <th>Path</th>
+                  </tr>
+              </thead>
+              <tbody>
+              <? foreach($inventory_items as $item): ?>
+              <? $url=url('clients/detail/' . $item['serial'] . '#tab_inventory-items') ?>
+                  <tr>
+                      <td>
+                        <a href='<?= $url ?>'>
+                          <?= $item['hostname'] ?>
+                        </a>
+                      </td>
+                      <td><?= $item['username'] ?></td>
+                      <td><?= $item['version'] ?></td>
+                      <td><?= $item['bundleid'] ?></td>
+                      <td><?= $item['bundlename'] ?></td>
+                      <td><?= $item['path'] ?></td>
+                  </tr>
+              <? endforeach ?>
+              </tbody>
+          </table>
+      <? else: ?>
+          <h2>Machines</h2>
+          <p><i>No machines.</i></p>
+      <? endif ?>
+    </div> <!-- /span 12 -->
+    
+  </div> <!-- /row -->
+  
 </div>  <!-- /container -->
 
 <?$this->view('partials/foot')?>
