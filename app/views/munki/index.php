b70053629e48f334017222168fae4aee4e3c1df8
Arjen van Bochoven <arjen@bochoven.net>
2013-11-17 00:03:03 +0100

    Removed munki

--- app/views/munki/index.php
+++
@@ -1,7 +0,0 @@
-<?$this->view('partials/head')?>
-<center>
-	<a href="http://munki.googlecode.com/">
-		<img src="http://munki.googlecode.com/files/munki10-no_expression-wandering_eye.jpg" />
-	</a>
-</center>
-<?$this->view('partials/foot')?>
--- app/views/munki/manifests.php
+++
@@ -1,6 +0,0 @@
-<?$this->view('partials/head')?>
-<h1>Manifests</h1>
-<p>
-	This is just a placeholder
-</p>
-<?$this->view('partials/foot')?>
