8ff8e485a008cf4b2ada9bbb27404d28ef850b8a
Noel B.A <nbalonso@gmail.com>
2013-11-30 20:14:09 +0300

    fixed migration on sqlite

--- app/migrations/directory_service_model/001_add_dsconfigad_data.php
+++ app/migrations/directory_service_model/001_add_dsconfigad_data.php
@@ -19,30 +19,50 @@ class Migration_add_dsconfigad_data extends Model
 					// Wrap in transaction
 					$dbh->beginTransaction();
 
-					// Create new columns
-					$sql = "ALTER TABLE directoryservice
-							ADD adforest VARCHAR(255),
-							ADD addomain VARCHAR(255),
-							ADD computeraccount VARCHAR(255),
-							ADD createmobileaccount BOOL,
-							ADD requireconfirmation BOOL,
-							ADD forcehomeinstartup BOOL,
-							ADD mounthomeassharepoint BOOL,
-							ADD usewindowsuncpathforhome BOOL,
-							ADD networkprotocoltobeused VARCHAR(255),
-							ADD defaultusershell VARCHAR(255),
-							ADD mappinguidtoattribute VARCHAR(255),
-							ADD mappingusergidtoattribute VARCHAR(255),
-							ADD mappinggroupgidtoattr VARCHAR(255),
-							ADD generatekerberosauth BOOL,
-							ADD preferreddomaincontroller VARCHAR(255),
-							ADD allowedadmingroups VARCHAR(255),
-							ADD authenticationfromanydomain BOOL,
-							ADD packetsigning VARCHAR(255),
-							ADD packetencryption VARCHAR(255),
-							ADD passwordchangeinterval VARCHAR(255),
-							ADD restrictdynamicdnsupdates VARCHAR(255),
-							ADD namespacemode VARCHAR(255)";
+					// Create new columns one at a time. Faster to do new table?
+					$sql = "ALTER TABLE directoryservice ADD COLUMN adforest VARCHAR(255)";
+					$dbh->exec($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN addomain VARCHAR(255)";
+					$dbh->exec($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN computeraccount VARCHAR(255)";
+					$dbh->exec($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN createmobileaccount BOOL";
+					$dbh->exec($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN requireconfirmation BOOL";
+					$dbh->exec($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN forcehomeinstartup BOOL";
+					$dbh->exec($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN mounthomeassharepoint BOOL";
+					$dbh->exec($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN usewindowsuncpathforhome BOOL";
+					$dbh->exec($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN networkprotocoltobeused VARCHAR(255)";
+					$dbh->exec($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN defaultusershell VARCHAR(255)";
+					$dbh->exec($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN mappinguidtoattribute VARCHAR(255)";
+					$dbh->exec($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN mappingusergidtoattribute VARCHAR(255)";
+					$dbh->exec($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN mappinggroupgidtoattr VARCHAR(255)";
+					$dbh->exec($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN generatekerberosauth BOOL";
+					$dbh->exec($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN preferreddomaincontroller VARCHAR(255)";
+					$dbh->exec($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN allowedadmingroups VARCHAR(255)";
+					$dbh->exec($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN authenticationfromanydomain BOOL";
+					$dbh->exec($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN packetsigning VARCHAR(255)";
+					$dbh->exec($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN packetencryption VARCHAR(255)";
+					$dbh->exec($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN passwordchangeinterval VARCHAR(255)";
+					$dbh->exec($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN restrictdynamicdnsupdates VARCHAR(255)";
+					$dbh->exec($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN namespacemode VARCHAR(255)";
 					$dbh->exec($sql);
 
 					$dbh->commit();
@@ -107,31 +127,51 @@ class Migration_add_dsconfigad_data extends Model
 					// Wrap in transaction
 					$dbh->beginTransaction();
 
-					// Create new columns
-					$sql = "ALTER TABLE directoryservice
-							ADD adforest VARCHAR(255),
-							ADD addomain VARCHAR(255),
-							ADD computeraccount VARCHAR(255),
-							ADD createmobileaccount BOOL,
-							ADD requireconfirmation BOOL,
-							ADD forcehomeinstartup BOOL,
-							ADD mounthomeassharepoint BOOL,
-							ADD usewindowsuncpathforhome BOOL,
-							ADD networkprotocoltobeused VARCHAR(255),
-							ADD defaultusershell VARCHAR(255),
-							ADD mappinguidtoattribute VARCHAR(255),
-							ADD mappingusergidtoattribute VARCHAR(255),
-							ADD mappinggroupgidtoattr VARCHAR(255),
-							ADD generatekerberosauth BOOL,
-							ADD preferreddomaincontroller VARCHAR(255),
-							ADD allowedadmingroups VARCHAR(255),
-							ADD authenticationfromanydomain BOOL,
-							ADD packetsigning VARCHAR(255),
-							ADD packetencryption VARCHAR(255),
-							ADD passwordchangeinterval VARCHAR(255),
-							ADD restrictdynamicdnsupdates VARCHAR(255),
-							ADD namespacemode VARCHAR(255)";
-					$this->query($sql);
+					// Create new columns one at a time. Faster to do new table?
+					$sql = "ALTER TABLE directoryservice ADD COLUMN adforest VARCHAR(255)";
+					$dbh->query($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN addomain VARCHAR(255)";
+					$dbh->query($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN computeraccount VARCHAR(255)";
+					$dbh->query($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN createmobileaccount BOOL";
+					$dbh->query($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN requireconfirmation BOOL";
+					$dbh->query($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN forcehomeinstartup BOOL";
+					$dbh->query($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN mounthomeassharepoint BOOL";
+					$dbh->query($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN usewindowsuncpathforhome BOOL";
+					$dbh->query($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN networkprotocoltobeused VARCHAR(255)";
+					$dbh->query($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN defaultusershell VARCHAR(255)";
+					$dbh->query($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN mappinguidtoattribute VARCHAR(255)";
+					$dbh->query($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN mappingusergidtoattribute VARCHAR(255)";
+					$dbh->query($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN mappinggroupgidtoattr VARCHAR(255)";
+					$dbh->query($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN generatekerberosauth BOOL";
+					$dbh->query($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN preferreddomaincontroller VARCHAR(255)";
+					$dbh->query($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN allowedadmingroups VARCHAR(255)";
+					$dbh->query($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN authenticationfromanydomain BOOL";
+					$dbh->query($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN packetsigning VARCHAR(255)";
+					$dbh->query($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN packetencryption VARCHAR(255)";
+					$dbh->query($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN passwordchangeinterval VARCHAR(255)";
+					$dbh->query($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN restrictdynamicdnsupdates VARCHAR(255)";
+					$dbh->query($sql);
+					$sql = "ALTER TABLE directoryservice ADD COLUMN namespacemode VARCHAR(255)";
+					$dbh->query($sql);
 
 					$dbh->commit();
 				}
