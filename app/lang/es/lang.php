248a53163652138626438ca51c2f9927da633b49
Noel B.A <nbalonso@gmail.com>
2013-12-20 00:09:32 +0300

    added more spanish translations

--- app/lang/es/lang.php
+++ app/lang/es/lang.php
@@ -21,6 +21,7 @@ $lang['minutes']	= 'minutos';
 $lang['seconds']	= 'segundos';
 
 $lang['version']	= 'versión';
+$lang['unknown']	= 'desconocido';
 
 // Authentication
 
@@ -34,6 +35,12 @@ $lang['auth_generate'] = 'Generar';
 $lang['auth_noauth_found'] = 'No se ha encontrado ninguna autenticación, por favor añade una cuenta al archivo de configuración.';
 $lang['auth_addtoconfig'] = 'Añade esta linea a tu config.php:';
 $lang['auth_gen_other'] = 'Generar otro';
+$lang['wrong_user_or_pass'] = 'Usuario o contraseña incorrectos';
+$lang['error_contacting_AD'] = 'Ha occurido un error al contactar con AD';
+$lang['not_authorized'] = 'No autorizado';
+$lang['empty_not_allowed'] = 'No están permitidos los valores en blanco';
+$lang['auth_insecure'] = 'Conexión insegura, cambiar a conexión segura';
+$lang['auth_secure'] = 'Estás usando una conexión insegura';
 
 $lang['back_to_site'] = 'Volver al sitio';
 
@@ -46,6 +53,8 @@ $lang['client_nonexistent'] = 'No existe ningún cliente con el número de serie
 $lang['error'] = 'Error';
 $lang['error_403'] = 'No está autorizado a ver esta página';
 $lang['error_404'] = 'Página no encontrada';
+$lang['error_426'] = 'Tienes que visitar esta web a través de una conexión segura. 
+<a href="%s">Cambiar a conexión segura</a>';
 
 // Warranty lookup
 
