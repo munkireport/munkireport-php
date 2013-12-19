b05ceb441f67296c363f56fff53b0cb74f55fd91
Noel B.A <nbalonso@gmail.com>
2013-12-19 23:37:15 +0300

    Reverted to use alt syntax
    it makes reading mixed php and html easier.

--- app/views/widgets/filevault_widget.php
+++ app/views/widgets/filevault_widget.php
@@ -10,28 +10,29 @@
 
 				<div class="list-group">
 
-				<?php
-					$fv = new filevault_status_model(); 
+				<?	$fv = new filevault_status_model(); 
 					$sql = "SELECT count(1) AS count,
 							filevault_status FROM filevault_status
 							GROUP BY filevault_status
 							ORDER BY filevault_status";
 					$cnt = 0;
-				
-					foreach($fv->query($sql) as $obj) {
-							if (empty($obj->filevault_status)) {
-								echo '<a class="list-group-item">' . lang('unknown') . '<span class="badge pull-right">' .$obj->count . '</span></a>';
-							} else {
-								echo '<a href=' . url('show/listing/security#'.$obj->filevault_status) . 
-								' class="list-group-item"><span class="badge">' . $obj->count . '</span>' . $obj->filevault_status . '</a>';
-							}
-							$cnt++;
-					}
-
-					if (! $cnt){
-						echo '<span class="list-group-item">No Filevault status available</span>';
-					}
 				?>
+					<?foreach($fv->query($sql) as $obj):?>
+							<?if (empty($obj->filevault_status)):?>
+								<a class="list-group-item"><?=lang('unknown')?>
+									<span class="badge pull-right"><?=$obj->count?></span>
+								</a>
+							<?else:?>
+								<a href="<?=url('show/listing/security#'.$obj->filevault_status)?>" class="list-group-item">
+									<span class="badge"><?=$obj->count?></span>
+									<?=$obj->filevault_status?>
+								</a>
+							<?endif?>
+							<?$cnt++?>
+					<?endforeach?>
+					<?if( ! $cnt):?>
+						<span class="list-group-item">No Filevault status available</span>
+						<?endif?>
 
 				</div>
 
