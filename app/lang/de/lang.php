d286f6a0ee29ae28514948a374e7ce2856ea1bc2
Fridolin Koch <fridomac@googlemail.com>
2013-12-19 11:13:07 +0100

    Updated german translation file

--- app/lang/de/lang.php
+++ app/lang/de/lang.php
@@ -22,6 +22,8 @@ $lang['seconds']	= 'Sekunden';
 
 $lang['version']	= 'version';
 
+// Authentication
+
 $lang['username']	= 'Username';
 $lang['password']	= 'Passwort';
 $lang['auth_login']	= 'Login';
@@ -32,6 +34,12 @@ $lang['auth_generate'] = 'Generiere';
 $lang['auth_noauth_found'] = 'Keine Informationen zur Authentifizierung gefunden, bitte einen Account zum Konfig-File hinzufuegen';
 $lang['auth_addtoconfig'] = 'Diese Zeile zur config.php hinzufuegen:';
 $lang['auth_gen_other'] = 'Noch eine generieren';
+$lang['wrong_user_or_pass'] = 'Falscher Username oder Passwort';
+$lang['error_contacting_AD'] = 'Beim der Kommunikation mit AD ist ein Fehler aufgetreten';
+$lang['not_authorized'] = 'Nicht authorisiert';
+$lang['empty_not_allowed'] = 'Leere Werte sind nicht erlaubt';
+$lang['auth_insecure'] = 'Unsichere Verbindung, schalte auf sichere Verbindung um';
+$lang['auth_secure'] = 'Sie benutzen eine sichere Verbindung';
 
 $lang['back_to_site'] = 'Zurueck zur Site';
 
@@ -44,6 +52,8 @@ $lang['client_nonexistent'] = 'Client mit der Seriennummer "%s" existiert nicht'
 $lang['error'] = 'Fehler';
 $lang['error_403'] = 'Sie sind nicht berechtigt, diese Seite zu sehen';
 $lang['error_404'] = 'Seite nicht gefunden';
+$lang['error_426'] = 'Sie muessen diese Site ueber eine sichere Verbindung verwenden. 
+<a href="%s">Auf gesicherte Site umschalten</a>';
 
 // Warranty lookup
 
