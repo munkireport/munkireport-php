1f84d1ffb4f43e2cb9a4a44016ffa71c2080fed3
Arjen van Bochoven <arjen@bochoven.net>
2013-12-25 23:17:58 +0100

    Fix purchase date calculation to resolve #58

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.0.5.621';
+$GLOBALS['version'] = '2.0.5.622';
 
 // Return version without commit count
 function get_version()
