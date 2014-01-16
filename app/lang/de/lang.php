e3df5f5d4cbf4a497722b1e1799a71f405046296
fridomac <fridomac@googlemail.com>
2014-01-16 19:58:07 +0100

    Added umlauts to german translation

--- app/lang/de/lang.php
+++ app/lang/de/lang.php
@@ -24,10 +24,10 @@ $lang['seconds']	= 'Sekunden';
 
 $lang['munkireport_version'] = 'Munkireport version %s';
 $lang['version']		= 'Version';
-$lang['unknown']		= 'Unknown';
+$lang['unknown']		= 'unbekannt';
 $lang['name']			= 'Name';
-$lang['enabled']		= 'Enabled';
-$lang['disabled']		= 'Disabled';
+$lang['enabled']		= 'Aktiviert';
+$lang['disabled']		= 'Deaktiviert';
 
 // Authentication
 
@@ -38,8 +38,8 @@ $lang['auth_signin']= 'Einloggen';
 $lang['auth_generate_hash'] = 'Generiere password hash';
 $lang['auth_generated_hash'] = 'Generierter hash';
 $lang['auth_generate'] = 'Generiere';
-$lang['auth_noauth_found'] = 'Keine Informationen zur Authentifizierung gefunden, bitte einen Account zum Konfig-File hinzufuegen';
-$lang['auth_addtoconfig'] = 'Diese Zeile zur config.php hinzufuegen:';
+$lang['auth_noauth_found'] = 'Keine Informationen zur Authentifizierung gefunden, bitte einen Account zum Konfig-File hinzufügen';
+$lang['auth_addtoconfig'] = 'Diese Zeile zur config.php hinzufügen:';
 $lang['auth_gen_other'] = 'Noch eine generieren';
 $lang['wrong_user_or_pass'] = 'Falscher Username oder Passwort';
 $lang['error_contacting_AD'] = 'Beim der Kommunikation mit AD ist ein Fehler aufgetreten';
@@ -48,7 +48,7 @@ $lang['empty_not_allowed'] = 'Leere Werte sind nicht erlaubt';
 $lang['auth_insecure'] = 'Unsichere Verbindung, schalte auf sichere Verbindung um';
 $lang['auth_secure'] = 'Sie benutzen eine sichere Verbindung';
 
-$lang['back_to_site'] = 'Zurueck zur Site';
+$lang['back_to_site'] = 'Zurück zur Site';
 
 // Client
 
@@ -72,7 +72,7 @@ $lang['path'] = 'Pfad';
 $lang['ethernet'] = 'Ethernet';
 $lang['ip_address'] = 'IP Adresse';
 $lang['network_mask'] = 'Netzmaske';
-$lang['prefix_length'] = 'Prefix Laenge';
+$lang['prefix_length'] = 'Prefix Länge';
 $lang['router'] = 'Router';
 $lang['configuration'] = 'Konfiguration';
 
@@ -81,7 +81,7 @@ $lang['configuration'] = 'Konfiguration';
 $lang['error'] = 'Fehler';
 $lang['error_403'] = 'Sie sind nicht berechtigt, diese Seite zu sehen';
 $lang['error_404'] = 'Seite nicht gefunden';
-$lang['error_426'] = 'Sie muessen diese Site ueber eine sichere Verbindung verwenden. 
+$lang['error_426'] = 'Sie müssen diese Site ueber eine sichere Verbindung verwenden. 
 <a href="%s">Auf gesicherte Site umschalten</a>';
 
 // Warranty lookup
