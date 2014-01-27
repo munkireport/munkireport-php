8c9062cc2e39c84d9bc5b04851d191c1dab84fd4
Arjen van Bochoven <arjen@bochoven.net>
2014-01-27 22:30:17 +0100

    Rewrite disk_info so it will run on 10.5

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.0.7.645';
+$GLOBALS['version'] = '2.0.7.646';
 
 // Return version without commit count
 function get_version()
