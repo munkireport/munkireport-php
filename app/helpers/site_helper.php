9cf14be3a1d694cc8065905da559a840fbc8596a
Arjen van Bochoven <arjen@bochoven.net>
2014-02-02 09:39:27 +0100

    Fixed some dashboard widgets
    Adjusted sizes for tablet

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.0.7.665';
+$GLOBALS['version'] = '2.0.7.666';
 
 // Return version without commit count
 function get_version()
