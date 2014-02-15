b26028d20fe80368e3bd9fe639c7c45d850b854e
Arjen van Bochoven <arjen@bochoven.net>
2014-02-15 23:02:39 +0100

    Fix admin: add authorized call

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.0.8.691';
+$GLOBALS['version'] = '2.0.8.692';
 
 // Return version without commit count
 function get_version()
