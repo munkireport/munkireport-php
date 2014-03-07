8f40e7257fc92e3a643825b2fcf2f08c787d759e
Joshua D. Miller <josh.miller@outlook.com>
2014-03-06 21:20:53 -0500

    Removed duplicate < in machine_info.php

--- app/views/client/machine_info.php
+++ app/views/client/machine_info.php
@@ -64,10 +64,10 @@
 					<dt>Software</dt>
 					<dd>OS X <?=$machine->os_version . ' ('
 							. $machine->cpu_arch . ')'?>&nbsp;</dd>
-					<dt>CPU Type</dt>
+					<dt>CPU</dt>
 					<dd><?=machine->cpu?>
-					<dt>CPU Cores</dt>
-					<dd><<?=$machine->number_processors?> core )</dd>
+					<dt>CPU Type</dt>
+					<dd><?=$machine->number_processors?> core</dd>
 					<dt>Serial Number</dt>
 					<dd><?=$serial_number?>&nbsp;</dd>
 					<dt>SMC Version</dt>
