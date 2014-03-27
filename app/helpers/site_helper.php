e74114d46e47eb92585aa809dbb1787ce681356b
Arjen van Bochoven <arjen@bochoven.net>
2014-03-27 23:27:43 +0100

    Disable crlf normalization
    crlf normalization wreaks havoc on win servers

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.0.9.740';
+$GLOBALS['version'] = '2.0.9.745';
 
 // Return version without commit count
 function get_version()
