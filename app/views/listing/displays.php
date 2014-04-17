e6bf1fcce45ff02d04f1ed9e7614c49612e47920
Noel B. A <contact@nbalonso.com>
2014-04-18 01:25:23 +0300

    hide empty rows in display listing

--- app/views/listing/displays.php
+++ app/views/listing/displays.php
@@ -53,8 +53,12 @@
             var name=$('td:eq(0)', nRow).html();
             if(name == ''){name = "No Name"};
             var sn=$('td:eq(1)', nRow).html();
-            var link = get_client_detail_link(name, sn, '<?=url()?>/');
-            $('td:eq(0)', nRow).html(link);
+            if(sn){
+              var link = get_client_detail_link(name, sn, '<?=url()?>/');
+              $('td:eq(0)', nRow).html(link);
+            } else { //hide empty rows
+              $('td', nRow).hide();
+            }
 
             // Internal vs External
             var status=$('td:eq(2)', nRow).html();
