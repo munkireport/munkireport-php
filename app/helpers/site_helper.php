e0d7ec05dfc3093e37ad643365c5c577559fe005
Arjen van Bochoven <arjen@bochoven.net>
2014-06-18 17:16:40 +0200

    Fix datetime in munkireport tab

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.0.10.785';
+$GLOBALS['version'] = '2.0.10.786';
 
 // Return version without commit count
 function get_version()
