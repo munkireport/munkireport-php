2e801d12179939af3f2539f5bf19232915d2e40b
Marnin <marnin.goldberg@rutgers.edu>
2014-06-18 11:56:25 -0400

    Revert "fixed widget bound to DS"
    This reverts commit bd08e57e0d58b5a1f3afc0f09633e3d4ab788536.

--- app/views/widgets/bound_to_ds_widget.php
+++ app/views/widgets/bound_to_ds_widget.php
@@ -2,7 +2,7 @@
 
 			<div class="panel panel-default">
 
-			  <div class="panel-heading" data-container="body" title="Totals since this data is being collected">
+			  <div class="panel-heading">
 
 			    <h3 class="panel-title"><i class="fa fa-bullseye"></i> Bound to a Directory Service</h3>
 
@@ -12,18 +12,17 @@
 			  	<?php
 			  	$queryobj = new directory_service_model();
 				$sql = "SELECT COUNT(1) as total,
-								COUNT(CASE WHEN (which_directory_service LIKE 'Active Directory'
-								OR which_directory_service LIKE 'LDAPv3') THEN 1 END) AS arebound
-								FROM directoryservice;";
+						COUNT(CASE WHEN which_directory_service LIKE '%Not bound%' THEN 1 END) AS notbound
+						FROM directoryservice;";
 				$obj = current($queryobj->query($sql));
 				?>
 				<?if($obj):?>
 				<a href="<?=url('show/listing/directoryservice')?>" class="btn btn-success">
-					<span class="bigger-150"> <?=$obj->arebound?> </span><br>
+					<span class="bigger-150"> <?=$obj->total - $obj->notbound?> </span><br>
 					Bound
 				</a>
 				<a href="<?=url('show/listing/directoryservice')?>" class="btn btn-danger">
-					<span class="bigger-150"> <?=$obj->total - $obj->arebound?> </span><br>
+					<span class="bigger-150"> <?=$obj->notbound?> </span><br>
 					Not Bound
 				</a>
 				<?endif?>
