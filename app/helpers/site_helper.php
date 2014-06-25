151f7372ff2219adc624602fabc6db88f2ca0d4b
Arjen van Bochoven <arjen@bochoven.net>
2014-06-25 14:14:12 +0200

    Use munki verbosity settings for messages

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.0.10.797';
+$GLOBALS['version'] = '2.0.10.804';
 
 // Return version without commit count
 function get_version()
