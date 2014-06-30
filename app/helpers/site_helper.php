028d8ccfc1d62cf81c2107dc0a1c5cc8ff94430d
Arjen van Bochoven <arjen@bochoven.net>
2014-06-30 16:45:00 +0200

    Bump minor version to 1

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.0.10.812';
+$GLOBALS['version'] = '2.1.0.812';
 
 // Return version without commit count
 function get_version()
