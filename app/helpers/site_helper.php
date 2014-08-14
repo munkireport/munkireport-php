5848e0e6940994e4d5a6e605108fadd99da688a6
Arjen van Bochoven <arjen@bochoven.net>
2014-08-14 18:00:16 +0200

    Removed en and nl lang files
    keeping de and es for reference

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.1.0.863';
+$GLOBALS['version'] = '2.1.0.864';
 
 // Return version without commit count
 function get_version()
