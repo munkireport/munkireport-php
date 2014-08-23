25c133b1bb8cb9ed62d45b6ccb995a8d0ef444f2
Arjen van Bochoven <arjen@bochoven.net>
2014-08-23 11:25:04 +0200

    Changed client widget
    More logical order

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.1.0.906';
+$GLOBALS['version'] = '2.1.0.907';
 
 // Return version without commit count
 function get_version()
