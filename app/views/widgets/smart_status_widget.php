3a7c6f6904a7c0fa3a35ef0e8bb3f809d312e573
kene101 <krelliot@it046278.massey.ac.nz>
2014-09-05 11:55:41 +1200

    Revert 4f0fbc4..808e362
    This rolls back to commit 4f0fbc4ef4a4eeccd20310a81446bda1797b3ad2.

--- app/views/widgets/smart_status_widget.php
+++ app/views/widgets/smart_status_widget.php
@@ -12,25 +12,32 @@
 
 			  <?php
 			  	$queryobj = new Machine_model();
-					$sql = "SELECT COUNT(*) AS total
-									FROM diskreport
-									WHERE SMARTStatus = 'Failing';";
+						$sql = "SELECT COUNT(CASE WHEN SMARTStatus='Failing' THEN 1 END) AS Failing,
+										COUNT(CASE WHEN SMARTStatus='Verified' THEN 1 END) AS Verified,
+										COUNT(CASE WHEN SMARTStatus='Not Supported' THEN 1 END) AS Not_Supported
+							 			FROM diskreport;";
 					$obj = current($queryobj->query($sql));
 				?>
 
-				<?if($obj):?>
+				<?if($obj->Failing > 0):?>
 
 					<a href="<?=url('show/listing/disk#failing')?>" class="btn btn-danger">
-						<span class="bigger-150"> <?=$obj->total?> </span><br>
+						<span class="bigger-150"> <?=$obj->Failing?> </span><br>
 						Failing!
 					</a>
 
 				<?else:?>
 
 					<a href="<?=url('show/listing/disk')?>" class="btn btn-success">
-						<span class="bigger-150"> <?=$obj->total?> </span><br>
+						<span class="bigger-150"> <?=$obj->Verified?> </span><br>
 						Verified
 					</a>
+					<?if($obj->Not_Supported > 0):?>
+						<a href="<?=url('show/listing/disk#Not Supported')?>" class="btn btn-info">
+							<span class="bigger-150"> <?=$obj->Not_Supported?> </span><br>
+							Not Supported
+						</a>
+					<?endif?>
 
 				<?endif?>
 
@@ -39,3 +46,5 @@
 			</div><!-- /panel -->
 
 		</div><!-- /col -->
+
+
