ede504ce915485df0a7451623b1cceb32250bfaf
Arjen van Bochoven <arjen@bochoven.net>
2014-10-02 13:13:58 +0200

    Fix ARD preference path

--- app/modules/ard/scripts/install.sh
+++ app/modules/ard/scripts/install.sh
@@ -6,4 +6,4 @@ ARDPREF='/Library/Preferences/com.apple.RemoteDesktop'
 defaults read "$ARDPREF" > /dev/null 2>&1 || defaults write "$ARDPREF" MR created
 
 # Add ARD Preferences to munkireport
-defaults write "${PREFPATH}" ReportItems -dict-add ard_model "$ARDPREF"
+defaults write "${PREFPATH}" ReportItems -dict-add ard_model "${ARDPREF}.plist"
