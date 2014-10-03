1f6461907d54e058a97c3ecddbbfc9d05c1c86ba
Arjen van Bochoven <arjen@bochoven.net>
2014-10-03 23:12:55 +0200

    Some pylint cleanups

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.1.0.915';
+$GLOBALS['version'] = '2.1.0.922';
 
 // Return version without commit count
 function get_version()
