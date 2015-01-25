b1a9b23dc1e1a1317ec62c71190e5f24bcaac899
Arjen van Bochoven <arjen@bochoven.net>
2015-01-25 22:23:17 +0100

    os_version adjustments

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.2.0.968';
+$GLOBALS['version'] = '2.2.0.969';
 
 // Return version without commit count
 function get_version()
