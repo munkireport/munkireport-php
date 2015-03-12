b8ff2820c2245411c84a88a5e40f051d0c2bd5e4
Arjen van Bochoven <arjen@bochoven.net>
2015-03-12 07:24:12 +0100

    Move d3 js to footer

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.4.0.1026';
+$GLOBALS['version'] = '2.4.0.1027';
 
 // Return version without commit count
 function get_version()
