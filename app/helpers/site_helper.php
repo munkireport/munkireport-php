832b58e22e5e763e154f14728c8623b603bf903f
Arjen van Bochoven <arjen@bochoven.net>
2015-04-17 17:08:11 +0200

    Add utf-8 encoding to profile.py

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.4.0.1042';
+$GLOBALS['version'] = '2.4.0.1067';
 
 // Return version without commit count
 function get_version()
