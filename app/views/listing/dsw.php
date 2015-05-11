e70e6e98500aaa90aeadc5aba6a03f750638748d
Benedicte Emilie Brækken <benedicte@braekken.org>
2015-05-11 14:05:29 +0200

    Copied listing from ard and modified.

--- app/views/listing/dsw.php
+++ app/views/listing/dsw.php
@@ -1,6 +1,6 @@
-<?$this->view('partials/head')?>
+<?php $this->view('partials/head'); ?>
 
-<? //Initialize models needed for the table
+<?php //Initialize models needed for the table
 new Machine_model;
 new Reportdata_model;
 new Dsw_model;
@@ -10,61 +10,51 @@ new Dsw_model;
 
   <div class="row">
 
-    <div class="col-lg-12">
-        <script type="text/javascript">
+  	<div class="col-lg-12">
+		<script type="text/javascript">
 
-        $(document).ready(function() {
+		$(document).on('appReady', function(e, lang) {
 
-                // Get column names from data attribute
-                var myCols = [];
-                $('.table th').map(function(){
-                      myCols.push({'mData' : $(this).data('colname')});
-                });
-                oTable = $('.table').dataTable( {
-                    "bProcessing": true,
-                    "bServerSide": true,
-                    "sAjaxSource": "<?=url('datatables/data')?>",
-                    "aoColumns": myCols,
-                    "fnCreatedRow": function( nRow, aData, iDataIndex ) {
-                        // Update name in first column to link
-                        var name=$('td:eq(0)', nRow).html();
-                        if(name == ''){name = "No Name"};
-                        var sn=$('td:eq(1)', nRow).html();
-                        var link = get_client_detail_link(name, sn, '<?=url()?>/');
-                        $('td:eq(0)', nRow).html(link);
+				// Get column names from data attribute
+				var myCols = [];
+				$('.table th').map(function(){
+					  myCols.push({'mData' : $(this).data('colname')});
+				});
+			    oTable = $('.table').dataTable( {
+			        "aoColumns": myCols,
+			        "sAjaxSource": "<?php echo url('datatables/data'); ?>",
+			        "fnCreatedRow": function( nRow, aData, iDataIndex ) {
+			        	// Update name in first column to link
+			        	var name=$('td:eq(0)', nRow).html();
+			        	if(name == ''){name = "No Name"};
+			        	var sn=$('td:eq(1)', nRow).html();
+			        	var link = get_client_detail_link(name, sn, '<?php echo url(); ?>/');
+			        	$('td:eq(0)', nRow).html(link);
 
-                    }
-                } );
+			        }
+			    } );
+			} );
+		</script>
 
-                // Use hash as searchquery
-                if(window.location.hash.substring(1))
-                {
-                    oTable.fnFilter( decodeURIComponent(window.location.hash.substring(1)) );
-                }
-
-
-            } );
-        </script>
-
-          <h3>DS report <span id="total-count" class='label label-primary'>…</span></h3>
-
-          <table class="table table-striped table-condensed table-bordered">
-            <thead>
-              <tr>
-                <th data-colname='machine#computer_name'>Name</th>
-                <th data-colname='machine#serial_number'>Serial</th>
-                <th data-colname='reportdata#long_username'>Username</th>
-                <th data-colname='deploystudiow#ds_workflow'>DS Workflow</th>
-              </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td colspan="7" class="dataTables_empty">Loading data from server</td>
-                </tr>
-            </tbody>
-          </table>
+		  <h3><span data-i18n="listing.ard.title">ARD report</span> <span id="total-count" class='label label-primary'>…</span></h3>
+		  
+		  <table class="table table-striped table-condensed table-bordered">
+		    <thead>
+		      <tr>
+		      	<th data-i18n="listing.computername" data-colname='machine#computer_name'>Name</th>
+		        <th data-i18n="serial" data-colname='machine#serial_number'>Serial</th>
+		        <th data-i18n="listing.username" data-colname='reportdata#long_username'>Username</th>
+		        <th data-i18n="listing.deploystudiow.text" data-i18n-options='{"number":1}' data-colname='deploystudiow#ds_workflow'>DeployStudio workflow</th>
+		      </tr>
+		    </thead>
+		    <tbody>
+		    	<tr>
+					<td colspan="7" class="dataTables_empty">Loading data from server</td>
+				</tr>
+		    </tbody>
+		  </table>
     </div> <!-- /span 12 -->
   </div> <!-- /row -->
 </div>  <!-- /container -->
 
-<?$this->view('partials/foot')?>
+<?php $this->view('partials/foot'); ?>
