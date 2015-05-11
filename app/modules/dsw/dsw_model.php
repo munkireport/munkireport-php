2b23d6f26f624251536f3d3f5861f101a1973d9a
Benedicte Emilie Brækken <benedicte@braekken.org>
2015-05-11 13:08:48 +0200

    Renamed tablename to deploystudiow. Maybe conflict.

--- app/modules/dsw/dsw_model.php
+++ app/modules/dsw/dsw_model.php
@@ -2,7 +2,7 @@
 class Dsw_model extends Model {
 
   function __construct($serial='') {
-    parent::__construct('id', 'dsw'); // Primary key, tablename
+    parent::__construct('id', 'deploystudiow'); // Primary key, tablename
 
     $this->rs['id'] = 0; // TODO Why is primary key set to 0?
     $this->rs['serial_number'] = ''; $this->rt['serial_number'] = 'VARCHAR(255) UNIQUE';
@@ -24,6 +24,8 @@ class Dsw_model extends Model {
     $parser->parse($data);
 
     $plist = $parser->toArray();
+
+    print_r($plist);
 
     if (isset($plist['ds_workflow'])) {
       $this->ds_workflow = $plist['ds_workflow'];
