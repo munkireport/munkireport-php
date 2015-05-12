40ef186d6dafe310c0a730ce929e7632a2e94fb3
Arjen van Bochoven <arjen@bochoven.net>
2015-05-12 20:40:42 +0200

    Add bu controller

---
+++ app/controllers/business_unit.php
@@ -0,0 +1,71 @@
+<?php
+class business_unit extends Controller
+{
+	function __construct()
+	{
+		if( ! $this->authorized())
+		{
+			redirect('auth/login');
+		}
+	}
+
+	function index()
+	{
+		$data = array('session' => $_SESSION);
+
+		echo 'BU dashboard<pre>';
+
+		print_r($_SESSION);
+		return;
+
+		$obj = new View();
+
+
+		// Check if there's a custom dashboard
+		if( file_exists(VIEW_PATH.'dashboard/custom_dashboard'.EXT))
+		{
+			$obj->view('dashboard/custom_dashboard', $data);
+		}
+		else
+		{
+			$obj->view('dashboard/dashboard', $data);
+		}
+		
+	}
+
+	function listing($which = '')
+	{
+		if($which)
+		{
+			$data['page'] = 'clients';
+			$data['scripts'] = array("clients/client_list.js");
+			$view = 'listing/'.$which;
+		}
+		else
+		{
+			$data = array('status_code' => 404);
+			$view = 'error/client_error';
+		}
+
+		$obj = new View();
+		$obj->view($view, $data);
+	}
+
+	function reports($which = 'default')
+	{
+		if($which)
+		{
+			$data['page'] = 'clients';
+			$view = 'report/'.$which;
+		}
+		else
+		{
+			$data = array('status_code' => 404);
+			$view = 'error/client_error';
+		}
+
+		$obj = new View();
+		$obj->view($view, $data);
+	}
+		
+}
