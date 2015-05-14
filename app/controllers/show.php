fdfba0ad9521cafb9505d7adee66e5570f33c1d6
Arjen van Bochoven <arjen@bochoven.net>
2015-05-15 00:14:36 +0200

    Allow multiple dashboards

--- app/controllers/show.php
+++ app/controllers/show.php
@@ -11,20 +11,37 @@ class show extends Controller
 
 	function index()
 	{
+		redirect('show/dashboard');	
+	}
+
+	function dashboard($which = '')
+	{
 		$data = array('session' => $_SESSION);
-		$obj = new View();
 
-
-		// Check if there's a custom dashboard
-		if( file_exists(VIEW_PATH.'dashboard/custom_dashboard'.EXT))
+		if($which)
 		{
-			$obj->view('dashboard/custom_dashboard', $data);
+			$view = 'dashboard/'.$which;
 		}
 		else
 		{
-			$obj->view('dashboard/dashboard', $data);
+			if( file_exists(VIEW_PATH.'dashboard/custom_dashboard'.EXT))
+			{
+				$view = 'dashboard/custom_dashboard';
+			}
+			else
+			{
+				$view = 'dashboard/dashboard';
+			}
 		}
-		
+
+		if( ! file_exists(VIEW_PATH.$view.EXT))
+		{
+			$data = array('status_code' => 404);
+			$view = 'error/client_error';
+		}
+
+		$obj = new View();
+		$obj->view($view, $data);
 	}
 
 	function listing($which = '')
