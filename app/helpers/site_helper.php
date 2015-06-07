8290c1e1b6d112d51c0c1f0810d6ea4f6db823c7
Arjen van Bochoven <arjen@bochoven.net>
2015-06-07 14:56:34 +0200

    Add locales

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.5.3.1219';
+$GLOBALS['version'] = '2.5.3.1220';
 
 // Return version without commit count
 function get_version()
--- app/views/listing/timemachine.php
+++ app/views/listing/timemachine.php
@@ -82,7 +82,7 @@ new Timemachine_model;
 	        	var name=$('td:eq(0)', nRow).html();
 	        	if(name == ''){name = "No Name"};
 	        	var sn=$('td:eq(1)', nRow).html();
-	        	var link = get_client_detail_link(name, sn, '<?=url()?>/', '#tab_power-tab');
+	        	var link = get_client_detail_link(name, sn, '<?=url()?>/', '#tab_summary');
 	        	$('td:eq(0)', nRow).html(link);
 
 	        	// Format start date
