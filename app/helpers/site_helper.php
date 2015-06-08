0fbcc2f5f4a1f33ea8b610ac452a747b9d3a0071
Arjen van Bochoven <arjen@bochoven.net>
2015-06-08 22:15:28 +0200

    Add locales

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.5.3.1227';
+$GLOBALS['version'] = '2.5.3.1228';
 
 // Return version without commit count
 function get_version()
