b6c5433fc0383c188d1404929067191c92fe4bf6
Arjen van Bochoven <arjen@bochoven.net>
2015-06-11 20:25:05 +0200

    Up Bootstrap to 3.3.4

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.5.3.1245';
+$GLOBALS['version'] = '2.5.3.1246';
 
 // Return version without commit count
 function get_version()
