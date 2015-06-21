d51a8cb71603994bb19d0f071cb7d8acb6c4ccea
Arjen van Bochoven <arjen@bochoven.net>
2015-06-21 09:31:25 +0200

    Fix uninstall scripts

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.5.0.1282';
+$GLOBALS['version'] = '2.5.0.1283';
 
 // Return version without commit count
 function get_version()
