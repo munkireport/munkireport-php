6ccc13dba26e85ebfff6109624c847fe588b255a
Arjen van Bochoven <arjen@bochoven.net>
2015-06-27 00:51:39 +0200

    Servermetrics, only store when sum entries > 0

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.5.0.1311';
+$GLOBALS['version'] = '2.5.0.1318';
 
 // Return version without commit count
 function get_version()
