693a0f1a9b55c6f7724c939f0846deab0e8ca44e
Arjen van Bochoven <arjen@bochoven.net>
2015-07-27 01:32:20 +0200

    Add exception handler for rundir
    Fixes #275

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.5.3.1363';
+$GLOBALS['version'] = '2.5.3.1366';
 
 // Return version without commit count
 function get_version()
