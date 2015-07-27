97cab143946b980015171a76d4cc449f7e8c3f6d
Arjen van Bochoven <arjen@bochoven.net>
2015-07-27 10:50:02 +0200

    Add support for custom curl and certificates

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.5.3.1366';
+$GLOBALS['version'] = '2.5.3.1367';
 
 // Return version without commit count
 function get_version()
