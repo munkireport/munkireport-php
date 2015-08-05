e8d145aaeae4d9becc4618a88278c327ac9de4d4
Arjen van Bochoven <arjen@bochoven.net>
2015-08-05 10:15:41 +0200

    Initial implementation of purl

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.6.0.1385';
+$GLOBALS['version'] = '2.6.0.1386';
 
 // Return version without commit count
 function get_version()
