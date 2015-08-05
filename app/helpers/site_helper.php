85595beabc9c4933ecdc3d87ba93e7125ecfbfba
Arjen van Bochoven <arjen@bochoven.net>
2015-08-05 19:41:47 +0200

    Add attribution info to purl.py

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.6.0.1386';
+$GLOBALS['version'] = '2.6.0.1387';
 
 // Return version without commit count
 function get_version()
