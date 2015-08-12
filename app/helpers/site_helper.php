e3a6f8ced31f93df42b7238506187459023e309f
Arjen van Bochoven <arjen@bochoven.net>
2015-08-12 08:46:48 +0200

    Fix BT migration for SQLite

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.6.0.1400';
+$GLOBALS['version'] = '2.6.0.1401';
 
 // Return version without commit count
 function get_version()
