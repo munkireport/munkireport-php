aa959ed44d7b7af37161fbcdcb7368c0891934c2
Arjen van Bochoven <arjen@bochoven.net>
2015-08-28 18:04:14 +0200

    Add debugging to server metrics module

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.6.0.1419';
+$GLOBALS['version'] = '2.6.0.1430';
 
 // Return version without commit count
 function get_version()
