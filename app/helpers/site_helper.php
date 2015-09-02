7037ff5dcad836ce74e4e16e6c74ec403a703b9f
Arjen van Bochoven <arjen@bochoven.net>
2015-09-02 21:11:08 +0200

    And now with the actual code change

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.6.0.1440';
+$GLOBALS['version'] = '2.6.0.1441';
 
 // Return version without commit count
 function get_version()
