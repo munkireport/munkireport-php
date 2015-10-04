2ceb821894edc93dd2f76168a29e47ba37cdc9f5
Arjen van Bochoven <arjen@bochoven.net>
2015-10-04 11:59:14 +0200

    Link pending to munki tab

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.6.0.1450';
+$GLOBALS['version'] = '2.6.0.1460';
 
 // Return version without commit count
 function get_version()
