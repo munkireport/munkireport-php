eb0f582e9eea7b89a72f2bb75975b9253d22a4f5
Arjen van Bochoven <arjen@bochoven.net>
2015-10-05 17:49:42 +0200

    New file vault status widget

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.6.0.1460';
+$GLOBALS['version'] = '2.6.0.1461';
 
 // Return version without commit count
 function get_version()
