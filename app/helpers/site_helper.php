512af7851b1b9a99511e3100423b99e016c23b54
Arjen van Bochoven <arjen@bochoven.net>
2015-10-15 19:16:04 +0200

    Fix crashplan model

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.6.0.1494';
+$GLOBALS['version'] = '2.6.0.1495';
 
 // Return version without commit count
 function get_version()
