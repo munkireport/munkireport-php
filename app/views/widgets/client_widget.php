35fcd87a3d5786c025a1e9866e8aa33a6f1c1cba
Arjen van Bochoven <arjen@bochoven.net>
2015-10-19 00:38:58 +0200

    Clients widget tweaks

--- app/views/widgets/client_widget.php
+++ app/views/widgets/client_widget.php
@@ -1,17 +1,24 @@
 <div class="col-lg-4 col-md-6">
 
-	<div class="panel panel-default">
+	<div class="panel panel-default" id="client-widget">
 
 		<div class="panel-heading">
 
-			<h3 class="panel-title"><i class="fa fa-group"></i> Active clients</h3>
+			<h3 class="panel-title"><i class="fa fa-group"></i>
+				<span data-i18n="client.activity"></span>
+			</h3>
 		
 		</div>
 
-		<div class="panel-body text-center">
+		<div class="panel-body">
 			
 			<svg id="test1" class="center-block"></svg>
-		
+			<div class="text-muted text-center">
+				<span data-i18n="client.total"></span>: <span class="total-clients"></span>
+				|
+				<span data-i18n="client.hour"></span>: <span class="hour-clients"></span>
+			</div>
+
 		</div>
 	
 	</div>
@@ -22,9 +29,17 @@
 
 $(document).on('appReady', function() {
 
+	// Add tooltip
+	$('#client-widget>div.panel-heading')
+		.attr('title', i18n.t('client.panel_title'))
+		.tooltip();
+	
+	var active = i18n.t('active'),
+		inactive = i18n.t('inactive');
+	
 	var testdata1 = [
-		{ key: "Active", y: 0 },
-		{ key: "Inactive", y: 100 }
+		{ key: active, y: 0 },
+		{ key: inactive, y: 100 }
 	];
 	var arcRadius1 = [
 		{ inner: .75, outer: 1.25 },
@@ -32,8 +47,8 @@ $(document).on('appReady', function() {
 	];
 	var colors = ["green", "gray"];
 	var chart;
-	var height = 350;
-	var width = 350;
+	var height = 258;
+	var width = 258;
 	nv.addGraph(function () {
 		chart = nv.models.pieChart()
 			.x(function (d) { return d.key })
@@ -45,7 +60,14 @@ $(document).on('appReady', function() {
 			.growOnHover(false)
 			.arcsRadius(arcRadius1);
 		
-		chart.title("0000");
+		chart.title("    ");
+		
+		chart.legend.dispatch.legendClick = function(d, i){
+		//redraw
+			console.log(d)
+			return false
+		};
+
 		
 		d3.select("#test1")
 			.datum(testdata1)
@@ -63,10 +85,13 @@ $(document).on('appReady', function() {
 		d3.json(url, function(data) {
 			testdata1[0].y = data.lastmonth // Active
 			testdata1[1].y = data.total - data.lastmonth // Inactive
-			chart.title(data.total);
+			chart.title(data.lastmonth);
 
 			d3.select('#test1').datum(testdata1).transition().duration(500).call(chart);
 			nv.utils.windowResize(chart.update);
+			
+			$('#client-widget span.total-clients').text(data.total);
+			$('#client-widget span.hour-clients').text(data.lasthour);
 		});
 
 	};
