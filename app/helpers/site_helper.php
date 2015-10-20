fe984e1cafa3b764b58b3333f132a5bf406887dd
Arjen van Bochoven <arjen@bochoven.net>
2015-10-20 14:27:00 +0200

    Bump version to 2.7.0

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.6.1.1504';
+$GLOBALS['version'] = '2.7.0.1504';
 
 // Return version without commit count
 function get_version()
