5a0bf2ac8b99427e8a918043c556d2475d056fba
Arjen van Bochoven <arjen@bochoven.net>
2015-10-21 12:51:59 +0200

    Enable logging for purl
    Fixes #338

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.7.0.1516';
+$GLOBALS['version'] = '2.7.0.1517';
 
 // Return version without commit count
 function get_version()
