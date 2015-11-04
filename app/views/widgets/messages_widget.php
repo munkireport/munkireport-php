f37af22137494e22f052c58a7c6927021bd9825a
Arjen van Bochoven <arjen@bochoven.net>
2015-11-04 14:35:51 +0100

    Reformat messages widget

--- app/views/widgets/messages_widget.php
+++ app/views/widgets/messages_widget.php
@@ -1,80 +1,70 @@
-		<div class="col-lg-4 col-md-6">
+<div class="col-lg-4 col-md-6">
 
-			<div class="panel panel-default">
+	<div class="panel panel-default">
 
-				<div class="panel-heading" data-container="body" title="Messages generated the last 24 h">
+		<div class="panel-heading" data-container="body" title="Messages generated the last 24 h">
 
-					<h3 class="panel-title"><i class="fa fa-comment"></i> Messages <span id="messages-danger" class="badge pull-right"></span></h3>
+			<h3 class="panel-title"><i class="fa fa-comment"></i> Messages <span id="messages-danger" class="badge pull-right"></span></h3>
 
-				</div>
+		</div>
 
-				<div id="messages" class="list-group scroll-box">
-				</div>
-			<script>
-			$(document).ready(function() {
+		<div id="messages" class="list-group scroll-box"></div>
 
-				function get_messages()
-				{
-					var msg_url='<?=url("module/messages/get")?>'
-					$.getJSON( msg_url )
-					.done(function( data ) {
+	</div><!-- /panel -->
 
-						if(data.error)
-						{
-							alert(data.error)
-							if(data.reload)
-							{
-								location.reload();
-							}
-						}
-						$('#messages').empty();
+</div><!-- /col -->
 
-						var arrayLength = data.items.length
-						if (arrayLength)
-						{
-							for (var i = 0; i < arrayLength; i++) {
-								serial=data.items[i].serial_number
-								type = data.items[i].type
-								$('#messages').append(
-									'<a class="list-group-item list-group-item-'+type+'" '+
-									'href="<?=url("clients/detail/")?>'+serial+'">'+data.items[i].msg+
-									'<span class="pull-right"><time datetime="'+data.items[i].timestamp+'">...</time></span></a>'
-								)
-							}
+<script>
+$(document).on('appUpdate', function(){
 
-							update_time()
-						}
-						else
-						{
-							$('#messages').append('<span class="list-group-item">No messages</span>')
-						}
+	$.getJSON( appUrl + '/module/messages/get')
+	.done(function( data ) {
 
-						setTimeout(function () { get_messages(); }, 60000);
+		if(data.error)
+		{
+			alert(data.error)
+			if(data.reload)
+			{
+				location.reload();
+			}
+		}
+		$('#messages').empty();
+
+		var arrayLength = data.items.length
+		if (arrayLength)
+		{
+			for (var i = 0; i < arrayLength; i++) {
+				serial=data.items[i].serial_number
+				type = data.items[i].type
+				$('#messages').append(
+					'<a class="list-group-item list-group-item-'+type+'" '+
+					'href="<?=url("clients/detail/")?>'+serial+'">'+data.items[i].msg+
+					'<span class="pull-right"><time datetime="'+data.items[i].timestamp+'">...</time></span></a>'
+				)
+			}
+
+			update_time();
+		}
+		else
+		{
+			$('#messages').append('<span class="list-group-item">No messages</span>');
+		}
 
 
-					}).fail(function( jqxhr, textStatus, error ) {
-						$('#messages').empty();
-					    var err = textStatus + ", " + error;
-						$('#messages').append('<span class="list-group-item list-group-item-danger">'+
-							"Request Failed: " + err+'</span>')
-					    setTimeout(function () { get_messages(); }, 60000);
-					});
-				}
-				
+	}).fail(function( jqxhr, textStatus, error ) {
+		$('#messages').empty();
+		var err = textStatus + ", " + error;
+		$('#messages').append('<span class="list-group-item list-group-item-danger">'+
+			"Request Failed: " + err+'</span>')
+	});
 
-				function update_time()
-				{
-					$( "time" ).each(function( index ) {
-						var date = new Date($(this).attr('datetime') * 1000);
-						$(this).html(moment(date).fromNow());
-					});
-				}
+	function update_time()
+	{
+		$( "time" ).each(function( index ) {
+			var date = new Date($(this).attr('datetime') * 1000);
+			$(this).html(moment(date).fromNow());
+		});
+	}
 
-				get_messages();
-				
-			});
-			</script>
-
-			</div><!-- /panel -->
-
-		</div><!-- /col -->
+});
+</script>
