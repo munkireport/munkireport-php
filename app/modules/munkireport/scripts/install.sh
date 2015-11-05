9a95e62fb6225c10cd9f5a1350814223ec4e1e90
Wesley Whetstone <wes@squareup.com>
2015-11-04 20:11:59 -0800

    changed hard coded directories to be dynamic based on munki's ManagedInstalls.plist

--- app/modules/munkireport/scripts/install.sh
+++ app/modules/munkireport/scripts/install.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+###############
+# modified by Wesley Whetstone to dynamically support Munki Install Directories.
+###############
 
-setreportpref "munkireport" '/Library/Managed Installs/ManagedInstallReport.plist'
+# Find out where the munki directory is to set accordingly. 
+munki_install_dir=$(/usr/bin/defaults read /Library/Preferences/ManagedInstalls ManagedInstallDir)
+
+# make sure the munki directory is defined. If not default back to normal
+if [[ "${munki_install_dir}" == "" ]]; then
+  setreportpref "munkireport" '/Library/Managed Installs/ManagedInstallReport.plist'
+fi
+
+setreportpref "munkireport" "${munki_install_dir}/ManagedInstallReport.plist"
