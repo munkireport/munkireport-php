1715ba6ba580faa710412b008d93fdff43a2972c
Arjen van Bochoven <arjen@bochoven.net>
2015-11-06 18:32:00 +0100

    Adjust FV2 escrow script

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.7.0.1568';
+$GLOBALS['version'] = '2.7.0.1578';
 
 // Return version without commit count
 function get_version()
