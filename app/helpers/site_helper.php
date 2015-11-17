da77392eef009952965bb305cb18b45149ff4ad1
Arjen van Bochoven <arjen@bochoven.net>
2015-11-17 21:35:40 +0100

    NL locale bijgewerkt

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.7.0.1607';
+$GLOBALS['version'] = '2.7.0.1608';
 
 // Return version without commit count
 function get_version()
