8e1fb6f6819f80ee6c5a7c765bfd45deff8a6d87
Arjen van Bochoven <arjen@bochoven.net>
2015-11-18 12:26:41 +0100

    Fix typo in de.json

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.7.0.1609';
+$GLOBALS['version'] = '2.7.0.1610';
 
 // Return version without commit count
 function get_version()
