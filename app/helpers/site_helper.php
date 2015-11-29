11e6ab1e362b8854076fed916a37b5ea10a242c2
Arjen van Bochoven <arjen@bochoven.net>
2015-11-29 08:53:36 +0100

    Better error handling for HTTP error

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.7.0.1626';
+$GLOBALS['version'] = '2.7.0.1629';
 
 // Return version without commit count
 function get_version()
