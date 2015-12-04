35e9fe74ebda9960bcd59b00ff09f5b7d47be962
Arjen van Bochoven <arjen@bochoven.net>
2015-12-04 18:18:15 +0100

    Better error handling for UseMunkiAdditionalHttpHeaders

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.7.0.1629';
+$GLOBALS['version'] = '2.7.0.1630';
 
 // Return version without commit count
 function get_version()
