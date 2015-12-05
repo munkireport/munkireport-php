b8f5aeb46f7b0f923ae2eba3df820ddd584b16d2
Arjen van Bochoven <arjen@bochoven.net>
2015-12-05 09:10:50 +0100

    Add Service module README

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.7.0.1637';
+$GLOBALS['version'] = '2.7.0.1638';
 
 // Return version without commit count
 function get_version()
