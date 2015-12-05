2779c71aa9302e67901cef87c5e7f708dc845555
Arjen van Bochoven <arjen@bochoven.net>
2015-12-05 08:51:30 +0100

    Add a README to the server metrics module

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.7.0.1634';
+$GLOBALS['version'] = '2.7.0.1635';
 
 // Return version without commit count
 function get_version()
