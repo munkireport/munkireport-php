bd9f972617d45104e6f8318174f33553f2d73c9c
Arjen van Bochoven <arjen@bochoven.net>
2016-01-13 20:44:54 +0100

    Fix report_broken_client

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.7.3.1649';
+$GLOBALS['version'] = '2.7.3.1650';
 
 // Return version without commit count
 function get_version()
