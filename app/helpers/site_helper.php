0d6aae45c85948d9d6bfd253155f10ba795df03f
Arjen van Bochoven <arjen@bochoven.net>
2016-01-25 16:20:57 +0100

    Add file vault widget to clients report

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.7.3.1652';
+$GLOBALS['version'] = '2.7.3.1653';
 
 // Return version without commit count
 function get_version()
