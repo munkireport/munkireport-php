b74067d792954bd89d47be279d20b921f78ab7a7
Arjen van Bochoven <arjen@bochoven.net>
2016-02-03 12:08:54 +0100

    Add pagination hotkeys to listings

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.7.3.1667';
+$GLOBALS['version'] = '2.7.3.1668';
 
 // Return version without commit count
 function get_version()
