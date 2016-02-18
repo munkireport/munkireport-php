9e1a68e057f18c9aa17b41780b858e96bccb0239
Arjen van Bochoven <arjen@bochoven.net>
2016-02-18 10:32:04 +0100

    Order app versions by version number

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.8.0.1684';
+$GLOBALS['version'] = '2.8.0.1685';
 
 // Return version without commit count
 function get_version()
