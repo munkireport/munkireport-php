df78fb08a842dc07811d1003056503f89586baf7
Erik Gomez <e@eriknicolasgomez.com>
2016-02-24 13:02:02 -0600

    move munkiinfo back to preflight

--- app/modules/munkiinfo/scripts/install.sh
+++ app/modules/munkiinfo/scripts/install.sh
@@ -7,19 +7,19 @@ MODULE_CACHE_FILE="munkiinfo.plist"
 CTL="${BASEURL}index.php?/module/${MODULE_NAME}/"
 
 # Get the scripts in the proper directories
-"${CURL[@]}" "${CTL}get_script/${MODULESCRIPT}" -o "${MUNKIPATH}postflight.d/${MODULESCRIPT}"
+"${CURL[@]}" "${CTL}get_script/${MODULESCRIPT}" -o "${MUNKIPATH}preflight.d/${MODULESCRIPT}"
 
 # Check exit status of curl
 if [ $? = 0 ]; then
 	# Make executable
-	chmod a+x "${MUNKIPATH}postflight.d/${MODULESCRIPT}"
+	chmod a+x "${MUNKIPATH}preflight.d/${MODULESCRIPT}"
 
-	# Set preference to include this file in the postflight check
+	# Set preference to include this file in the preflight check
 	setreportpref $MODULE_NAME "${CACHEPATH}${MODULE_CACHE_FILE}"
 
 else
 	echo "Failed to download all required components!"
-	rm -f "${MUNKIPATH}postflight.d/${MODULESCRIPT}"
+	rm -f "${MUNKIPATH}preflight.d/${MODULESCRIPT}"
 
 	# Signal that we had an error
 	ERR=1
