e59c2f33ddc37ec505b0443062fb5aefed4b6e34
Marnin <marnin.goldberg@rutgers.edu>
2016-02-26 09:39:30 -0500

    Added security model, sql statemnet

--- app/controllers/clients.php
+++ app/controllers/clients.php
@@ -34,11 +34,13 @@ class clients extends Controller
             new Disk_report_model;
             new Warranty_model;
             new Localadmin_model;
+            new Security_model;
 
-            $sql = "SELECT m.*, r.*, w.purchase_date, w.end_date, w.status,
+            $sql = "SELECT m.*, r.*, s.*, w.purchase_date, w.end_date, w.status,
                     l.users, d.TotalSize, d.FreeSpace, d.SMARTStatus, d.CoreStorageEncrypted
                 FROM machine m 
                 LEFT JOIN reportdata r ON (m.serial_number = r.serial_number)
+                LEFT JOIN security s ON (m.serial_number = s.serial_number)
                 LEFT JOIN warranty w ON (m.serial_number = w.serial_number)
                 LEFT JOIN localadmin l ON (m.serial_number = l.serial_number)
                 LEFT JOIN diskreport d ON (m.serial_number = d.serial_number AND d.MountPoint = '/')
