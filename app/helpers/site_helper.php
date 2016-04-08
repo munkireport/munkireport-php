4d9d7886e529655d9aece09e591d2e56b28359d2
Arjen van Bochoven <arjen@bochoven.net>
2016-04-08 20:55:31 +0200

    Limit event widget to max 50 messages

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.9.0.1892';
+$GLOBALS['version'] = '2.9.0.1893';
 
 // Return version without commit count
 function get_version()
