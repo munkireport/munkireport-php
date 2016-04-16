eea5084408c333a5cfb6cc65edf8c45e43960e2a
Arjen van Bochoven <arjen@bochoven.net>
2016-04-16 22:42:02 +0200

    Adjust email template

--- app/modules/notification/views/notification_email.php
+++ app/modules/notification/views/notification_email.php
@@ -1,12 +1,10 @@
-<p>The following events were recorded:</p>
-    
 <?php foreach($events as $event):?>
-<h2><?php echo $event->computer_name . ' - ' . $event->type; ?></h2>
+<h2><?php echo $event->computer_name . ' - ' . $i18nObj->translate($event->type); ?></h2>
 <p><?php echo date('Y-m-d H:i:s', $event->timestamp);?></p>
 <p>    
     <?php echo $i18nObj->translate($event->msg, $event->data); ?><br>
     <?php echo $event->serial_number; ?><br>
-    <a href="<?php printf("%s%s/clients/detail/%s", conf('webhost'), conf('subdirectory'), $event->serial_number); ?>"">
+    <a href="<?php printf("%s%sindex.php?/clients/detail/%s", conf('webhost'), conf('subdirectory'), $event->serial_number); ?>"">
         Visit machine in Munkireport
     </a>
     
