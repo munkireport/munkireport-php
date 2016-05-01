972cce4db33bfe07171c1dee99bf1aa66a51b8e9
Arjen van Bochoven <arjen@bochoven.net>
2016-05-01 10:23:59 +0200

    Add custom script timeout
    Fixes #472
    
    Usage:
    defaults write /Library/Preferences/MunkiReport.plist scriptTimeOut
    -int 20

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.9.0.1893';
+$GLOBALS['version'] = '2.9.0.1894';
 
 // Return version without commit count
 function get_version()
