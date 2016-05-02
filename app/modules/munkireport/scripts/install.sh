bcbdc2030bec51e3748f5f1ecc86c1068c484f78
Wesley Whetstone <wes@squareup.com>
2016-05-01 18:24:40 -0700

    adding support for dynamic munki preferences with CFPreferences, configures munkireport to match that of munki's ManagedInstalls if changed from the default settings

--- app/modules/munkireport/scripts/install.sh
+++ app/modules/munkireport/scripts/install.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-setreportpref "munkireport" '/Library/Managed Installs/ManagedInstallReport.plist'
+# Find out where the munki directory is so we can configure accordingly.
+munki_install_dir=$(/usr/bin/python -c 'import CoreFoundation; print CoreFoundation.CFPreferencesCopyAppValue("ManagedInstallDir", "ManagedInstalls")')
+munki_install_dir_len=$((${#munki_install_dir}-1))
+
+#check if the format of the munki_install_dir is correct.
+if [[ ${munki_install_dir:$munki_install_dir_len:1} == '/' ]]; then
+    munki_install_dir=$(echo ${munki_install_dir} | sed 's/.$//')
+fi
+# make sure the munki directory is defined. If not default back to normal
+if [[ "${munki_install_dir}" == "None" ]]; then
+    #Default back to normal since munki doesn't appear to be installed
+    setreportpref "munkireport" '/Library/Managed Installs/ManagedInstallReport.plist'
+else
+    # configure munkireport to use munki's config
+    setreportpref "munkireport" "${munki_install_dir}/ManagedInstallReport.plist"
+fi
