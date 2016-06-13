a30012cf1810fd3ab408b46e3dd177b4a8b6434b
Arjen van Bochoven <arjen@bochoven.net>
2016-06-13 14:22:12 +0200

    Add checks to managedinstalls.py

--- app/modules/managedinstalls/scripts/managedinstalls.py
+++ app/modules/managedinstalls/scripts/managedinstalls.py
@@ -4,16 +4,22 @@ Filter the result of /Library/Managed Installs/ManagedInstallReport.plist
 to only the parts that represent the installed items
 """
 
-import subprocess
 import plistlib
 import sys
 import os
+
+debug = False
 
 # Skip manual check
 if len(sys.argv) > 1:
     if sys.argv[1] == 'manualcheck':
         print 'Manual check: skipping'
         exit(0)
+    if sys.argv[1] == 'debug':
+        print '**** DEBUGGING ENABLED ****'
+        debug = True
+        import pprint
+        pp = pprint.PrettyPrinter(indent=4)
 
 
 def _DictFromPlist(path):
@@ -48,19 +54,19 @@ def installResult(itemList, globalList):
 
 
 def filterItem(item):
-    """ Only return keys for item """
-    keys = ["display_name", "installed_version", "installed_size",
-            "version_to_install", "installed", "note"]
+    
+        keys = ["display_name", "installed_version", "installed_size",
+                "version_to_install", "installed", "note"]
 
-    out = {}
-    for key in keys:
-        try:
-            out[key] = item[key]
-        # pylint: disable=pointless-except
-        except KeyError:  # not all objects have all these attributes
-            pass
+        out = {}
+        for key in keys:
+            try:
+                out[key] = item[key]
+            # pylint: disable=pointless-except
+            except KeyError:  # not all objects have all these attributes
+                pass
 
-    return out
+        return out
 
 
 # Create cache dir if it does not exist
@@ -68,7 +74,13 @@ cachedir = '%s/cache' % os.path.dirname(os.path.realpath(__file__))
 if not os.path.exists(cachedir):
     os.makedirs(cachedir)
 
-installReport = _DictFromPlist('/Library/Managed Installs/ManagedInstallReport.plist')
+# Check if ManagedInstallReport exists
+ManagedInstallReport = '/Library/Managed Installs/ManagedInstallReport.plist'
+if not os.path.exists(ManagedInstallReport):
+    print '%s is missing.' % ManagedInstallReport
+    installReport = {}
+else:
+    installReport = _DictFromPlist(ManagedInstallReport)
 
 # InstallResults
 # ItemsToInstall
@@ -77,15 +89,26 @@ installReport = _DictFromPlist('/Library/Managed Installs/ManagedInstallReport.p
 # ProblemInstalls
 # RemovalResults
 # RemovedItems
+# pp.pprint(installReport)
 
 globalList = {}
-addItems(installReport.ManagedInstalls, globalList, 'installed')
-addItems(installReport.ProblemInstalls, globalList, 'install_failed')
-addItems(installReport.ItemsToRemove, globalList, 'pending_removal')
-addItems(installReport.ItemsToInstall, globalList, 'pending_install')
+if installReport.get('ManagedInstalls'):
+    addItems(installReport.ManagedInstalls, globalList, 'installed')
+if installReport.get('ProblemInstalls'):
+    addItems(installReport.ProblemInstalls, globalList, 'install_failed')
+if installReport.get('ItemsToRemove'):
+    addItems(installReport.ItemsToRemove, globalList, 'pending_removal')
+if installReport.get('ItemsToInstall'):
+    addItems(installReport.ItemsToInstall, globalList, 'pending_install')
+if installReport.get('AppleUpdates'):
+    addItems(installReport.AppleUpdates, globalList, 'pending_install')
+if installReport.get('RemovalResults'):
+    removeResult(installReport.RemovalResults, globalList)
+if installReport.get('InstallResults'):
+    installResult(installReport.InstallResults, globalList)
 
-removeResult(installReport.RemovalResults, globalList)
-installResult(installReport.InstallResults, globalList)
+if debug:
+    pp.pprint(globalList)
 
 # Write report to cache
 plistlib.writePlist(globalList, "%s/managedinstalls.plist" % cachedir)
