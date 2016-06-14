2f3e0f926483767b43ef3efbb091e9db5926b20b
Arjen van Bochoven <arjen@bochoven.net>
2016-06-14 17:29:22 +0200

    Add list view for managed_installs

--- app/modules/managedinstalls/managedinstalls_controller.php
+++ app/modules/managedinstalls/managedinstalls_controller.php
@@ -14,24 +14,45 @@ class managedinstalls_controller extends Module_controller
 	{
 		// Store module path
 		$this->module_path = dirname(__FILE__);
+		$this->view_path = dirname(__FILE__) . '/views/';
 	}
 	
-  /**
- * Get managedinstalls information for serial_number
- *
- * @param string $serial serial number
- **/
-public function get_data($serial_number = '')
-{
-      $obj = new View();
-      if( ! $this->authorized())
-      {
-          $obj->view('json', array('msg' => 'Not authorized'));
-          return;
-      }
+	  /**
+	 * Get managedinstalls information for serial_number
+	 *
+	 * @param string $serial serial number
+	 **/
+	public function get_data($serial_number = '')
+	{
+	      $obj = new View();
+	      if( ! $this->authorized())
+	      {
+	          $obj->view('json', array('msg' => 'Not authorized'));
+	          return;
+	      }
 
-      $managedinstalls = new managedinstalls_model($serial_number);
-      $obj->view('json', array('msg' => $managedinstalls->rs));
-}
+	      $managedinstalls = new managedinstalls_model($serial_number);
+	      $obj->view('json', array('msg' => $managedinstalls->rs));
+	}
+	
+	/**
+	 * undocumented function summary
+	 *
+	 * Undocumented function long description
+	 *
+	 * @param type var Description
+	 * @return {11:return type}
+	 */
+	public function listing($value='')
+	{
+		if( ! $this->authorized())
+		{
+			redirect('auth/login');
+		}
+		$data['page'] = 'clients';
+		$data['scripts'] = array("clients/client_list.js");
+        $obj = new View();
+        $obj->view('managed_installs_list', $data, $this->view_path);
+	}
 
 } // END class default_module
