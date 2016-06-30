2dd092272295a4d535b1a913d7d7454cda81b73d
tuxudo <John@client-75-102-101-73.mobility.psu.edu>
2016-06-29 21:27:44 -0400

    Reorg helper 2

--- app/helpers/ds_helper.php
+++ app/helpers/ds_helper.php
@@ -1,21 +1,6 @@
 <?php
 /**
  *
- * @param UUID of DeployStudio workflow
- * @author n8felton (Nathan Felton)
- **/
-function get_workflow_title($workflow_id){
-	$ds_server = conf('ds_server');
-	$url = "{$ds_server}/workflows/get/entry?id={$workflow_id}";
-	$ds_auto_workflow_result = get_url($url);
-	$workflow = new CFPropertyList();
-	$workflow->parse($ds_auto_workflow_result);
-	$ds_auto_workflow_result = $workflow->toArray();
-	return $ds_auto_workflow_result['title'];
-}
-
-/**
- *
  * @param object deploystudio model instance
  * @author tuxudo (John Eberle)
  * Idea provided by @n8felton
@@ -27,15 +12,15 @@ function pull_ds_data(&$Ds_model)
 
 	$ds_server = conf('ds_server');
 
-  // Get computer data from DeployStudio
-  $url = "{$ds_server}/computers/get/entry?id={$Ds_model->serial_number}";
-  $ds_computer_result = get_url($url);
+	// Get computer data from DeployStudio
+	$url = "{$ds_server}/computers/get/entry?id={$Ds_model->serial_number}";
+	$ds_computer_result = get_url($url);
 
-  require_once(APP_PATH . 'lib/CFPropertyList/CFPropertyList.php');
-  $plist = new CFPropertyList();
-  $plist->parse($ds_computer_result);
+	require_once(APP_PATH . 'lib/CFPropertyList/CFPropertyList.php');
+	$plist = new CFPropertyList();
+	$plist->parse($ds_computer_result);
 
-  $plist = $plist->toArray();
+	$plist = $plist->toArray();
 	$plist = array_values($plist);
 	$plist = $plist[0];
 
@@ -64,9 +49,9 @@ function pull_ds_data(&$Ds_model)
  * Retrieve url
  *
  * @return mixed string if successfull, FALSE if failed
- * @author AvB, modified by tuxudo
+ * @author n8felton (Nathan Felton)
  **/
-function get_url($url, $options = array())
+function get_url($url)
 {
 	$ds_username = conf('ds_username');
 	$ds_password = conf('ds_password');
@@ -83,44 +68,16 @@ function get_url($url, $options = array())
 }
 
 /**
- * Add proxy server variables to context_options
  *
- * @return boolean TRUE if succeeded, FALSE if config error
- * @author AvB
+ * @param UUID of DeployStudio workflow
+ * @author n8felton (Nathan Felton)
  **/
-function add_proxy_server(&$context_options)
-{
-	$proxy = conf('proxy');
-
-	if ( ! isset($proxy['server']))
-	{
-		return FALSE;
-	}
-
-	$proxy['server'] = str_replace('tcp://', '', $proxy['server']);
-
-	// If port is not set, default to 8080
-	$proxy['port'] = isset($proxy['port']) ? $proxy['port'] : 8080;
-
-	$context_options['http']['proxy'] = 'tcp://' . $proxy['server'].':'.$proxy['port'];
-	$context_options['http']['request_fulluri'] = TRUE;
-
-	// Authenticated proxy
-	if(isset($proxy['username']) && isset($proxy['password']))
-	{
-		// Encode username and password
-		$auth = base64_encode($proxy['username'].':'.$proxy['password']);
-
-		if( ! isset($context_options['http']['header']))
-		{
-			$context_options['http']['header'] = "";
-		}
-
-		// Add authentication header
-		$context_options['http']['header'] .= "Proxy-Authorization: Basic $auth\r\n";
-
-	}
-
-	return TRUE;
-
+function get_workflow_title($workflow_id){
+	$ds_server = conf('ds_server');
+	$url = "{$ds_server}/workflows/get/entry?id={$workflow_id}";
+	$ds_auto_workflow_result = get_url($url);
+	$workflow = new CFPropertyList();
+	$workflow->parse($ds_auto_workflow_result);
+	$ds_auto_workflow_result = $workflow->toArray();
+	return $ds_auto_workflow_result['title'];
 }
