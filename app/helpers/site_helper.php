42f12b99e3c4657b8b37ad02504bc1c822df2d44
Arjen van Bochoven <arjen@bochoven.net>
2016-07-03 08:56:56 +0200

    Fix time in install history tab

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.9.0.2006';
+$GLOBALS['version'] = '2.9.0.2007';
 
 // Return version without commit count
 function get_version()
