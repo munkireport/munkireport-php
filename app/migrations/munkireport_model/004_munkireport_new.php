c4554f70eb4a2d92895b55b6fba2c85a6572dc79
Arjen van Bochoven <abn290@MW-027134.clients.vu.nl>
2016-07-04 11:28:38 +0200

    Fix migration for SQLite

--- app/migrations/munkireport_model/004_munkireport_new.php
+++ app/migrations/munkireport_model/004_munkireport_new.php
@@ -18,13 +18,13 @@ class Migration_munkireport_new extends Model
                 'report_plist',
                 'runstate',
             );
-            
+
     // Add these columns
     protected $addcols = array(
                 'error_json' => 'BLOB',
                 'warning_json' => 'BLOB',
-            ); 
-    
+            );
+
     /**
      * Constructor
      */
@@ -36,22 +36,21 @@ class Migration_munkireport_new extends Model
             throw new Exception("Only migrate in admin session", 1);
         }
 
-        
-        parent::__construct('id', 'munkireport'); //primary key, tablename        
+
+        parent::__construct('id', 'munkireport'); //primary key, tablename
     }
 
-    
+
     public function up()
-    {            
+    {
         // Get database handle
         $dbh = $this->getdbh();
 
-        
-        // Wrap in transaction
-        $dbh->beginTransaction();
-        
+
+
+
         // ***** Add columns
-        
+
         try {
             foreach($this->addcols as $colname => $type){
                 $sql = sprintf('ALTER TABLE %s ADD COLUMN %s %s',
@@ -61,50 +60,54 @@ class Migration_munkireport_new extends Model
         } catch (Exception $e) {
             // We do nothing here
         }
-        
 
-        
+
+
         // ***** Move content from report_plist to managedinstalls module
-        
+
         // Check if we have compression support
         $compress = function_exists('gzdeflate');
-        
+
         // Instantiate managedinstalls model
         $managedinstalls = new Managedinstalls_model;
-                
+
         // Lock tables
         if ($this->get_driver() == 'mysql'){
-            $sql = "LOCK TABLES munkireport WRITE, managedinstalls WRITE, 
+            $sql = "LOCK TABLES munkireport WRITE, managedinstalls WRITE,
                         migration WRITE, event WRITE, reportdata WRITE, munkiinfo WRITE";
             $this->exec($sql);
         }
-        
+        else{
+            // Wrap in transaction
+            $dbh->beginTransaction();
+        }
+
         // Get number of records to convert
         $sql = "SELECT count(*) as count
-                FROM munkireport 
+                FROM munkireport
                 WHERE report_plist != ''";
         $resultset = $this->query($sql);
         $count = $resultset[0]->count;
-        
+
         // Get limited
-        $sql = "SELECT serial_number, report_plist 
-                FROM munkireport 
+        $sql = "SELECT serial_number, report_plist
+                FROM munkireport
                 WHERE report_plist != ''
                 LIMIT 50";
         if($resultset = $this->query($sql)){
             foreach($resultset as $arr)
             {
                 $report = unserialize( $this->COMPRESS_ARRAY ? gzinflate( $arr->report_plist ) : $arr->report_plist );
-                
+
                 // Load legacy support TODO: use autoloader
                 include_once (APP_PATH . '/lib/munkireport/Legacy_munkireport.php');
                 $legacyObj = new munkireport\Legacy_munkireport;
                 $install_list = $legacyObj->parse($report)->getList();
-                
+
                 // Store data in managedinstalls
                 $managedinstalls->setSerialNumber($arr->serial_number);
                 $managedinstalls->processData($install_list);
-                
+
                 // Save errors and warnings
                 if(isset($report['Errors'])){
                     $sql = sprintf("UPDATE munkireport SET error_json = ? WHERE serial_number = '%s'",
@@ -112,21 +115,25 @@ class Migration_munkireport_new extends Model
                     $stmt = $this->prepare( $sql );
                     $this->execute($stmt, array(json_encode($report['Errors'])));
                 }
-                
+
                 if(isset($report['Warnings'])){
                     $sql = sprintf("UPDATE munkireport SET warning_json = ? WHERE serial_number = '%s'",
                                 $arr->serial_number);
                     $stmt = $this->prepare( $sql );
                     $this->execute($stmt, array(json_encode($report['Warnings'])));
                 }
-                
+
                 // Reset report_plist
                 $sql = sprintf("UPDATE munkireport SET report_plist = '' WHERE serial_number = '%s'",
                                 $arr->serial_number);
                 $this->exec($sql);
             }
-            
-            
+
+            if ($this->get_driver() == 'sqlite'){
+              // Commit transaction
+              $dbh->commit();
+            }
+
             // Prevent migration to be marked complete
             throw new Exception("Still converting $count entries", 1);
         }
@@ -134,14 +141,14 @@ class Migration_munkireport_new extends Model
         // ***** Modify columns
 
         if ($this->get_driver() == 'mysql')
-        {            
+        {
             // Drop columns
             $sql = sprintf('ALTER TABLE munkireport DROP %s', implode(', DROP ', $this->dropCols));
-            $this->exec($sql);             
+            $this->exec($sql);
         }
         else{
             //throw new Exception("SQLite migration not ready", 1);
-            
+
             // Create a temporary table
             $sql = "CREATE TABLE munkireport_temp (
                         id INTEGER PRIMARY KEY,
@@ -157,28 +164,28 @@ class Migration_munkireport_new extends Model
                         error_json BLOB,
                         warning_json BLOB)";
             $this->exec($sql);
-            
+
             // Copy everything to the temp table
-            $sql = "INSERT INTO munkireport_temp 
+            $sql = "INSERT INTO munkireport_temp
                         SELECT id, serial_number, timestamp, runtype, starttime, endtime, version, errors, warnings, manifestname, error_json, warning_json FROM munkireport";
             $this->exec($sql);
-            
+
             // Drop original table
             $sql = "DROP table munkireport";
             $this->exec($sql);
-            
+
             // Rename temp table
             $sql = "ALTER TABLE munkireport_temp RENAME TO munkireport";
             $this->exec($sql);
 
         }
-        
+
         // Commit transaction
         $dbh->commit();
-        
+
         //throw new Exception("Error Processing Request", 1);
-        
-        
+
+
 
     }// End function up()
 
@@ -190,10 +197,10 @@ class Migration_munkireport_new extends Model
      **/
     public function down()
     {
-        
+
         // No down
     }
-    
+
 
 
 }
