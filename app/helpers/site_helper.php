7be28af04e7a1137ab36dc9e66153085b4594314
Arjen van Bochoven <arjen@bochoven.net>
2016-08-03 14:09:47 +0100

    Add munkiprotocol listing
    Fixes #462

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.9.0.2030';
+$GLOBALS['version'] = '2.9.0.2031';
 
 // Return version without commit count
 function get_version()
