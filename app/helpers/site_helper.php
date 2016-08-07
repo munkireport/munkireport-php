917e41f672be40f5b1909e28b85251087faf9760
Arjen van Bochoven <arjen@bochoven.net>
2016-08-07 11:04:47 +0100

    Add support for multiple crash plan destinations

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.9.0.2039';
+$GLOBALS['version'] = '2.9.0.2042';
 
 // Return version without commit count
 function get_version()
