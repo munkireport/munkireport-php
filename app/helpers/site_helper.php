f93fd7f599ecbbb02f177c7614fdd33ec454687f
Arjen van Bochoven <arjen@bochoven.net>
2016-08-23 12:53:52 +0200

    Add pending to widgets

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.9.0.2055';
+$GLOBALS['version'] = '2.9.0.2056';
 
 // Return version without commit count
 function get_version()
--- app/views/widgets/pending_apple_widget.php
+++ app/views/widgets/pending_apple_widget.php
@@ -26,7 +26,7 @@ $(document).on('appUpdate', function(e, lang) {
 		if(data.length){
 			$.each(data, function(i,d){
 				var badge = '<span class="badge pull-right">'+d.count+'</span>',
-                    url = appUrl+'/module/managedinstalls/listing/'+d.name,
+                    url = appUrl+'/module/managedinstalls/listing/'+d.name+'#pending_install',
                     display_name = d.display_name || d.name;
                 
 				box.append('<a href="'+url+'" class="list-group-item">'+display_name+' '+d.version+badge+'</a>');
