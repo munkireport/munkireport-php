5cfe7509e314b56f0172b0309faa10313caf5195
eholtam <eholtam@mac.com>
2016-08-24 00:12:21 -0500

    Parse log and write with appropriate destination

--- app/modules/timemachine/scripts/timemachine.py
+++ app/modules/timemachine/scripts/timemachine.py
@@ -20,49 +20,64 @@ cachedir = '%s/cache' % os.path.dirname(os.path.realpath(__file__))
 if not os.path.exists(cachedir):
     os.makedirs(cachedir)
 
-# Run tmutil destinationinfo -X and capture the xml output to parse
+# Filter the logs for the last run
+logproc = subprocess.Popen(['syslog', '-F', '$((Time)(utc)) $Message', '-k', 'Sender', 'com.apple.backupd', '-k', 'Time', 'ge', '-7d'], stdout=subprocess.PIPE)
+logout, err = logproc.communicate()
+
+#Use regex to capture a block of backup logs from "Starting automatic|manual backup to Backup completed|failed"
+found = re.findall(r'([\d\-]* [\d:]*Z Starting \D* backup\n[\s\S]*?[\d\-]* [\d:]*Z Backup \D*\.)', logout)
+
+#Concatenate the found log block to the logresult and write to a temp file
+logresult = ''
+logresult += '\n' + found[-1] + '\n'
+
+# Run tmutil destinationinfo -X and capture the xml output to parse Destination info
 sp = subprocess.Popen(['tmutil', 'destinationinfo', '-X'], stdout=subprocess.PIPE)
 out, err = sp.communicate()
 
 plist = plistlib.readPlistFromString(out)
 destinations = plist['Destinations']
-result = ''
+
+#Initialize variables
 destinationCount = 0
+result = ''
+isNetwork = False
+isLocal = False
 
 # Examine destinations for information.  May have multiple destinations.
 for destination in destinations:
     destinationCount += 1
     if destination.get('Kind') == "Network":
         try:
+            isNetwork = True
             result += "TM_LOCATION: " + destination['URL'] + '\n'
         except KeyError:
             result += "TM_LOCATION: Undetected" + '\n'
     elif destination.get('Kind') == "Local":
         try:
+            isLocal = True
             result += "TM_LOCATION: " + destination['MountPoint'] + '\n'
         except KeyError:
             result += "TM_LOCATION: Not Mounted" + '\n'
     else:
         result += "TM_LOCATION: UNKNOWN" + '\n'
-
+    
     result += "TM_KIND: " + destination['Kind'] + '\n'
     result += "TM_NAME: " + destination['Name'] + '\n'
+
+    if isLocal:
+    	name = destination.get('Name')
+        if re.search(str(name), logresult):
+            result += logresult
+    if isNetwork:
+    	url = destination.get('URL')
+        if re.search(str(url), logresult):
+            result += logresult       
+
     result += "---------" + '\n'
 
 # Write to disk
 txtfile = open("%s/timemachine.txt" % cachedir, "w")
 txtfile.write("TM_DESTINATIONS: " + str(destinationCount) + '\n')
-
-# Filter the logs for the last run
-logproc = subprocess.Popen(['syslog', '-F', '$((Time)(utc)) $Message', '-k', 'Sender', 'com.apple.backupd', '-k', 'Time', 'ge', '-7d'], stdout=subprocess.PIPE)
-out, err = logproc.communicate()
-
-#Use regex to capture a block of backup logs from "Starting automatic|manual backup to Backup completed|failed"
-found = re.findall(r'([\d\-]* [\d:]*Z Starting \D* backup\n[\s\S]*?[\d\-]* [\d:]*Z Backup \D*\.)', out)
-
-#Concatenate the found log block to the result
-result += '\n' + found[-1] + '\n'
-
-#Write out the result to file
 txtfile.write(result.encode('utf-8'))
 txtfile.close()
