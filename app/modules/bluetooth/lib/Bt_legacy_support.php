5f3a68452a760b61e4c569e29c9c3731f0f748cc
Arjen van Bochoven <arjen@bochoven.net>
2016-08-31 10:50:16 +0200

    Add legacy support

---
+++ app/modules/bluetooth/lib/Bt_legacy_support.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace munkireport;
+
+/**
+ * Utility for legacy bluetooth support
+ *
+ * Converts data from clients with the old Bluetooth module
+ * to the new format
+ *
+ * 
+ */
+class Bt_legacy_support {
+	
+	private $data;
+	
+	public function __construct($data)
+	{
+		$this->data = $data;
+	}
+	
+	function toArray()
+	{
+		$out = array();
+		
+		// Translate network strings to db fields
+		$translate = array(
+			'Status = ' => 'bluetooth_power',
+			'Keyboard = ' => 'keyboard',
+			'Mouse = ' => 'mouse',
+			'Trackpad = ' => 'trackpad');
+			
+		//clear any previous data we had
+		foreach($translate as $search => $field) {
+			$this->$field = -1;
+		}
+		// Parse data
+		foreach(explode("\n", $this->data) as $line) {
+			// Translate standard entries
+			foreach($translate as $search => $field) {
+
+				if(strpos($line, $search) === 0) {
+
+					$value = trim(substr($line, strlen($search)));
+					
+					// Legacy client module
+					if($value == 'Disconnected')
+					{
+						// Don't report on disconnected devices
+						continue;
+					}
+					elseif(preg_match('/(\d+)% battery life remaining/', $value, $matches))
+					{
+						$value = $matches[1];
+					}
+					elseif(preg_match('/Bluetooth is (.+)/', $value, $matches))
+					{
+						$value = $matches[1] == 'on' ? 1 : 0;
+					}
+
+					$out[$field] = intval($value);
+					break;
+				}
+			}
+
+		} //end foreach explode lines
+		
+		return $out;
+	}
+}
