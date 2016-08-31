4d6a6234cb6b0535d9345242a9d9fc9b5185b93b
Arjen van Bochoven <arjen@bochoven.net>
2016-08-31 10:49:59 +0200

    Fix migration

--- app/migrations/bluetooth_model/002_bluetooth_all_devices_in_seperate_db_row.php
+++ app/migrations/bluetooth_model/002_bluetooth_all_devices_in_seperate_db_row.php
@@ -9,9 +9,13 @@ class Migration_bluetooth_all_devices_in_seperate_db_row extends Model
         // frequently that if the client doesn't checkin the values
         // are already out of date
         // Plus this is a major table change.
+        
+        // Get database handle
+        $dbh = $this->getdbh();
+
         $sql = "DROP table bluetooth";
         $dbh->exec($sql);
-        break;
+
     }// End function up()
 
     public function down()
