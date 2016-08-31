43116e8256054b6973596d63af951c752dfafb55
Arjen van Bochoven <arjen@bochoven.net>
2016-08-31 22:46:23 +0200

    Handle unsafe strings, add operator to tablequery

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.9.0.2100';
+$GLOBALS['version'] = '2.9.0.2116';
 
 // Return version without commit count
 function get_version()
