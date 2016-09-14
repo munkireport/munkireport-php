942ca7d15b9b9fc83403b6e7c7affaba7702e0d6
Arjen van Bochoven <arjen@bochoven.net>
2016-09-14 17:04:34 +0200

    Fix authLDAP.php

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.10.0.2167';
+$GLOBALS['version'] = '2.10.0.2168';
 
 // Return version without commit count
 function get_version()
