e71f3e2a53e00fcb5473cfb4b9a895678bb89d5e
Arjen van Bochoven <arjen@bochoven.net>
2016-09-18 21:48:59 +0200

    Remove short_open_tag from kissmvc_core

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.10.0.2179';
+$GLOBALS['version'] = '2.10.0.2186';
 
 // Return version without commit count
 function get_version()
