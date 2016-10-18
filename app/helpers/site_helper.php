2508c137ed243989bdd92c5340c9c70f48226a0a
Arjen van Bochoven <arjen@bochoven.net>
2016-10-18 22:54:22 +0200

    Fix network location widget
    thanks to @poundbangbash

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.11.0.2204';
+$GLOBALS['version'] = '2.11.0.2208';
 
 // Return version without commit count
 function get_version()
