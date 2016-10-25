f56c25d5d7f3b5acbfeee216b90f329da998434e
computeronix <computeronix@users.noreply.github.com>
2016-10-25 12:25:58 -0400

    Add files via upload

---
+++ app/views/client/sccm_status_tab.php
@@ -0,0 +1,38 @@
+<?php //Initialize models needed for the table
+$sccm_status = new sccm_status_model($serial_number);
+?>
+
+	<h2>SCCM_Agent_Status</h2>
+
+		<table class="table table-striped">
+			<tbody>
+				<tr>
+					<td>Enrollment Status</td>
+					<td><?=$sccm_status->agent_status?></td>
+				</tr>
+				<tr>
+					<td>Management Point</td>
+					<td><?=$sccm_status->mgmt_point?></td>
+				</tr>
+					<tr>
+					<td>Enrollment User Name</td>
+					<td><?=$sccm_status->enrollment_name?></td>
+				</tr>
+					<tr>
+					<td>Last Agent Checkin</td>
+					<td><?=$sccm_status->last_checkin?></td>
+				</tr>
+					<tr>
+					<td>Client Cert Expiry</td>
+					<td><?=$sccm_status->cert_exp?></td>
+				</tr>
+				<tr>
+					<td>Enrollment User Name</td>
+					<td><?=$sccm_status->enrollment_name?></td>
+				</tr>
+					<tr>
+					<td>Enrollment Server</td>
+					<td><?=$sccm_status->enrollment_server?></td>
+				</tr>
+			</tbody>
+		</table>
