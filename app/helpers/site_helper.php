dfb51a3b4a8045dcb76b06592e28cec0712c1d2a
Arjen van Bochoven <arjen@bochoven.net>
2016-10-25 22:56:27 +0200

    Fix table redraw after delete machine

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.11.0.2210';
+$GLOBALS['version'] = '2.11.0.2211';
 
 // Return version without commit count
 function get_version()
