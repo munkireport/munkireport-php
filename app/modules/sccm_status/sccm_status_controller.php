12cf97306aecb7611d6b70c7912ac908d096ee51
computeronix <computeronix@users.noreply.github.com>
2016-10-26 11:59:26 -0400

    Update sccm_status_controller.php
    added public

--- app/modules/sccm_status/sccm_status_controller.php
+++ app/modules/sccm_status/sccm_status_controller.php
@@ -10,7 +10,7 @@ class sccm_status_controller extends Module_controller
 {
         
         /*** Protect methods with auth! ****/
-        function __construct()
+        public function __construct()
         {
                 // Store module path
                 $this->module_path = dirname(__FILE__);
@@ -21,7 +21,7 @@ class sccm_status_controller extends Module_controller
          *
          * @author Calum Hunter
          **/
-        function index()
+        public function index()
         {
                 echo "You've loaded the sccm_status module!";
         }
