497ce4e6a4f1e8418270cc6f64f4ab6fd9558183
Arjen van Bochoven <arjen@bochoven.net>
2016-10-26 22:57:18 +0200

    Fix tab name

--- app/views/client/client_detail.php
+++ app/views/client/client_detail.php
@@ -25,7 +25,7 @@ $tab_list = array(
 	'power-tab' => array('view' => 'client/power_tab', 'i18n' => 'client.tab.power'),
 	'printer-tab' => array('view' => 'client/printer_tab', 'i18n' => 'client.tab.printers', 'badge' => 'printer-cnt'),
 	'profile-tab' => array('view' => 'client/profile_tab', 'i18n' => 'client.tab.profiles'),
-	'sccm-tab' => array('view' => 'client/sccm_status_tab', 'i18n' => 'sccm_status.title'),
+	'sccm_status-tab' => array('view' => 'client/sccm_status_tab', 'i18n' => 'sccm_status.title'),
 		);
 
 // Add custom tabs
