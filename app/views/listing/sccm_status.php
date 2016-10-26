3c16c71b29e822b386ab74727e8b7dd0214a6a67
computeronix <computeronix@users.noreply.github.com>
2016-10-25 23:11:12 -0400

    Update sccm_status.php
    Fixed view to new app code

--- app/views/listing/sccm_status.php
+++ app/views/listing/sccm_status.php
@@ -1,6 +1,6 @@
-<?$this->view('partials/head')?>
+<?php $this->view('partials/head'); ?>
 
-<? //Initialize models needed for the table
+<?php //Initialize models needed for the table
 new Machine_model;
 new Reportdata_model;
 new Sccm_status_model;
@@ -11,86 +11,25 @@ new Sccm_status_model;
   <div class="row">
 
   	<div class="col-lg-12">
-		<script type="text/javascript">
 
-		$(document).ready(function() {
-
-				// Get modifiers from data attribute
-				var myCols = [], // Colnames
-					mySort = [], // Initial sort
-					hideThese = [], // Hidden columns
-					col = 0; // Column counter
-
-				$('.table th').map(function(){
-
-					  myCols.push({'mData' : $(this).data('colname')});
-
-					  if($(this).data('sort'))
-					  {
-					  	mySort.push([col, $(this).data('sort')])
-					  }
-
-					  if($(this).data('hide'))
-					  {
-					  	hideThese.push(col);
-					  }
-
-					  col++
-				});
-
-			    oTable = $('.table').dataTable( {
-			        "bProcessing": true,
-			        "bServerSide": true,
-			        "sAjaxSource": "<?=url('datatables/data')?>",
-			        "aaSorting": mySort,
-			        "aoColumns": myCols,
-			        "aoColumnDefs": [
-			        	{ 'bVisible': false, "aTargets": hideThese }
-					],
-			        "fnCreatedRow": function( nRow, aData, iDataIndex ) {
-			        	// Update name in first column to link
-			        	var name=$('td:eq(0)', nRow).html();
-			        	if(name == ''){name = "No Name"};
-			        	var sn=$('td:eq(1)', nRow).html();
-			        	var link = get_client_detail_link(name, sn, '<?=url()?>/', '#tab_sccm_status-tab');
-			        	$('td:eq(0)', nRow).html(link);
-			        	
-			        	// Translate bool. todo function for any bool we find
-                        var status=$('td:eq(7)', nRow).html();
-                        status = status == 1 ? 'Yes' : 
-                        (status === '0' ? 'No' : '')
-                        $('td:eq(7)', nRow).html(status)
-
-				    }
-			    } );
-
-			    // Use hash as searchquery
-			    if(window.location.hash.substring(1))
-			    {
-					oTable.fnFilter( decodeURIComponent(window.location.hash.substring(1)) );
-			    }
-			    
-			} );
-		</script>
-
-		  <h3>SCCM Agent Status report <span id="total-count" class='label label-primary'>…</span></h3>
+		  <h3>Directory Services report <span id="total-count" class='label label-primary'>…</span></h3>
 
 		  <table class="table table-striped table-condensed table-bordered">
 		    <thead>
 		      <tr>
-		      	<th data-colname='machine#computer_name'>Name</th>
-		        <th data-colname='machine#serial_number'>Serial</th>
-		        <th data-colname='sccm_status#agent_status'>SCCM Agent Status</th> 
-		        <th data-colname='sccm_status#mgmt_point'>Management Point</th>
-		        <th data-colname='sccm_status#enrollment_name'>Enrollment User Name</th>
-		        <th data-colname='sccm_status#enrollment_server'>Enrollment Server</th>
-		        <th data-colname='sccm_status#last_checkin'>Last SCCM Check In</th>
-		        <th data-colname='sccm_status#cert_exp'>SCCM Client Cert Expiry</th>
+		      	<th data-i18n="listing.computername" data-colname='machine.computer_name'>Name</th>
+		        <th data-i18n="serial" data-colname='machine.serial_number'>Serial</th>
+		        <th data-colname='sccm_status.agent_status'>SCCM Agent Status</th> 
+		        <th data-colname='sccm_status.mgmt_point'>Management Point</th>
+		        <th data-colname='sccm_status.enrollment_name'>Enrollment User Name</th>
+		        <th data-colname='sccm_status.enrollment_server'>Enrollment Server</th>
+				<th data-colname='sccm_status.last_checkin'>Last SCCM Check In</th>
+				<th data-colname='sccm_statis.cert_exp'>SCCM Client Cert Expiry</th>
 		      </tr>
 		    </thead>
 		    <tbody>
 		    	<tr>
-					<td colspan="5" class="dataTables_empty">Loading data from server</td>
+					<td data-i18n="listing.loading" colspan="5" class="dataTables_empty"></td>
 				</tr>
 		    </tbody>
 		  </table>
@@ -98,4 +37,67 @@ new Sccm_status_model;
   </div> <!-- /row -->
 </div>  <!-- /container -->
 
-<?$this->view('partials/foot')?>
+<script type="text/javascript">
+
+	$(document).on('appUpdate', function(e){
+
+		var oTable = $('.table').DataTable();
+		oTable.ajax.reload();
+		return;
+
+	});
+			
+	$(document).on('appReady', function(e, lang) {
+
+        // Get modifiers from data attribute
+        var mySort = [], // Initial sort
+            hideThese = [], // Hidden columns
+            col = 0, // Column counter
+            columnDefs = [{ visible: false, targets: hideThese }]; //Column Definitions
+
+        $('.table th').map(function(){
+
+            columnDefs.push({name: $(this).data('colname'), targets: col});
+
+            if($(this).data('sort'))
+            {
+              mySort.push([col, $(this).data('sort')])
+            }
+
+            if($(this).data('hide'))
+            {
+              hideThese.push(col);
+            }
+
+            col++
+        });
+
+	    oTable = $('.table').dataTable( {
+            ajax: {
+                url: appUrl + '/datatables/data',
+                type: "POST"
+            },
+            dom: mr.dt.buttonDom,
+            buttons: mr.dt.buttons,
+            order: mySort,
+            columnDefs: columnDefs,
+		    createdRow: function( nRow, aData, iDataIndex ) {
+	        	// Update name in first column to link
+	        	var name=$('td:eq(0)', nRow).html();
+	        	if(name == ''){name = "No Name"};
+	        	var sn=$('td:eq(1)', nRow).html();
+	        	var link = mr.getClientDetailLink(name, sn, '#tab_sccm_status-tab');
+	        	$('td:eq(0)', nRow).html(link);
+	        	
+	        	// Translate bool. todo function for any bool we find
+                var status=$('td:eq(7)', nRow).html();
+                status = status == 1 ? 'Yes' : 
+                (status === '0' ? 'No' : '')
+                $('td:eq(7)', nRow).html(status)
+
+		    }
+	    } );
+	} );
+</script>
+
+<?php $this->view('partials/foot'); ?>
