02604b367fad0952afc567faf104101d7594f715
Arjen van Bochoven <arjen@bochoven.net>
2016-10-26 19:56:06 +0200

    Add JSON data method to sccm controller
    Used to get JSON data via index.php?/module/sccm_status/get_data/SERIALNUMBER

--- app/modules/sccm_status/sccm_status_controller.php
+++ app/modules/sccm_status/sccm_status_controller.php
@@ -25,8 +25,23 @@ class sccm_status_controller extends Module_controller
         {
                 echo "You've loaded the sccm_status module!";
         }
-
         
+        /**
+         * Retrieve data in json format
+         *
+         **/
+        public function get_data($serial_number = '')
+        {
+            $obj = new View();
+
+            if (! $this->authorized()) {
+                $obj->view('json', array('msg' => 'Not authorized'));
+            }
+
+            $sccm_status_model = new sccm_status_model($serial_number);
+            $obj->view('json', array('msg' => $sccm_status_model->rs));
+        }
 } // END class default_module
+
 
 
