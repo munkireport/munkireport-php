f65eddbc6bcf7de7dd6de5d59a1e08ddcb007457
computeronix <computeronix@users.noreply.github.com>
2016-10-26 12:25:05 -0400

    Update sccm_status_tab.php
    added locales (en-us)

--- app/views/client/sccm_status_tab.php
+++ app/views/client/sccm_status_tab.php
@@ -1,42 +1,38 @@
 <?php //Initialize models needed for the table
 $sccm_status = "";
 ?>
-	<h2>SCCM_Agent_Status</h2>
+	<h2 data-i18n="sccm_status.title"></h2>
 
 		<table class="table table-striped">
 			<tbody>
 			<thead>
 				<tr>
-					<th>SCCM Agent Information:</th>
+					<th data-i18n="sccm_status.titleheader"></th>
 					<th>Value</th>
 				</tr>
 			</thead>
 				<tr>
-					<td>Enrollment Status</td>
+					<td data-i18n="sccm_status.enrollment_status"></td>
 					<td><?=$sccm_status->agent_status?></td>
 				</tr>
 				<tr>
-					<td>Management Point</td>
+					<td data-i18n="sccm_status.mgmt_point"></td>
 					<td><?=$sccm_status->mgmt_point?></td>
 				</tr>
 				<tr>
-					<td>Enrollment User Name</td>
+					<td data-i18n="sccm_status.enrollment_name"></td>
 					<td><?=$sccm_status->enrollment_name?></td>
 				</tr>
 				<tr>
-					<td>Last Agent Checkin</td>
+					<td data-i18n="sccm_status.last_checkin"></td>
 					<td><?=$sccm_status->last_checkin?></td>
 				</tr>
 				<tr>
-					<td>Client Cert Expiry</td>
+					<td data-i18n="sccm_status.cert_exp"></td>
 					<td><?=$sccm_status->cert_exp?></td>
 				</tr>
 				<tr>
-					<td>Enrollment User Name</td>
-					<td><?=$sccm_status->enrollment_name?></td>
-				</tr>
-				<tr>
-					<td>Enrollment Server</td>
+					<td data-i18n="sccm_status.enrollment_server"></td>
 					<td><?=$sccm_status->enrollment_server?></td>
 				</tr>
 			</tbody>
