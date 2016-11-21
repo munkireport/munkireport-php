737f8dd8f3829d3649faeb306592a9ba039b9252
Arjen van Bochoven <arjen@bochoven.net>
2016-11-21 20:19:53 +0100

    Fix SCCM en locale

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.11.0.2285';
+$GLOBALS['version'] = '2.11.0.2286';
 
 // Return version without commit count
 function get_version()
