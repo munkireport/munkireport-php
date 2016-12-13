2415a30346c5515e6c2a5b9b2030169814f9fcd6
Arjen van Bochoven <arjen@bochoven.net>
2016-12-13 15:47:07 +0100

    Fix h2 color

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.12.0.2327';
+$GLOBALS['version'] = '2.12.0.2328';
 
 // Return version without commit count
 function get_version()
