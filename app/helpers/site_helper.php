b5eed646192532d1c24f1a045114a23af80941cc
Arjen van Bochoven <arjen@bochoven.net>
2016-12-13 21:11:50 +0100

    Move glyph icons to assets/themes

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.12.0.2331';
+$GLOBALS['version'] = '2.12.0.2332';
 
 // Return version without commit count
 function get_version()
