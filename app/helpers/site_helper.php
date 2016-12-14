426cd23f2c6925fdba8f97489f49dbb4492849c0
Arjen van Bochoven <arjen@bochoven.net>
2016-12-14 20:03:06 +0100

    Fix foreground color in client default view

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.12.0.2337';
+$GLOBALS['version'] = '2.12.0.2338';
 
 // Return version without commit count
 function get_version()
