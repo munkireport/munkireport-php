91a5dd64d354c36e8432cc8961ae726e35e05ffb
Arjen van Bochoven <arjen@bochoven.net>
2016-12-18 22:44:06 +0100

    Moved widgets into module directories

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.13.0.2344';
+$GLOBALS['version'] = '2.13.0.2345';
 
 // Return version without commit count
 function get_version()
