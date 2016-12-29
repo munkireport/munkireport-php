c3a2a2c8793f07d7692bd50c35ddd0b8088c17b7
Rick Heil <rickheil@partnersandsimons.com>
2016-12-29 09:19:14 -0500

    remove old versions of the script on install

--- app/modules/security/scripts/install.sh
+++ app/modules/security/scripts/install.sh
@@ -6,6 +6,16 @@ MODULE_CACHE_FILE="security.plist"
 
 CTL="${BASEURL}index.php?/module/${MODULE_NAME}/"
 
+# Remove old versions of the script and cache file
+if [ -f "${MUNKIPATH}preflight.d/security.sh" ]; then
+	rm "${MUNKIPATH}preflight.d/security.sh"
+fi
+
+if [ -f "${MUNKIPATH}preflight.d/cache/security.txt" ]; then
+	rm "${MUNKIPATH}preflight.d/cache/security.txt"
+fi
+
+
 # Get the scripts in the proper directories
 "${CURL[@]}" "${CTL}get_script/${MODULESCRIPT}" -o "${MUNKIPATH}preflight.d/${MODULESCRIPT}"
 
