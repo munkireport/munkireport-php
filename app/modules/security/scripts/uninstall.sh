5c27ca60dd224d0ee997595066dbfa65bf6f1950
Rick Heil <rickheil@partnersandsimons.com>
2016-12-29 09:21:27 -0500

    uninstall old versions of security scripts too

--- app/modules/security/scripts/uninstall.sh
+++ app/modules/security/scripts/uninstall.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Remove current version of files
 rm -f "${MUNKIPATH}preflight.d/security.py"
 rm -f "${CACHEPATH}security.plist"
 
+# Remove older version of files
+rm -f "${MUNKIPATH}preflight.d/security.sh"
+rm -f "${CACHEPATH}security.txt"
