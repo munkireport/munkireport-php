58f8bd6a9cf2ac898c04a21bb3fb4de27e2d6e61
Arjen van Bochoven <arjen@bochoven.net>
2017-01-14 23:41:49 +0100

    Upgrade moment.js to 2.17.1

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.13.0.2362';
+$GLOBALS['version'] = '2.13.0.2363';
 
 // Return version without commit count
 function get_version()
