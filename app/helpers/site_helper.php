1f6501142fde69876247e2239cabb3ca6e63903b
Arjen van Bochoven <arjen@bochoven.net>
2017-01-14 23:17:45 +0100

    Bump version

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.12.0.2361';
+$GLOBALS['version'] = '2.13.0.2362';
 
 // Return version without commit count
 function get_version()
