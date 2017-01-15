f817ec6fa6268e2af2bb081faefc01762d38b9df
Arjen van Bochoven <arjen@bochoven.net>
2017-01-15 21:57:43 +0100

    Fix locales for security listing

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.13.0.2367';
+$GLOBALS['version'] = '2.13.0.2368';
 
 // Return version without commit count
 function get_version()
--- app/views/listing/security.php
+++ app/views/listing/security.php
@@ -25,12 +25,12 @@ new Security_model;
 		        <th data-i18n="user.local_admins" data-colname='localadmin.users'></th>
 		        <th data-i18n="filevault.users" data-colname='filevault_status.filevault_users'></th>
 		        <th data-i18n="type"data-colname='machine.machine_name'></th>
-	                <th data-i18n="storage.encryption_status" data-colname='diskreport.CoreStorageEncrypted'></th>
-        	        <th data-i18n="security.gatekeeper" data-colname='security.gatekeeper'></th>
-			<th data-i18n="security.sip" data-colname='security.sip'></th>
-			<th data-i18n="security.ssh_users" data-colname='security.ssh_users'></th>
-			<th data-i18n="security.ard_users" data-colname='security.ard_users'></th>
-			<th data-i18n="security.firmwarepw" data-colname='security.firmwarepw'></th>
+                <th data-i18n="storage.encryption_status" data-colname='diskreport.CoreStorageEncrypted'></th>
+    	        <th data-i18n="security.gatekeeper" data-colname='security.gatekeeper'></th>
+    			<th data-i18n="security.sip" data-colname='security.sip'></th>
+    			<th data-i18n="security.ssh_users" data-colname='security.ssh_users'></th>
+    			<th data-i18n="security.ard_users" data-colname='security.ard_users'></th>
+    			<th data-i18n="security.firmwarepw" data-colname='security.firmwarepw'></th>
 		      </tr>
 		    </thead>
 		    <tbody>
@@ -127,23 +127,23 @@ new Security_model;
                 var gk = $('td:eq(7)', nRow).html();
                 $('td:eq(7)', nRow).html(function(){
                     if( gk == 'Active'){
-                        return '<span class="label label-success">'+i18n.t('Enabled')+'</span>';
+                        return '<span class="label label-success">'+i18n.t('enabled')+'</span>';
                     }
-		    return '<span class="label label-danger">'+i18n.t('Disabled')+'</span>';
+		    return '<span class="label label-danger">'+i18n.t('disabled')+'</span>';
 		});
 		var sip = $('td:eq(8)', nRow).html();
                 $('td:eq(8)', nRow).html(function(){
                     if( sip == 'Active'){
-                        return '<span class="label label-success">'+i18n.t('Enabled')+'</span>';
+                        return '<span class="label label-success">'+i18n.t('enabled')+'</span>';
                     }
-                    return '<span class="label label-danger">'+i18n.t('Disabled')+'</span>';
+                    return '<span class="label label-danger">'+i18n.t('disabled')+'</span>';
 		});
                  var firmwarepw = $('td:eq(11)', nRow).html();
                  $('td:eq(11)', nRow).html(function(){
                      if( firmwarepw == 'Yes'){
-                         return '<span class="label label-success">'+i18n.t('Enabled')+'</span>';
+                         return '<span class="label label-success">'+i18n.t('enabled')+'</span>';
                      }
-                     return '<span class="label label-danger">'+i18n.t('Disabled')+'</span>';
+                     return '<span class="label label-danger">'+i18n.t('disabled')+'</span>';
                  });
 		    }
 	    } );
