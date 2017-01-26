4b1e3685255c399c4f35183b930a00a03cc6e5c4
Arjen van Bochoven <arjen@bochoven.net>
2017-01-26 20:33:56 +0100

    Add checkin to managed installs listing

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.13.0.2368';
+$GLOBALS['version'] = '2.13.0.2384';
 
 // Return version without commit count
 function get_version()
