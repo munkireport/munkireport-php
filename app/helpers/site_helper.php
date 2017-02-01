b2b0dc990855bc593e378fd0a8a16f4c977ac1cb
Arjen van Bochoven <arjen@bochoven.net>
2017-02-01 14:15:14 +0100

    Add theme class

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.13.0.2427';
+$GLOBALS['version'] = '2.13.0.2428';
 
 // Return version without commit count
 function get_version()
