71a492cade3d111f39c8ec46c39b71390ee6221c
tuxudo <tuxudo@users.noreply.github.com>
2017-02-08 17:12:13 -0500

    Caching Module (#578)
    * Add caching module
    
    Created caching module and widget
    
    * Fix unauthenticated controller
    
    Allow scripts to download
    
    * Renamed script and added install checks
    
    Will only install if caching metrics database exists
    
    * Add serial_number to indexes
    
    * Add caching report listing
    
    * Widget math now done on client
    
    * Changed JSON output for graph widget
    
    Not able to get graphing widget working :(

---
+++ app/modules/caching/views/caching_widget.php
@@ -0,0 +1,33 @@
+<div class="col-lg-4 col-md-6">
+	<div class="panel panel-default" id="caching-widget">
+		<div class="panel-heading" data-container="body" data-i18n="[title]widget.caching.tooltip">
+			<h3 class="panel-title"><i class="fa fa-database"></i> <span data-i18n="widget.caching.title"></h3>
+		</div>
+		<div class="panel-body text-center"></div>
+	</div><!-- /panel -->
+</div><!-- /col -->
+
+<script>
+$(document).on('appUpdate', function(e, lang) {
+
+    $.getJSON( appUrl + '/module/caching/caching_widget', function( data ) {
+
+    	if(data.error){
+    		//alert(data.error);
+    		return;
+    	}
+		
+		var panel = $('#caching-widget div.panel-body'),
+		baseUrl = appUrl + '/show/listing/clients';
+		panel.empty();
+		
+		// Set statuses
+		if(data.fromorigin){
+			panel.append(' <a href="'+baseUrl+'" class="btn btn-danger"><span class="bigger-150">'+fileSize(data.fromorigin, 2)+'</span><br>&nbsp;&nbsp;'+i18n.t('caching.from_origin')+'&nbsp;&nbsp;</a>');
+		}
+		if(data.fromcache){
+			panel.append(' <a href="'+baseUrl+'" class="btn btn-success"><span class="bigger-150">'+fileSize(data.fromcache, 2)+'</span><br>&nbsp;&nbsp;'+i18n.t('caching.from_cache')+'&nbsp;&nbsp;</a>');
+		}
+    });
+});
+</script>
