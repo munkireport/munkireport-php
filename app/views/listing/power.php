d8224266dd710bda504368af7fee50fb148c596a
tuxudo <tuxudo@users.noreply.github.com>
2017-02-08 16:21:31 -0500

    Fixed listings "Loading data from server" width (#592)
    * Adjusted loading columns amount...
    
    ...fixed whitespace and localized titles
    
    * Fixed whitespace

--- app/views/listing/power.php
+++ app/views/listing/power.php
@@ -13,7 +13,7 @@ new Power_model;
 
   	<div class="col-lg-12">
 
-		  <h3>Power report <span id="total-count" class='label label-primary'>…</span></h3>
+		  <h3><span data-i18n="nav.reports.power"></span> <span id="total-count" class='label label-primary'>…</span></h3>
 
 		  <table class="table table-striped table-condensed table-bordered">
 		    <thead>
--- app/views/listing/sccm_status.php
+++ app/views/listing/sccm_status.php
@@ -12,7 +12,7 @@ new Sccm_status_model;
 
   	<div class="col-lg-12">
 
-		  <h3>SCCM Agent Status report <span id="total-count" class='label label-primary'>…</span></h3>
+		  <h3><span data-i18n="nav.reports.sccm"></span> <span id="total-count" class='label label-primary'>…</span></h3>
 
 		  <table class="table table-striped table-condensed table-bordered">
 		    <thead>
