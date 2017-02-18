3ebb74b136be2dd7e4f419820501f766ddc7f596
tuxudo <John@Thunderchild.local>
2017-02-17 19:24:08 -0500

    Resented version

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.13.0.2474';
+$GLOBALS['version'] = '2.13.0.2477';
 
 // Return version without commit count
 function get_version()
