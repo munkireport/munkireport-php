d6a42e3cfa3e4eab462e016d401cfb08d946bef1
tuxudo <John@Thunderchild.local>
2017-02-22 12:43:06 -0500

    First commit of smart_stats module

---
+++ app/modules/smart_stats/README.md
@@ -0,0 +1,37 @@
+SMART Stats module
+================
+
+
+The following information is stored in the smart_stats table:
+
+
+* disk_number - int - device id (/dev/diskX)
+* model_family - varchar(255) - Family model of the disk
+* device_model - varchar(255) - Device model of the disk
+* serial\_number_hdd - varchar(255) - Disk serial number
+* lu\_wwn\_device_id - varchar(255)
+* firmware_version - varchar(255) - Firmware version of disk
+* user_capacity - varchar(255) - Raw capacity of disk
+* sector_size - varchar(255) - Reported sector size of disk
+* rotation_rate - varchar(255) - Rotation rate of disk
+* device_is - varchar(255) - Status of disk in smartctl's database
+* ata\_version_is - varchar(255) - Information about ATA protocol in use
+* sata\_version_is - varchar(255) - Information about SATA protocol in use
+* form_factor - varchar(255) - Form factor of drive
+* smart\_support_is - varchar(255) - SMART support about drive
+* smart_is - varchar(255) - Is SMART enabled
+* error_count - int - Amount of SMART errors
+* error_poh - int - Power on hour count at last SMART error
+* timestamp - int - Timestamp of last data pull
+
+All other table columns correspond with SMART attributes.  
+
+##Dependency
+
+smart_stats module required smartctl, a part of smartmontools: [https://www.smartmontools.org/]()
+
+Download the latest version of smartmontools for macOS here: [https://sourceforge.net/projects/smartmontools/files/latest/download?source=files]()
+ 
+##Notes
+
+Because smartmontools does not yet fully support NVMe drives on macOS, this module does not support NVMe drives.
---
+++ app/views/client/smart_stats_tab.php
@@ -0,0 +1,61 @@
+
+<div id="smart_stats"></div>
+<h2 data-i18n="smart_stats.clienttabtitle"></h2>
+
+
+<script>
+$(document).on('appReady', function(){
+	$.getJSON(appUrl + '/module/smart_stats/get_client_tab_data/' + serialNumber, function(data){
+		var skipThese = ['id','serial_number','disk_number','temperature_unit'];
+		$.each(data, function(i,d){
+			
+			// Generate rows from data
+			var rows = ''
+			for (var prop in d){
+				// Skip skipThese
+				if(skipThese.indexOf(prop) == -1){
+                    if (d[prop] == -9876540){
+					   // Do nothing for the fake nulls to blank them
+                    } else if (d[prop] == "" && d[prop] != "0"){
+					   // Do nothing for the nulls to blank them
+                    } else if (d[prop] == "Enabled"){ // Localize enabled
+					   rows = rows + '<tr><th>'+i18n.t('smart_stats.'+prop)+'</th><td>'+i18n.t('yes')+'</td></tr>';
+                    } else if (d[prop] == "Disabled"){ // Localize disabled
+					   rows = rows + '<tr><th>'+i18n.t('smart_stats.'+prop)+'</th><td>'+i18n.t('no')+'</td></tr>';
+                    } else if (d[prop] == "In smartctl database [for details use: -P show]"){ // Localize if drive is in database
+					   rows = rows + '<tr><th>'+i18n.t('smart_stats.'+prop)+'</th><td>'+i18n.t('yes')+'</td></tr>';
+                    } else if (d[prop] == "Not in smartctl database [for details use: -P showall]"){ // Localize if drive is not in database
+					   rows = rows + '<tr><th>'+i18n.t('smart_stats.'+prop)+'</th><td>'+i18n.t('no')+'</td></tr>';
+                    } else if (prop == "error_poh" || prop == "error_count"){ // Formate SMART Errors
+					   rows = rows + '<tr><th>'+i18n.t('smart_stats.'+prop)+'</th><td class="danger">'+d[prop]+'</td></tr>';
+                    } else if (prop == "timestamp"){ // Format timestamp
+					   var timestamp = (d[prop] * 1000)
+					   rows = rows + '<tr><th>'+i18n.t('smart_stats.'+prop)+'</th><td>'+moment(timestamp).format("YYYY-MM-DD H:mm:ss")+'</td></tr>';
+                    } else if (prop == "airflow_temperature_cel" || prop == "temperature_celsius"){ // Formate temperatures
+					   temperature_f = parseFloat(((d[prop] * 9/5 ) + 32 ).toFixed(2));
+					   if (d['temperature_unit'] == "F"){
+					        rows = rows + '<tr><th>'+i18n.t('smart_stats.'+prop)+'</th><td><span title="'+d[prop]+'°C">'+temperature_f+'°F</span></td></tr>';
+					   } else {
+					        rows = rows + '<tr><th>'+i18n.t('smart_stats.'+prop)+'</th><td><span title="'+temperature_f+'°F">'+d[prop]+'°C</span></td></tr>';
+					   }
+                    } else {
+                        rows = rows + '<tr><th>'+i18n.t('smart_stats.'+prop)+'</th><td>'+d[prop]+'</td></tr>';
+                    }
+				}
+			}
+            
+			$('#smart_stats-tab')
+				.append($('<h4>')
+					.append($('<i>')
+						.addClass('fa fa-hdd-o '))
+					.append(' /dev/disk'+d.disk_number))
+				.append($('<div style="max-width:650px;">')
+					.addClass('table-responsive')
+					.append($('<table>')
+						.addClass('table table-striped table-condensed')
+						.append($('<tbody>')
+							.append(rows))))
+		})
+	});
+});
+</script>
