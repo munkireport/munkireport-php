fe59b253c325376f420f1c73d96bf099b57a9acc
tuxudo <John@Thunderchild.local>
2017-03-02 16:29:39 -0500

    Move listing to new location

--- app/modules/fonts/views/fonts_widget.php
+++ app/modules/fonts/views/fonts_widget.php
@@ -18,7 +18,7 @@ $(document).on('appUpdate', function(e, lang) {
 		if(data.length){
 			$.each(data, function(i,d){
 				var badge = '<span class="badge pull-right">'+d.count+'</span>';
-                box.append('<a href="'+appUrl+'/show/listing/fonts/#'+d.type_name+'" class="list-group-item">'+d.type_name+badge+'</a>')
+                box.append('<a href="'+appUrl+'/show/listing/fonts/fonts/#'+d.type_name+'" class="list-group-item">'+d.type_name+badge+'</a>')
 			});
 		}
 		else{
