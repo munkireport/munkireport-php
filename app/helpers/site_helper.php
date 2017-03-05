6ea1eed3b8c54b0578c67059382c7572bd2b3a5f
Arjen van Bochoven <arjen@bochoven.net>
2017-03-05 21:33:34 +0100

    Add custom to gitignore

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.13.0.2507';
+$GLOBALS['version'] = '2.13.0.2521';
 
 // Return version without commit count
 function get_version()
