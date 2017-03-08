2e6e8229f6130af974dcd54a6fea7abb3f74d275
Arjen van Bochoven <arjen@bochoven.net>
2017-03-08 14:58:21 +0100

    Fix handling of empty csv

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.13.0.2524';
+$GLOBALS['version'] = '2.13.0.2525';
 
 // Return version without commit count
 function get_version()
