2d76fec99380b2e5fa76de46d68f5028e459757f
tuxudo <John@Thunderchild.local>
2017-03-08 10:19:08 -0500

    Time Machine 10.12 fix
    Fixed Time Machine module, but it is disabled because it always times
    out :(

--- app/modules/timemachine/scripts/timemachine.sh
+++ app/modules/timemachine/scripts/timemachine.sh
@@ -10,5 +10,18 @@ DIR=$(dirname $0)
 
 mkdir -p "$DIR/cache"
 
+# OS check
+OSVERSION=$(/usr/bin/sw_vers -productVersion | /usr/bin/cut -d . -f 2)
+
+if [[ ${OSVERSION} -lt 12 ]]; then
+
 # Store 7 days of relevant syslog events
 syslog -F '$((Time)(utc)) $Message' -k Sender com.apple.backupd -k Time ge -7d -k Message R '^(Backup|Starting).*' > "$DIR/cache/timemachine.txt" 
+
+else
+# 10.12+
+#log show --last 7d --predicate 'subsystem == "com.apple.TimeMachine"' --info | grep 'upd: (' | cut -c 1-19,140-999
+# Command is disabled because it stresses system and times out
+# Mostly because Apple's new fancy log system is not very good
+
+fi
