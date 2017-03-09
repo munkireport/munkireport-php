7ccb62164f5f102a8b62bd632d3cbf2a8a5918c1
Arjen van Bochoven <arjen@bochoven.net>
2017-03-10 00:12:31 +0100

    Update provides.php

--- app/modules/fonts/provides.php
+++ app/modules/fonts/provides.php
@@ -2,10 +2,10 @@
 
 return array(
     'client_tabs' => array(
-        'fonts-tab' => array('view' => 'fonts_tab', 'i18n' => 'fonts.clienttitle', 'badge' => 'fonts-cnt'),
+        'fonts-tab' => array('view' => 'fonts_tab', 'i18n' => 'fonts.clienttab', 'badge' => 'fonts-cnt'),
     ),
     'listings' => array(
-        'fonts' => array('view' => 'fonts_listing'),
+        'fonts' => array('view' => 'fonts_listing', 'i18n' => 'fonts.clienttab'),
     ),
     'widgets' => array(
        array('view' => 'fonts_widget'),
