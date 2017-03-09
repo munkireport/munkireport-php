67873a17784f122adeedaeaa2483864ea8bdfca9
Arjen van Bochoven <arjen@bochoven.net>
2017-03-09 23:38:02 +0100

    Update module template

--- build/addmodule.sh
+++ build/addmodule.sh
@@ -4,13 +4,19 @@
 
 DIR=$(dirname $0)
 MODULE="${1}"
-MODULE_PATH="${DIR}/../app/modules/${MODULE}/"
+MODULE_PATH=$(pwd)"/modules/${MODULE}/"
 SCRIPT_DIR="${MODULE_PATH}/scripts/"
+VIEW_DIR="${MODULE_PATH}/views/"
+LOCALE_DIR="${MODULE_PATH}/locales/"
 
 # Create directories
 mkdir "${MODULE_PATH}" || exit 1
 mkdir "${SCRIPT_DIR}" || exit 1
-mkdir "${MODULE_PATH}views" || exit 1
+mkdir "${VIEW_DIR}" || exit 1
+mkdir "${LOCALE_DIR}" || exit 1
+
+# Copy provides.php
+cat "${DIR}/templates/provides.php" | sed "s/MODULE/${MODULE}/g" > "${MODULE_PATH}provides.php"
 
 # Copy install scripts from template
 cat "${DIR}/templates/install.sh" | sed "s/MODULE/${MODULE}/g" > "${SCRIPT_DIR}install.sh"
@@ -20,6 +26,14 @@ cat "${DIR}/templates/module_script.sh" | sed "s/MODULE/${MODULE}/g" > "${SCRIPT
 # Copy php templates
 cat "${DIR}/templates/controller.php" | sed "s/MODULE/${MODULE}/g" > "${MODULE_PATH}${MODULE}_controller.php"
 cat "${DIR}/templates/model.php" | sed "s/MODULE/${MODULE}/g" > "${MODULE_PATH}${MODULE}_model.php"
+
+# Copy views
+cat "${DIR}/templates/listing.php" | sed "s/MODULE/${MODULE}/g" > "${VIEW_DIR}${MODULE}_listing.php"
+cat "${DIR}/templates/report.php" | sed "s/MODULE/${MODULE}/g" > "${VIEW_DIR}${MODULE}_report.php"
+cat "${DIR}/templates/widget.php" | sed "s/MODULE/${MODULE}/g" > "${VIEW_DIR}${MODULE}_widget.php"
+
+# Copy locale template
+cat "${DIR}/templates/en.json" | sed "s/MODULE/${MODULE}/g" > "${LOCALE_DIR}en.json"
 
 echo "** Created module ${MODULE} **"
 
