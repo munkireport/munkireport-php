fe5c5ea9985928e805909ecec6f091f41493ab13
tuxudo <John@Thunderchild.local>
2017-03-09 19:49:12 -0500

    Moved localizations to module

--- app/modules/smart_stats/provides.php
+++ app/modules/smart_stats/provides.php
@@ -5,6 +5,6 @@ return array(
         'smart_stats-tab' => array('view' => 'smart_stats_tab', 'i18n' => 'smart_stats.clienttabtitle'),
     ),
     'listings' => array(
-        'smart_stats' => array('view' => 'smart_stats_listing'),
+        'smart_stats' => array('view' => 'smart_stats_listing', 'i18n' => 'smart_stats.clienttabtitle'),
     ),
 );
