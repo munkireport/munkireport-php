68d8aa1487142aa3b65541666c0b63e48466699f
Arjen van Bochoven <arjen@bochoven.net>
2017-03-10 15:34:12 +0100

    Move bluetooth locales to module

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.13.0.2541';
+$GLOBALS['version'] = '2.13.0.2542';
 
 // Return version without commit count
 function get_version()
