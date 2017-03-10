d3197be35891c4cb46e8dfbd9ee3bd77be60c613
Arjen van Bochoven <arjen@bochoven.net>
2017-03-10 09:19:59 +0100

    Add dutch localisation to font module

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.13.0.2532';
+$GLOBALS['version'] = '2.13.0.2534';
 
 // Return version without commit count
 function get_version()
