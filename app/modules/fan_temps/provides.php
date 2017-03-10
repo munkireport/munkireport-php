d46f768955e918a5c63e74c620fbab3b6fad57b4
tuxudo <John@Thunderchild.local>
2017-03-09 19:51:01 -0500

    Moved localizations to module

--- app/modules/fan_temps/provides.php
+++ app/modules/fan_temps/provides.php
@@ -5,6 +5,6 @@ return array(
         'fan_temps-tab' => array('view' => 'fan_temps_tab', 'i18n' => 'fans.tabtitle'),
     ),
     'listings' => array(
-        'fan_temps' => array('view' => 'fan_temps_listing'),
+        'fan_temps' => array('view' => 'fan_temps_listing', 'i18n' => 'fans.tabtitle'),
     ),
 );
