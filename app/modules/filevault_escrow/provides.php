04db2297028cdf6ec0c250674023d913f8f06b74
Arjen van Bochoven <arjen@bochoven.net>
2017-03-11 23:25:02 +0100

    Filevault escrow locale

--- app/modules/filevault_escrow/provides.php
+++ app/modules/filevault_escrow/provides.php
@@ -2,6 +2,6 @@
 
 return array(
     'client_tabs' => array(
-        'filevault-tab' => array('view' => 'filevault_tab', 'i18n' => 'client.tab.fv_escrow'),
+        'filevault-tab' => array('view' => 'filevault_tab', 'i18n' => 'filevault_escrow.fv_escrow'),
     ),
 );
