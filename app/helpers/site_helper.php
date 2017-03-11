da9ce409fda34c0ccea87cb49b36ea89dfdda9e8
Arjen van Bochoven <arjen@bochoven.net>
2017-03-12 00:40:50 +0100

    Installhistory locale move

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.13.0.2558';
+$GLOBALS['version'] = '2.13.0.2559';
 
 // Return version without commit count
 function get_version()
