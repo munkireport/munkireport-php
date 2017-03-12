92bbdeae8c1e91ea7b7d8e1414829e63ea5076ff
Arjen van Bochoven <arjen@bochoven.net>
2017-03-12 09:20:23 +0100

    Wifi locale move

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.13.0.2562';
+$GLOBALS['version'] = '2.13.0.2563';
 
 // Return version without commit count
 function get_version()
