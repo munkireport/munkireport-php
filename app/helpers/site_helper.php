57b00e5e319b74b66e8c02ca70dea184dddaa01c
Arjen van Bochoven <arjen@bochoven.net>
2017-03-12 10:56:02 +0100

    Prettify and sort json

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.13.0.2566';
+$GLOBALS['version'] = '2.13.0.2567';
 
 // Return version without commit count
 function get_version()
