6afcece6c67061221fc81199e87a73bf2fc5ed59
Arjen van Bochoven <arjen@bochoven.net>
2017-03-12 09:42:27 +0100

    USB locale move

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.13.0.2564';
+$GLOBALS['version'] = '2.13.0.2565';
 
 // Return version without commit count
 function get_version()
--- app/modules/usb/provides.php
+++ app/modules/usb/provides.php
@@ -2,16 +2,16 @@
 
 return array(
     'client_tabs' => array(
-        'usb-tab' => array('view' => 'usb_tab', 'i18n' => 'nav.listings.usb', 'badge' => 'usb-cnt'),
+        'usb-tab' => array('view' => 'usb_tab', 'i18n' => 'usb.devices', 'badge' => 'usb-cnt'),
     ),
     'listings' => array(
-        'usb' => array('view' => 'usb_listing'),
+        'usb' => array('view' => 'usb_listing', 'i18n' => 'usb.devices'),
     ),
     'widgets' => array(
         array('view' => 'usb_devices_widget'),
         array('view' => 'usb_types_widget'),
     ),
     'reports' => array(
-        'peripheral' => array('view' => 'peripheral'),
+        'peripheral' => array('view' => 'usb_report', 'i18n' => 'usb_report'),
     ),
 );
