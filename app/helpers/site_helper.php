f014d75d79499bf40df7412ceb90eb5c6d432596
Arjen van Bochoven <arjen@bochoven.net>
2017-03-12 14:47:00 +0100

    Add empty provides to filevault_status

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.13.0.2573';
+$GLOBALS['version'] = '2.13.0.2574';
 
 // Return version without commit count
 function get_version()
