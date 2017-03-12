7d0d3fb39953c9c3e1f9107b151d8bab96655c6b
Arjen van Bochoven <arjen@bochoven.net>
2017-03-12 23:49:22 +0100

    Managed installs move locale

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.13.0.2582';
+$GLOBALS['version'] = '2.13.0.2583';
 
 // Return version without commit count
 function get_version()
--- app/modules/managedinstalls/views/pending_widget.php
+++ app/modules/managedinstalls/views/pending_widget.php
@@ -1,7 +1,7 @@
 <div class="col-lg-4 col-md-6">
     <div class="panel panel-default" id="pending-widget">
         <div class="panel-heading">
-            <h3 class="panel-title"><i class="fa fa-moon"></i> <span data-i18n="widget.pending.title"></span></h3>
+            <h3 class="panel-title"><i class="fa fa-moon"></i> <span data-i18n="managedinstalls.widget.pending.title"></span></h3>
         </div>
         <div class="list-group scroll-box"></div>
     </div><!-- /panel -->
@@ -10,19 +10,19 @@
 <script>
 
 $(document).on('appUpdate', function(e, lang) {
-	
+
 	var box = $('#pending-widget div.scroll-box'),
         hours = 24; // Hours back
-	
+
 	$.getJSON( appUrl + '/module/managedinstalls/get_clients/pending_install/'+hours, function( data ) {
-		
+
 		box.empty();
 
 		if(data.length){
 			$.each(data, function(i,d){
 				var badge = '<span class="badge pull-right">'+d.count+'</span>',
                     url = appUrl+'/clients/detail/'+d.serial_number+'#tab_munki';
-                
+
                 d.computer_name = d.computer_name || i18n.t('empty');
 				box.append('<a href="'+url+'" class="list-group-item">'+d.computer_name+badge+'</a>');
 			});
@@ -31,5 +31,5 @@ $(document).on('appUpdate', function(e, lang) {
 			box.append('<span class="list-group-item">'+i18n.t('no_clients')+'</span>');
 		}
 	});
-});	
+});
 </script>
