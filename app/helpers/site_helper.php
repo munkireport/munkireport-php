886e880a4875f29079368584cc8947714d88ec3f
Arjen van Bochoven <arjen@bochoven.net>
2017-03-13 00:24:29 +0100

    ARD locale fix

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.13.0.2586';
+$GLOBALS['version'] = '2.13.0.2587';
 
 // Return version without commit count
 function get_version()
--- app/modules/ard/provides.php
+++ app/modules/ard/provides.php
@@ -2,6 +2,6 @@
 
 return array(
     'listings' => array(
-        'ard' => array('view' => 'ard_listing'),
+        'ard' => array('view' => 'ard_listing', 'i18n' => 'ard.ard'),
     ),
 );
