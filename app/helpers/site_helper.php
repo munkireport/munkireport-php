bf3ed39d79d5cab1deb8a5abf00715fca35f08fc
Arjen van Bochoven <arjen@bochoven.net>
2017-03-13 17:27:12 +0100

    Move new clients to machine locale

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.13.0.2592';
+$GLOBALS['version'] = '2.13.0.2595';
 
 // Return version without commit count
 function get_version()
