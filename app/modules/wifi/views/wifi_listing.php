b0cd6d4d8ac4f9af6834ad0f21880b91c06feea9
tuxudo <John@Thunderchild.local>
2017-03-14 10:41:13 -0400

    Added signal noise ratio and localized data

--- app/modules/wifi/views/wifi_listing.php
+++ app/modules/wifi/views/wifi_listing.php
@@ -25,6 +25,7 @@ new wifi_model;
 		      	<th data-i18n="wifi.listing.lasttx" data-colname='wifi.lasttxrate'></th>
 		      	<th data-i18n="wifi.listing.maxtx" data-colname='wifi.maxrate'></th>
 		      	<th data-i18n="wifi.listing.channel" data-colname='wifi.channel'></th>
+		      	<th data-colname='wifi.id'>SNR</th>
 		      	<th data-colname='wifi.agrctlrssi'>RSSI</th>
 		      	<th data-i18n="wifi.listing.noise" data-colname='wifi.agrctlnoise'></th>
 		      	<th data-i18n="wifi.listing.authtype" data-colname='wifi.x802_11_auth'></th>
@@ -35,7 +36,7 @@ new wifi_model;
 		    </thead>
 		    <tbody>
 		    	<tr>
-					<td data-i18n="listing.loading" colspan="14" class="dataTables_empty"></td>
+					<td data-i18n="listing.loading" colspan="15" class="dataTables_empty"></td>
 				</tr>
 		    </tbody>
 		  </table>
@@ -74,8 +75,79 @@ new wifi_model;
 	        	var name=$('td:eq(0)', nRow).html();
 	        	if(name == ''){name = "No Name"};
 	        	var sn=$('td:eq(1)', nRow).html();
-	        	var link = mr.getClientDetailLink(name, sn);
+	        	var link = mr.getClientDetailLink(name, sn, '#tab_wifi-tab');
 	        	$('td:eq(0)', nRow).html(link);
+                
+                // Format Last Tx
+	        	var lastTx=$('td:eq(5)', nRow).html();
+	        	$('td:eq(5)', nRow).html('<span title="'+(lastTx*0.125)+' MB/sec">'+lastTx+" Mbps</span>");
+                
+                // Format Max Tx
+	        	var maxTx=$('td:eq(6)', nRow).html();
+	        	$('td:eq(6)', nRow).html('<span title="'+(maxTx*0.125)+' MB/sec">'+maxTx+" Mbps</span>");
+                
+	        	// Calculate signal to noise ratio
+	        	var rssi=$('td:eq(9)', nRow).html();
+	        	var noise=$('td:eq(10)', nRow).html();
+	        	$('td:eq(8)', nRow).html('<span title="'+i18n.t('wifi.snr_detail')+'">'+(rssi-noise)+' db</span>');
+                                
+                // Format RSSI
+	        	var rssi=$('td:eq(9)', nRow).html();
+	        	$('td:eq(9)', nRow).html('<span title="'+i18n.t('wifi.rssi_detail')+'">'+rssi+' db</span>');
+                                
+                // Format Noise
+	        	var noise=$('td:eq(10)', nRow).html();
+	        	$('td:eq(10)', nRow).html('<span title="'+i18n.t('wifi.noise_detail')+'">'+noise+' db</span>');
+                
+	        	// Format 802.1x mode
+	        	var eightmode=$('td:eq(11)', nRow).html();
+	        	eightmode = eightmode == 'open' ? i18n.t('wifi.open') :
+	        		(eightmode)
+	        	$('td:eq(11)', nRow).html(eightmode)
+                
+	        	// Format Link Auth
+	        	var linkauth=$('td:eq(12)', nRow).html();
+	        	linkauth = linkauth == 'none' ? i18n.t('wifi.none') :
+	        	linkauth = linkauth == '802.1x' ? i18n.t('wifi.802.1x') :
+	        	linkauth = linkauth == 'leap' ? i18n.t('wifi.leap') :
+	        	linkauth = linkauth == 'wps' ? i18n.t('wifi.wps') :
+	        	linkauth = linkauth == 'wep' ? i18n.t('wifi.wep') :
+	        	linkauth = linkauth == 'wpa' ? i18n.t('wifi.wpa') :
+	        	linkauth = linkauth == 'wpa-psk' ? i18n.t('wifi.wpa_psk') :
+	        	linkauth = linkauth == 'wpa2-psk' ? i18n.t('wifi.wpa2_psk') :
+	        	(linkauth === 'wpa2' ? i18n.t('wifi.wpa2') : linkauth)
+	        	$('td:eq(12)', nRow).html(linkauth)
+                
+	        	// Format AP Mode
+	        	var apmode=$('td:eq(13)', nRow).html();
+	        	apmode = apmode == 'station ' ? i18n.t('wifi.station') :
+	        		(apmode)
+	        	$('td:eq(13)', nRow).html(apmode)
+                                
+	        	// Blank row if no wifi
+	        	var wifistate=$('td:eq(4)', nRow).html();
+                if ( wifistate == 'no wifi' || wifistate == 'off') {
+                    $('td:eq(5)', nRow).html("")
+                    $('td:eq(6)', nRow).html("")
+                    $('td:eq(7)', nRow).html("")
+                    $('td:eq(8)', nRow).html("")
+                    $('td:eq(9)', nRow).html("")
+                    $('td:eq(10)', nRow).html("")
+                    $('td:eq(11)', nRow).html("")
+                    $('td:eq(12)', nRow).html("")
+                    $('td:eq(13)', nRow).html("")
+                    $('td:eq(14)', nRow).html("")
+                }
+                
+	        	// Format wifi state
+	        	var wifistate=$('td:eq(4)', nRow).html();
+	        	wifistate = wifistate == 'running' ? i18n.t('wifi.running') :
+	        	wifistate = wifistate == 'off' ? i18n.t('wifi.off') :
+	        	wifistate = wifistate == 'no wifi' ? i18n.t('wifi.no_wifi') :
+	        	wifistate = wifistate == 'init' ? i18n.t('wifi.init') :
+	        	wifistate = wifistate == 'sharing' ? i18n.t('wifi.sharing') :
+	        	(wifistate === 'unknown' ? i18n.t('wifi.unknown') : wifistate)
+	        	$('td:eq(4)', nRow).html(wifistate)
 
 	        }
 	    });
