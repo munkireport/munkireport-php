db063628d67eadbd0b92901576410b085ef8670f
Arjen van Bochoven <arjen@bochoven.net>
2017-03-15 14:36:28 +0100

    Fix locales for fan_temps

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.13.0.2610';
+$GLOBALS['version'] = '2.13.0.2616';
 
 // Return version without commit count
 function get_version()
--- app/modules/fan_temps/provides.php
+++ app/modules/fan_temps/provides.php
@@ -2,9 +2,9 @@
 
 return array(
     'client_tabs' => array(
-        'fan_temps-tab' => array('view' => 'fan_temps_tab', 'i18n' => 'fans.tabtitle'),
+        'fan_temps-tab' => array('view' => 'fan_temps_tab', 'i18n' => 'fan_temps.tabtitle'),
     ),
     'listings' => array(
-        'fan_temps' => array('view' => 'fan_temps_listing', 'i18n' => 'fans.tabtitle'),
+        'fan_temps' => array('view' => 'fan_temps_listing', 'i18n' => 'fan_temps.tabtitle'),
     ),
 );
