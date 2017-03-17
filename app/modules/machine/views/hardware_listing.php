d7c3b4610652121d7dbe6605ae453a251d3ced98
eholtam <eholtam@mac.com>
2017-03-17 14:59:55 -0500

    Hardware localization fix for Listing title

--- app/modules/machine/views/hardware_listing.php
+++ app/modules/machine/views/hardware_listing.php
@@ -11,7 +11,7 @@ new Reportdata_model;
 
   	<div class="col-lg-12">
 
-		  <h3><span data-i18n="nav.reports.hardware"></span> <span id="total-count" class='label label-primary'>…</span></h3>
+		  <h3><span data-i18n="machine.hardware_report"></span> <span id="total-count" class='label label-primary'>…</span></h3>
 
 		  <table class="table table-striped table-condensed table-bordered">
 		    <thead>
