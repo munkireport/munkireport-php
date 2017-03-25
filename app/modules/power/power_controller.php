270757d04851781a35bfdfb90708bdf38c89d6f4
tuxudo <tuxudo@users.noreply.github.com>
2017-03-25 11:00:11 -0400

    Issue 697 fix (#698)
    * Issue 697 fix
    
    * Fixed condition import

--- app/modules/power/power_controller.php
+++ app/modules/power/power_controller.php
@@ -76,10 +76,10 @@ class Power_controller extends Module_controller
 
         $queryobj = new Power_model();
         $sql = "SELECT COUNT(CASE WHEN `condition` = 'Normal' THEN 1 END) AS normal,
-						COUNT(CASE WHEN `condition` = 'Replace Soon' THEN 1 END) AS soon,
-						COUNT(CASE WHEN `condition` = 'Service Battery' THEN 1 END) AS service,
-						COUNT(CASE WHEN `condition` = 'Replace Now' THEN 1 END) AS now,
-						COUNT(CASE WHEN `condition` = 'No Battery' THEN 1 END) AS missing
+						COUNT(CASE WHEN `condition` = 'Replace Soon' OR `condition` = 'ReplaceSoon'  THEN 1 END) AS soon,
+						COUNT(CASE WHEN `condition` = 'Service Battery' OR `condition` = 'ServiceBattery'  THEN 1 END) AS service,
+						COUNT(CASE WHEN `condition` = 'Replace Now' OR `condition` = 'ReplaceNow' THEN 1 END) AS now,
+						COUNT(CASE WHEN `condition` = 'No Battery' OR `condition` = 'NoBattery' THEN 1 END) AS missing
 			 			FROM power
 			 			LEFT JOIN reportdata USING (serial_number)
 			 			".get_machine_group_filter();
