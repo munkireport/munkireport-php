58e34572acef221744a3841927f1e0ac8b30558b
eholtam <eholtam@mac.com>
2017-03-26 15:24:20 -0500

    Bluetooth edit

--- app/modules/bluetooth/views/bluetooth_battery_widget.php
+++ app/modules/bluetooth/views/bluetooth_battery_widget.php
@@ -7,7 +7,7 @@
 			<h3 class="panel-title"><i class="fa fa-bolt"></i> 
 				<span data-i18n="bluetooth.bluetooth_battery_widget"></span>
 				<span class="counter badge pull-right"></span>
-				<list-link data-url="/show/listing/reportdata/clients"></list-link>
+				<list-link data-url="/show/listing/bluetooth/bluetooth"></list-link>
 			</h3>
 
 		</div>
