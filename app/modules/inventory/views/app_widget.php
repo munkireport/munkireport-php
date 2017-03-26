74f0638a44c689975bd2f73c1a78ce399dc2b194
poundbangbash <eholtam@mac.com>
2017-03-25 23:22:42 -0500

    Add app specific link to App Version Report titlebar

--- app/modules/inventory/views/app_widget.php
+++ app/modules/inventory/views/app_widget.php
@@ -1,7 +1,7 @@
 <?php foreach(conf('apps_to_track', array()) as $string):?>
 <div class="col-lg-4 col-md-6">
     <div class="panel panel-default app-widget" data-ident="<?php echo $string;?>">
-        <div class="panel-heading" data-container="body" onclick="location.href=appUrl+'/show/listing/inventory/inventory/'">
+        <div class="panel-heading" data-container="body">
             <h3 class="panel-title"><i class="fa fa-tachometer"></i> <span></span></h3>
         </div>
         <div class="list-group scroll-box"></div>
@@ -19,7 +19,7 @@ $(document).on('appReady', function(){
         var appName = $(this).data('ident');
         // Set tooltip
         $(this).find('div.panel-heading')
-            .attr('title', i18n.t('inventory.app.versions', {app: appName}))
+	    .attr('title', i18n.t('inventory.app.versions', {app: appName}))
             .tooltip();
         // Set title
         $(this).find('.panel-title span').text(appName);
@@ -28,6 +28,11 @@ $(document).on('appReady', function(){
             name: appName,
             list: $(this).find('div.list-group')
         });
+	// Link title bar to listing of all versions of the app
+        $(this).click(function(){
+                location.href=appUrl + '/module/inventory/items/' + appName
+            });
+
     });
 
     $(document).on('appUpdate', function(){
