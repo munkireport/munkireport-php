94a11a46ace48f419ed866a098de6064ac1a2ff1
tuxudo <John@Thunderchild.local>
2017-03-29 12:20:02 -0400

    Widget Rewrites
    Changed format of Time Machine, Certificates, and Battery Condition
    widget to be colour blocks instead of scrolling. Added purged bytes
    block to caching widget.

--- app/modules/timemachine/views/timemachine_widget.php
+++ app/modules/timemachine/views/timemachine_widget.php
@@ -1,57 +1,38 @@
-        <div class="col-lg-4 col-md-6">
-
-            <div class="panel panel-default">
-
-                <div class="panel-heading" data-container="body" title="">
-
-                    <h3 class="panel-title"><i class="fa fa-clock-o"></i> <span data-i18n="timemachine.timemachine"></span></h3>
-
-                </div>
-
-                <div class="list-group scroll-box">
-                    <a id="tm-today" href="<?=url('show/listing/timemachine/timemachine')?>" class="list-group-item list-group-item-success hide">
-                        <span class="badge">0</span>
-                        <span data-i18n="backup.today"></span>
-                    </a>
-                    <a id="tm-lastweek" href="<?=url('show/listing/timemachine/timemachine')?>" class="list-group-item list-group-item-warning hide">
-                        <span class="badge">0</span>
-                        <span data-i18n="backup.lastweek"></span>
-                    </a>
-                    <a id="tm-week_plus" href="<?=url('show/listing/timemachine/timemachine')?>" class="list-group-item list-group-item-danger hide">
-                        <span class="badge">0</span>
-                        <span data-i18n="backup.week_plus"></span>
-                    </a>
-                    <span id="tm-nodata" data-i18n="no_clients" class="list-group-item"></span>
-                </div>
-
-            </div><!-- /panel -->
-
-        </div><!-- /col -->
+<div class="col-lg-4 col-md-6">
+    <div class="panel panel-default" id="time-machine-status-widget">
+        <div class="panel-heading" data-container="body" title="">
+            <h3 class="panel-title"><i class="fa fa-clock-o"></i> <span data-i18n="timemachine.timemachinewidget"></span></h3>
+        </div>
+        <div class="panel-body text-center"></div>
+    </div><!-- /panel -->
+</div><!-- /col -->
 
 <script>
 $(document).on('appUpdate', function(e, lang) {
 
     $.getJSON( appUrl + '/module/timemachine/get_stats', function( data ) {
 
-        // Show no clients span
-        $('#tm-nodata').removeClass('hide');
-
-        $.each(data, function(prop, val){
-            if(val > 0)
-            {
-                $('#tm-' + prop).removeClass('hide');
-                $('#tm-' + prop + ' > .badge').text(val);
-
-                // Hide no clients span
-                $('#tm-nodata').addClass('hide');
-            }
-            else
-            {
-                $('#tm-' + prop).addClass('hide');
-            }
-        });
+    	if(data.error){
+    		//alert(data.error);
+    		return;
+    	}
+		
+		var panel = $('#time-machine-status-widget div.panel-body'),
+			baseUrl = appUrl + '/show/listing/timemachine/timemachine';
+		panel.empty();
+		
+		// Set statuses
+		if(data.week_plus){
+			panel.append(' <a href="'+baseUrl+'" class="btn btn-danger"><span class="bigger-150">'+data.week_plus+'</span><br>'+i18n.t('timemachine.week_plus')+'</a>');
+		}
+		if(data.lastweek){
+			panel.append(' <a href="'+baseUrl+'" class="btn btn-warning"><span class="bigger-150">'+data.lastweek+'</span><br>'+i18n.t('timemachine.lastweek')+'</a>');
+		}
+		if(data.today){
+			panel.append(' <a href="'+baseUrl+'" class="btn btn-success"><span class="bigger-150">'+data.today+'</span><br>'+i18n.t('timemachine.today')+'</a>');
+		}
+        
+        $('#time-machine-status-widget .counter').html(data.total);
     });
 });
-
-
 </script>
