b2e3eecc9de1fc156a82d4bafc020847f141f5fe
tuxudo <John@Thunderchild.local>
2017-03-29 12:30:14 -0400

    Updated caching widget

--- app/modules/caching/views/caching_widget.php
+++ app/modules/caching/views/caching_widget.php
@@ -1,7 +1,10 @@
 <div class="col-lg-4 col-md-6">
-	<div class="panel panel-default" id="caching-widget">
+	<div class="panel panel-default" id="caching-widget">            
 		<div class="panel-heading" data-container="body" data-i18n="[title]caching.widget_tooltip">
-			<h3 class="panel-title"><i class="fa fa-database"></i> <span data-i18n="caching.widget_title"></h3>
+			<h3 class="panel-title"><i class="fa fa-database"></i>
+			    <span data-i18n="caching.widget_title"></span>
+			    <list-link data-url="/show/listing/caching/caching"></list-link>
+			</h3>
 		</div>
 		<div class="panel-body text-center"></div>
 	</div><!-- /panel -->
