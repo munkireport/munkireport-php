b372f7444f8d81e9dd0d36e65a611757ccf5129d
tuxudo <John@Thunderchild.local>
2017-03-29 12:47:22 -0400

    Remove extra caching block

--- app/modules/caching/views/caching_widget.php
+++ app/modules/caching/views/caching_widget.php
@@ -34,9 +34,7 @@ $(document).on('appUpdate', function(e, lang) {
 		if(data.fromcache){
 			panel.append(' <a href="'+baseUrl+'" class="btn btn-success"><span class="bigger-150">'+fileSize(data.fromcache, 2)+'</span><br>'+i18n.t('caching.from_cache')+'</a>');
 		}
-		if(data.fromorigin){
-			panel.append(' <a href="'+baseUrl+'" class="btn btn-info"><span class="bigger-150">'+fileSize(data.fromorigin, 2)+'</span><br>&nbsp;&nbsp;'+i18n.t('caching.from_origin')+'&nbsp;&nbsp;</a>');
-		}
+
     });
 });
 </script>
