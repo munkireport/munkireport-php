c46a63eca949e2c2014f88137db10d846e0d8ef5
tuxudo <John@Thunderchild.local>
2017-04-02 17:44:46 -0400

    Fix for empty munkiinfo db

--- app/modules/munkiinfo/provides.php
+++ app/modules/munkiinfo/provides.php
@@ -1,6 +1,9 @@
 <?php
 
 return array(
+    'listings' => array(
+        'munkiinfo' => array('view' => 'munkiinfo_listing', 'i18n' => 'munkiinfo.listing'),
+    ),
     'widgets' => array(
         array('view' => 'munkiinfo_munkiprotocol_widget'),
     ),
