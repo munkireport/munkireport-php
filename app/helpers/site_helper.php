d894085a6386642de2ba15780f678c089e816ab3
Arjen van Bochoven <arjen@bochoven.net>
2017-05-12 17:55:27 +0200

    Always enable the event module

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.15.0.2787';
+$GLOBALS['version'] = '2.15.0.2789';
 
 // Return version without commit count
 function get_version()
