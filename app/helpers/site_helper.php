91f54123b3c799b486cf6893b2d98d06b5b5eef4
Arjen van Bochoven <arjen@bochoven.net>
2017-05-21 23:10:24 +0200

    Fix #812

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '2.15.0.2790';
+$GLOBALS['version'] = '2.15.0.2795';
 
 // Return version without commit count
 function get_version()
