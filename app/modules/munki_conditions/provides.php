0c25e4ba278f3eda02b3cf754432704b5056acb5
Nathan Perkins <nperkins487@gmail.com>
2017-08-02 20:25:51 -0700

    add client tab

--- app/modules/munki_conditions/provides.php
+++ app/modules/munki_conditions/provides.php
@@ -1,7 +1,9 @@
 <?php
 
 return array(
-    'client_tabs' => array(),
+    'client_tabs' => array(
+      'munki_conditions' => array('view' => 'client_munki_conditions_tab', 'i18n' => 'munki_conditions.client_tab'),
+    ),
     'listings' => array(
         'munki_conditions' => array('view' => 'munki_conditions_listing', 'i18n' => 'munki_conditions.listing'),
     ),
