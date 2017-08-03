587a255ebbc1b07c6e620c2e34ba1d8ce749fb3e
Nathan Perkins <nperkins487@gmail.com>
2017-08-03 08:29:29 -0700

    clean up munki_conditions script
    get path from prefs, shift code around

--- app/modules/munki_conditions/scripts/munki_conditions.py
+++ app/modules/munki_conditions/scripts/munki_conditions.py
@@ -4,21 +4,17 @@ munki_conditions for munkireport
 """
 
 import os
-import sys
 import plistlib
+import CoreFoundation
 
-#todo: get report path from preferences.
 
-CACHEPATH = '%s/cache' % os.path.dirname(os.path.realpath(__file__))
-REPORTPATH = '/Library/Managed Installs/ManagedInstallReport.plist'
-
-def gather_conditions():
+def gather_conditions(path):
   """
   Read condition keys and values from the ManagedInstallReport and process them into a
   dict of strings less than 256 characters.
   """
   try:
-    plist = plistlib.readPlist(REPORTPATH)
+    plist = plistlib.readPlist(path)
   except:
     print 'Could not find ManagedInstallReport.plist'
     return {}
@@ -41,12 +37,23 @@ def main():
   """Main"""
 
   # Create cache dir if it does not exist
-  if not os.path.exists(CACHEPATH):
-      os.makedirs(CACHEPATH)
+  cache_dir = '%s/cache' % os.path.dirname(os.path.realpath(__file__))
+  if not os.path.exists(cache_dir):
+    os.makedirs(cache_dir)
+
+  managed_install_dir = CoreFoundation.CFPreferencesCopyAppValue( "ManagedInstallDir", "ManagedInstalls")
+  managed_install_dir = managed_install_dir or '/Library/Managed Installs'
+  managed_install_report = managed_install_dir + '/ManagedInstallReport.plist'
+
+  if not os.path.exists(managed_install_report):
+    print "%s is missing." % managed_install_report
+    conditions_report = {}
+  else:
+    conditions_report = gather_conditions(managed_install_report)
 
   # Write munkiinfo report to cache
-  output_plist = os.path.join(CACHEPATH, 'munki_conditions.plist')
-  plistlib.writePlist(gather_conditions(), output_plist)
+  output_plist = os.path.join(cache_dir, 'munki_conditions.plist')
+  plistlib.writePlist(conditions_report, output_plist)
 
 if __name__ == '__main__':
   main()
