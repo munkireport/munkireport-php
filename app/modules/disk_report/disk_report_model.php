a208233912fefad72064cacdff721ed87e25046d
abn290 <abn290@vu.nl>
2017-08-30 16:34:05 +0200

    Fix disk_report_model
    Remove trailing comma

--- app/modules/disk_report/disk_report_model.php
+++ app/modules/disk_report/disk_report_model.php
@@ -62,7 +62,7 @@ class Disk_report_model extends Model
         $sql = 'SELECT COUNT(CASE WHEN media_type = "hdd" THEN 1 END) AS hdd,
 						COUNT(CASE WHEN media_type = "ssd" THEN 1 END) AS ssd,
 						COUNT(CASE WHEN media_type = "fusion" THEN 1 END) AS fusion,
-						COUNT(CASE WHEN media_type = "raid" THEN 1 END) AS raid,
+						COUNT(CASE WHEN media_type = "raid" THEN 1 END) AS raid
 						FROM diskreport
 						LEFT JOIN reportdata USING (serial_number)
 						WHERE Internal = 1
