c9437639451ab1f674a2e9e67806519ccad82010
abn290 <abn290@vu.nl>
2017-09-03 22:09:23 +0200

    Update migration
    Default value

--- app/modules/disk_report/migrations/003_disk_report_add_media_type.php
+++ app/modules/disk_report/migrations/003_disk_report_add_media_type.php
@@ -15,13 +15,13 @@ class Migration_disk_report_add_media_type extends Model
     {
         Schema::table('diskreport', function ($table)
         {
-            $table->string('media_type');
+            $table->string('media_type')->default('-');
             $table->index('media_type');
         });
         
         // Populate new column
         $sql = "UPDATE diskreport 
-                SET media_type = volumeType, volumeType = 'hfs' 
+                SET media_type = volumeType, volumeType = '-' 
                 WHERE volumeType IN ('hdd', 'ssd', 'fusion', 'raid')";
         $this->exec($sql);
 
