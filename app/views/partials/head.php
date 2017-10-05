b7f1130bbf66c4c4d32a25d39a449a220637db64
abn290 <abn290@vu.nl>
2017-10-05 14:37:50 +0200

    Fix head.php
    No need for full urls

--- app/views/partials/head.php
+++ app/views/partials/head.php
@@ -34,7 +34,7 @@
 
 	<script>
 		var baseUrl = "<?php echo conf('subdirectory'); ?>",
-			appUrl = "<?php echo rtrim(url('', true), '/'); ?>",
+			appUrl = "<?php echo rtrim(url(), '/'); ?>",
 			businessUnitsEnabled = <?php echo conf('enable_business_units') ? 'true' : 'false'; ?>;
 			isAdmin = <?php echo $_SESSION['role'] == 'admin' ? 'true' : 'false'; ?>;
 			isManager = <?php echo $_SESSION['role'] == 'manager' ? 'true' : 'false'; ?>;
