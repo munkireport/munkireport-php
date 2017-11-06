4d654aacad96e97abc0fd484826cf74d5e455089
tuxudo <John@Thunderchild.local>
2017-11-06 13:44:32 -0500

    Changed main script to Python

--- app/modules/usage_stats/scripts/install.sh
+++ app/modules/usage_stats/scripts/install.sh
@@ -4,19 +4,19 @@
 CTL="${BASEURL}index.php?/module/usage_stats/"
 
 # Get the scripts in the proper directories
-"${CURL[@]}" "${CTL}get_script/usage_stats.sh" -o "${MUNKIPATH}preflight.d/usage_stats.sh"
+"${CURL[@]}" "${CTL}get_script/usage_stats.sh" -o "${MUNKIPATH}preflight.d/usage_stats"
 
 # Check exit status of curl
 if [ $? = 0 ]; then
 	# Make executable
-	chmod a+x "${MUNKIPATH}preflight.d/usage_stats.sh"
+	chmod a+x "${MUNKIPATH}preflight.d/usage_stats"
 
 	# Set preference to include this file in the preflight check
 	setreportpref "usage_stats" "${CACHEPATH}usage_stats.plist"
 
 else
 	echo "Failed to download all required components!"
-	rm -f "${MUNKIPATH}preflight.d/usage_stats.sh"
+	rm -f "${MUNKIPATH}preflight.d/usage_stats"
 
 	# Signal that we had an error
 	ERR=1
