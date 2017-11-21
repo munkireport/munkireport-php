79a04e1495ce6bfdd1922a55d2c9eef136984e3a
jbaker10 <jer.baker10@gmail.com>
2017-11-21 11:11:51 -0500

    Updating install and unisntall scripts to use new python script. Updating networkinfo.py to iterate over tunnel adapters to catch any that are present

--- app/modules/network/scripts/networkinfo.py
+++ app/modules/network/scripts/networkinfo.py
@@ -14,13 +14,13 @@ def bashCommand(script):
         return "[* Error] **%s** [%s]" % (err, str(script))
 
 def get_network_info():
-    networkservices = bashCommand(['networksetup', '-listallnetworkservices']).split('\n')[:-1]
+    networkservices = bashCommand(['/usr/sbin/networksetup', '-listallnetworkservices']).split('\n')[:-1]
     for network in networkservices:
         if "asterisk" in network:
             pass
         else:
             network_service_list.append('Service: %s' % network)
-            network_info = bashCommand(['networksetup', '-getinfo', network]).split('\n')[:-1]
+            network_info = bashCommand(['/usr/sbin/networksetup', '-getinfo', network]).split('\n')[:-1]
             for info in network_info:
                 network_service_list.append(info)
 
@@ -29,59 +29,61 @@ def get_tunnel_info():
     ## Things to do:
     ## 1: Make the parser smarter so in case things aren't where we expect, we don't ship bad info
     ## 2: Allow the function to loop over all 'utun' addresses if there are multiple
-    try:
-        tunnel_ip = bashCommand(['ifconfig', 'utun1'])
-        tunnel_ip = tunnel_ip.split()
-        # Helpful functions to convert ip and subnet mask to router address
-        # May not be fully useful and might give incorrect info if CIDR is greater than /24
-        # greater in this case means less, i.e. /22 = greater number of addresses
-        def netmask_to_cidr(netmask):
-            return sum([bin(int(x)).count('1') for x in netmask.split('.')])
-
-        def get_network_addr(ip, cidr):
-            ip = ip.split('.')
-            cidr = int(cidr)
-
-            mask = [0, 0, 0, 0]
-            for i in range(cidr):
-                mask[i/8] = mask[i/8] + (1 << (7 - i % 8))
-
-            net = []
-            for i in range(4):
-                net.append(int(ip[i]) & mask[i])
-
-            broad = list(net)
-            brange = 32 - cidr
-            for i in range(brange):
-                broad[3 - i/8] = broad[3 - i/8] + (1 << (i % 8))
-
-            return ".".join(map(str, net))
-
-        # Get address info from parsed output
-        ipv4_address = tunnel_ip[5]
-        subnet_mask = socket.inet_ntoa(struct.pack('!L', int(tunnel_ip[9], 16)))
-        cidr = netmask_to_cidr(subnet_mask)
-        router_address = get_network_addr(ipv4_address, cidr)
-
-        if not 'fe80' in tunnel_ip[11]:
-            ipv6_address = tunnel_ip[11]
-        else:
-            ipv6_address = "none"
-
-        network_service_list.append('Service: Tunnel Interface')
-        network_service_list.append('DHCP Configuration')
-        network_service_list.append('IP address: %s' % str(ipv4_address))
-        network_service_list.append('Subnet mask: %s' % str(subnet_mask))
-        network_service_list.append('Router: %s' % str(router_address))
-        network_service_list.append('Ethernet Address: 00:00:00:00:00:00')
-        network_service_list.append('Client ID:')
-        network_service_list.append('IPv6: Automatic')
-        network_service_list.append('IPv6 IP address: %s' % str(ipv6_address))
-        network_service_list.append('IPv6 Router: none')
-    except:
-        return
-
     
+    utun_adapter_count = os.popen('ifconfig | grep -c "utun[0-9]:"').read()[0]
+    if int(utun_adapter_count) > 1:
+        for i in range(1, int(utun_adapter_count)):
+            try:
+                tunnel_ip = bashCommand(['/sbin/ifconfig', 'utun%i' % i])
+                tunnel_ip = tunnel_ip.split()
+                # Helpful functions to convert ip and subnet mask to router address
+                # May not be fully useful and might give incorrect info if CIDR is greater than /24
+                # greater in this case means less, i.e. /22 = greater number of addresses
+                def netmask_to_cidr(netmask):
+                    return sum([bin(int(x)).count('1') for x in netmask.split('.')])
+
+                def get_network_addr(ip, cidr):
+                    ip = ip.split('.')
+                    cidr = int(cidr)
+
+                    mask = [0, 0, 0, 0]
+                    for i in range(cidr):
+                        mask[i/8] = mask[i/8] + (1 << (7 - i % 8))
+
+                    net = []
+                    for i in range(4):
+                        net.append(int(ip[i]) & mask[i])
+
+                    broad = list(net)
+                    brange = 32 - cidr
+                    for i in range(brange):
+                        broad[3 - i/8] = broad[3 - i/8] + (1 << (i % 8))
+
+                    return ".".join(map(str, net))
+
+                # Get address info from parsed output
+                ipv4_address = tunnel_ip[5]
+                subnet_mask = socket.inet_ntoa(struct.pack('!L', int(tunnel_ip[9], 16)))
+                cidr = netmask_to_cidr(subnet_mask)
+                router_address = get_network_addr(ipv4_address, cidr)
+
+                if not 'fe80' in tunnel_ip[11]:
+                    ipv6_address = tunnel_ip[11]
+                else:
+                    ipv6_address = "none"
+
+                network_service_list.append('Service: Tunnel Interface (utun%i)' % i)
+                network_service_list.append('DHCP Configuration')
+                network_service_list.append('IP address: %s' % str(ipv4_address))
+                network_service_list.append('Subnet mask: %s' % str(subnet_mask))
+                network_service_list.append('Router: %s' % str(router_address))
+                network_service_list.append('Ethernet Address: 00:00:00:00:00:00')
+                network_service_list.append('Client ID:')
+                network_service_list.append('IPv6: Automatic')
+                network_service_list.append('IPv6 IP address: %s' % str(ipv6_address))
+                network_service_list.append('IPv6 Router: none')
+            except:
+                return
 
 def main():
     '''Main'''
@@ -100,8 +102,6 @@ def main():
     get_network_info()
 
     # Get VPN/Tunnel info
-    # Need to add checks to this function to ensure we actually want to call it
-    # otherwise, we might have a parsing error
     get_tunnel_info()
 
     # Write network results to cache
