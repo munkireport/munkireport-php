30c6ae01094da1ef03d67de0120fae576c9e8da6
mosen <mosen@fastmail.com>
2017-11-22 22:31:28 +1100

    add illuminate migrations for ard, business_unit, backup2go, bluetooth and caching

--- app/modules/ard/ard_model.php
+++ app/modules/ard/ard_model.php
@@ -36,6 +36,47 @@ class Ard_model extends \Model
         $this->serial = $serial;
     }
 
+    // Override create_table to use illuminate/database capsule
+    public function create_table() {
+        // Check if we instantiated this table before
+        if (isset($GLOBALS['tables'][$this->tablename])) {
+            return true;
+        }
+
+        $capsule = $this->getCapsule();
+
+        try {
+            $exist = $capsule::table('ard')->limit(1)->count();
+        } catch (PDOException $e) {
+            $capsule::schema()->create('ard', function ($table) {
+                $table->increments('id');
+                $table->string('serial_number')->unique();
+                $table->string('Text1');
+                $table->string('Text2');
+                $table->string('Text3');
+                $table->string('Text4');
+
+                $table->index('Text1', 'ard_Text1');
+                $table->index('Text2', 'ard_Text2');
+                $table->index('Text3', 'ard_Text3');
+                $table->index('Text4', 'ard_Text4');
+            });
+
+//            // Store schema version in migration table
+//            $migration = new Migration($this->tablename);
+//            $migration->version = $this->schema_version;
+//            $migration->save();
+//
+//            alert("Created table '$this->tablename' version $this->schema_version");
+        }
+
+        // Store this table in the instantiated tables array
+        $GLOBALS['tables'][$this->tablename] = $this->tablename;
+
+        // Create table succeeded
+        return true;
+    }
+
     public function process($data)
     {
         $parser = new CFPropertyList();
--- app/modules/backup2go/backup2go_model.php
+++ app/modules/backup2go/backup2go_model.php
@@ -32,7 +32,41 @@ class backup2go_model extends \Model {
 
         $this->serial = $serial;
 	}
-	
+
+    // Override create_table to use illuminate/database capsule
+    public function create_table() {
+        // Check if we instantiated this table before
+        if (isset($GLOBALS['tables'][$this->tablename])) {
+            return true;
+        }
+
+        $capsule = $this->getCapsule();
+
+        try {
+            $exist = $capsule::table('backup2go')->limit(1)->count();
+        } catch (PDOException $e) {
+            $capsule::schema()->create('backup2go', function ($table) {
+                $table->increments('id');
+                $table->string('serial_number')->unique();
+                $table->string('backupdate');
+
+                $table->index('serial_number', 'backup2go_serial_number');
+            });
+
+//            // Store schema version in migration table
+//            $migration = new Migration($this->tablename);
+//            $migration->version = $this->schema_version;
+//            $migration->save();
+//
+//            alert("Created table '$this->tablename' version $this->schema_version");
+        }
+
+        // Store this table in the instantiated tables array
+        $GLOBALS['tables'][$this->tablename] = $this->tablename;
+
+        // Create table succeeded
+        return true;
+    }
 	
 	/**
 	 * function: process($data)
