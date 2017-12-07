ecc0b9d9a704575d4a3baae530e953c4abecbc61
mosen <mosen@fastmail.com>
2017-12-07 22:32:34 +1100

    testing detection of legacy migration version

--- app/modules/disk_report/migrations/2017_02_09_234243_diskreport.php
+++ app/modules/disk_report/migrations/2017_02_09_234243_diskreport.php
@@ -8,31 +8,40 @@ class Diskreport extends Migration
     public function up()
     {
         $capsule = new Capsule();
-        $capsule::schema()->create('diskreport', function (Blueprint $table) {
-            $table->increments('id');
 
-            $table->string('serial_number');
-            $table->bigInteger('TotalSize');
-            $table->bigInteger('FreeSpace');
-            $table->bigInteger('Percentage');
-            $table->string('SMARTStatus');
-            $table->string('VolumeType');
-            $table->string('media_type');
-            $table->string('BusProtocol');
-            $table->integer('Internal');
-            $table->string('MountPoint');
-            $table->string('VolumeName');
-            $table->integer('CoreStorageEncrypted');
-            $table->string('timestamp');
+        $legacy_migration_version = $capsule::table('migration')
+            ->where('table_name', '=', 'diskreport')
+            ->first();
 
-            $table->index('serial_number', 'diskreport_serial_number');
-            $table->index('MountPoint', 'diskreport_MountPoint');
-            $table->index('media_type', 'diskreport_media_type');
-            $table->index('VolumeName', 'diskreport_VolumeName');
-            $table->index('VolumeType', 'diskreport_VolumeType');
+        if ($legacy_migration_version && $legacy_migration_version->version === 3) {
+            // No need to run this migration
+        } else {
+            $capsule::schema()->create('diskreport', function (Blueprint $table) {
+                $table->increments('id');
+
+                $table->string('serial_number');
+                $table->bigInteger('TotalSize');
+                $table->bigInteger('FreeSpace');
+                $table->bigInteger('Percentage');
+                $table->string('SMARTStatus');
+                $table->string('VolumeType');
+                $table->string('media_type');
+                $table->string('BusProtocol');
+                $table->integer('Internal');
+                $table->string('MountPoint');
+                $table->string('VolumeName');
+                $table->integer('CoreStorageEncrypted');
+                $table->string('timestamp');
+
+                $table->index('serial_number', 'diskreport_serial_number');
+                $table->index('MountPoint', 'diskreport_MountPoint');
+                $table->index('media_type', 'diskreport_media_type');
+                $table->index('VolumeName', 'diskreport_VolumeName');
+                $table->index('VolumeType', 'diskreport_VolumeType');
 
 //            $table->timestamps();
-        });
+            });
+        }
     }
 
     public function down()
