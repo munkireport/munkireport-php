24f24bbde16e721adf8b9695796c8faddd92cd46
Zack McCauley <wpgraphix@gmail.com>
2017-12-14 15:08:58 -0700

    Better PHP formatted indents, install script will create file if need be.

--- app/modules/detectx/scripts/install.sh
+++ app/modules/detectx/scripts/install.sh
@@ -2,5 +2,8 @@
 
 # detectx controller
 #CTL="${BASEURL}index.php?/module/detectx/"
-	# Set preference to include this file in the preflight check
+# Set preference to include this file in the preflight check
 setreportpref "detectx" "${CACHEPATH}detectx.json"
+if [ ! -f "${CACHEPATH}detectx.json" ]; then
+  touch "${CACHEPATH}detectx.json"
+fi
--- app/modules/detectx/views/detectx_tab.php
+++ app/modules/detectx/views/detectx_tab.php
@@ -3,41 +3,41 @@
 
 <script>
 $(document).on('appReady', function(){
-	$.getJSON(appUrl + '/module/detectx/get_data/' + serialNumber, function(data){
-		// Set count to detectx's number of issues
+  $.getJSON(appUrl + '/module/detectx/get_data/' + serialNumber, function(data){
+    // Set count to detectx's number of issues
     $('#detectx-cnt').text(data[0].numberofissues);
-		var skipThese = ['id','serial_number'];
-		$.each(data, function(i,d){
+    var skipThese = ['id','serial_number'];
+    $.each(data, function(i,d){
 
-			// Generate rows from data
-			var rows = ''
-			for (var prop in d){
-				// Skip skipThese
-				if(skipThese.indexOf(prop) == -1){
-           if(prop == 'searchdate'){
-             var reporteddate = d[prop];
-             var date = new Date(reporteddate * 1000)
-					   rows = rows + '<tr><th>'+i18n.t('detectx.listing.'+prop)+'</th><td>'+moment(date).format('llll')+'</td></tr>';
-             }
-					    else {
-                if(prop == 'issues'){
-                  var issue = '';
-                  issue = d[prop].split(';').join('<br />');
-                  rows = rows + '<tr><th>'+i18n.t('detectx.listing.'+prop)+'</th><td>'+issue+'</td></tr>';
-                  }
-                else{
-                rows = rows + '<tr><th>'+i18n.t('detectx.listing.'+prop)+'</th><td>'+d[prop]+'</td></tr>';
-              }
-           }
-				}
-			}
-			$('#detectx-tab')
-				.append($('<table style="max-width: 900px">')
-						.addClass('table table-responsive table-striped table-condensed')
-						.append($('<tbody>')
-							.append(rows)))
-		})
+      // Generate rows from data
+      var rows = ''
+      for (var prop in d){
+        // Skip skipThese
+        if(skipThese.indexOf(prop) == -1){
+          if(prop == 'searchdate'){
+            var reporteddate = d[prop];
+            var date = new Date(reporteddate * 1000)
+            rows = rows + '<tr><th>'+i18n.t('detectx.listing.'+prop)+'</th><td>'+moment(date).format('llll')+'</td></tr>';
+          }
+          else {
+            if(prop == 'issues'){
+              var issue = '';
+              issue = d[prop].split(';').join('<br />');
+              rows = rows + '<tr><th>'+i18n.t('detectx.listing.'+prop)+'</th><td>'+issue+'</td></tr>';
+            }
+            else{
+              rows = rows + '<tr><th>'+i18n.t('detectx.listing.'+prop)+'</th><td>'+d[prop]+'</td></tr>';
+            }
+          }
+        }
+      }
+      $('#detectx-tab')
+      .append($('<table style="max-width: 900px">')
+      .addClass('table table-responsive table-striped table-condensed')
+      .append($('<tbody>')
+      .append(rows)))
+    })
 
-	});
+  });
 });
 </script>
