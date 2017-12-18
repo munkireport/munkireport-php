17aa10f29594a48b560058ec63906992f13142b3
Mosen <mosen@users.noreply.github.com>
2017-12-18 21:14:36 +1100

    error feedback improved a little using bootstrap panel

--- app/views/system/database.php
+++ app/views/system/database.php
@@ -1,23 +1,31 @@
 <?php $this->view('partials/head'); ?>
 
-<div class="container">
-    <div class="row">
-        <div id="mr-migrations" class="col-lg-12 loading">
-            <h1><span id="database-update-count">(n/a)</span> <span data-i18n="database.migrations.pending">Database Update(s) Pending</span></h1>
+    <div class="container">
+        <div class="row">
+            <div id="mr-migrations" class="col-lg-12 loading">
+                <h1><span id="database-update-count">(n/a)</span> <span data-i18n="database.migrations.pending">Database Update(s) Pending</span>
+                </h1>
+            </div>
         </div>
-    </div>
 
-    <div class="row">
-        <div class="col-lg-12">
-            <button id="db-upgrade" class="btn btn-primary">
-                <span id="db-upgrade-label" data-i18n="database.update">Update</span>
-            </button>
+        <div class="row">
+            <div class="col-lg-12">
+                <button id="db-upgrade" class="btn btn-primary">
+                    <span id="db-upgrade-label" data-i18n="database.update">Update</span>
+                </button>
+            </div>
         </div>
-    </div>
-    <div class="row">
-        <div id="database-upgrade-log" class="col-lg-6">
-            <table class="table table-console">
-                <thead>
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="alert alert-danger database-alert" style="display: none;"></div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div id="database-upgrade-log" class="col-lg-6">
+                <table class="table table-console">
+                    <thead>
                     <tr>
                         <th colspan="1">
                             <a class="disclosure" href="#">
@@ -25,87 +33,83 @@
                             </a>
                         </th>
                     </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td data-i18n="database.loghelp">Nothing to show</td>
-                    </tr>
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td data-i18n="database.loghelp">Nothing to show</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
 
-</div>  <!-- /container -->
+    </div>  <!-- /container -->
 
-<script>
-    $(document).on('appReady', function(e, lang) {
+    <script>
+      $(document).on('appReady', function (e, lang) {
 
         // Show/Hide the upgrade log
-        $('.disclosure').click(function() {
-            $(this).toggleClass('disclosure-active');
-            $(this).closest('table').toggleClass('disclosure-active');
+        $('.disclosure').click(function () {
+          $(this).toggleClass('disclosure-active');
+          $(this).closest('table').toggleClass('disclosure-active');
         });
 
-        $('#db-upgrade').click(function(e) {
-            $(this).attr('disabled', true);
-            $(this).find('#db-upgrade-label').html('Upgrading&hellip;');
-            var $btn = $(this);
+        $('#db-upgrade').click(function (e) {
+          $(this).attr('disabled', true);
+          $(this).find('#db-upgrade-label').html('Upgrading&hellip;');
+          var $btn = $(this);
 
-            function done() {
-                $btn.attr('disabled', false);
-                $btn.find('#db-upgrade-label').html('Update');
-            }
+          function done () {
+            $btn.attr('disabled', false);
+            $btn.find('#db-upgrade-label').html('Update');
+          }
 
-            var tbody = $('.table-console tbody');
+          var tbody = $('.table-console tbody');
 
-            $.getJSON(appUrl + '/system/migrate', function(data) {
-                done();
-                tbody.empty();
-
-                if (data.error) {
-                    tbody.append($('<tr><td class="log-error">' + data.error + '</td></tr>'));
-                } else {
-                    if (data.notes) {
-                        tbody.empty();
-
-                        for (var i = 0; i < data.notes.length; i++) {
-                            tbody.append($('<tr><td>' + data.notes[i] + '</td></tr>')); // .text(data.notes[i])
-                        }
-                    }
-                }
-
-
-            }).fail(function(jqXHR, textStatus, error) {
-                done();
-            })
-        });
-        
-        $.getJSON(appUrl + '/system/migrationsPending', function( data ) {
-            var tbody = $('#mr-migrations tbody').empty();
-            $('.loading').removeClass('loading');
+          $.getJSON(appUrl + '/system/migrate', function (data) {
+            done();
 
             if (data.error) {
-                  
-            } else {
-
+              $('.database-alert').show().text(data.error);
             }
 
-            $('#database-update-count').text(data['files_pending'].length);
+            if (data.notes) {
+              tbody.empty();
 
-            if (data.files_pending) {
-                for (var i = 0; i < data['files_pending'].length; i++) {
-                  tbody.append($('<tr><td></td></tr>').text(data['files_pending'][i]));
-                }
+              for (var i = 0; i < data.notes.length; i++) {
+                tbody.append($('<tr><td>' + data.notes[i] + '</td></tr>')); // .text(data.notes[i])
+              }
             }
+        }).fail(function (jqXHR, textStatus, error) {
+          done();
         })
-            .fail(function( jqxhr, textStatus, error ) {
-                var err = textStatus + ", " + error;
-                $('#mr-db table tr td')
-                    .empty()
-                    .addClass('text-danger')
-                    .text(i18n.t('errors.loading', {error:err}));
-            });
-    });
-</script>
+      });
+
+      $.getJSON(appUrl + '/system/migrationsPending', function (data) {
+        var tbody = $('.table-console tbody').empty();
+        $('.loading').removeClass('loading');
+
+        if (data.error) {
+          $('.database-alert').show().text(data.error);
+        }
+
+        $('#database-update-count').text(data['files_pending'].length);
+
+        if (data['files_pending'].length) {
+          for (var i = 0; i < data['files_pending'].length; i++) {
+            tbody.append('<tr><td>' + data['files_pending'][i] + '</td></tr>');
+          }
+        }
+      })
+        .fail(function (jqxhr, textStatus, error) {
+          var err = textStatus + ", " + error;
+          $('#mr-db table tr td')
+            .empty()
+            .addClass('text-danger')
+            .text(i18n.t('errors.loading', {error: err}));
+        });
+      })
+      ;
+    </script>
 <?php
 $this->view('partials/foot');
