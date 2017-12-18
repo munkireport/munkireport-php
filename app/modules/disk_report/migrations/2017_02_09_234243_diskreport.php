6e41b3d298b282c91e284271d13fe934f07961b1
Arjen van Bochoven <abn290@vu.nl>
2017-12-18 10:23:34 +0100

    Remove legacy check from diskreport migration

--- app/modules/disk_report/migrations/2017_02_09_234243_diskreport.php
+++ app/modules/disk_report/migrations/2017_02_09_234243_diskreport.php
@@ -9,39 +9,32 @@ class Diskreport extends Migration
     {
         $capsule = new Capsule();
 
-        $legacy_migration_version = $capsule::table('migration')
-            ->where('table_name', '=', 'diskreport')
-            ->first(['version']);
+        $capsule::schema()->create('diskreport', function (Blueprint $table) {
+            $table->increments('id');
 
-        if ($legacy_migration_version && $legacy_migration_version->version === 3) {
-            // No need to run this migration
-        } else {
-            $capsule::schema()->create('diskreport', function (Blueprint $table) {
-                $table->increments('id');
+            $table->string('serial_number');
+            $table->bigInteger('TotalSize');
+            $table->bigInteger('FreeSpace');
+            $table->bigInteger('Percentage');
+            $table->string('SMARTStatus');
+            $table->string('VolumeType');
+            $table->string('media_type');
+            $table->string('BusProtocol');
+            $table->integer('Internal');
+            $table->string('MountPoint');
+            $table->string('VolumeName');
+            $table->integer('CoreStorageEncrypted');
+            $table->string('timestamp');
 
-                $table->string('serial_number');
-                $table->bigInteger('TotalSize');
-                $table->bigInteger('FreeSpace');
-                $table->bigInteger('Percentage');
-                $table->string('SMARTStatus');
-                $table->string('VolumeType');
-                $table->string('media_type');
-                $table->string('BusProtocol');
-                $table->integer('Internal');
-                $table->string('MountPoint');
-                $table->string('VolumeName');
-                $table->integer('CoreStorageEncrypted');
-                $table->string('timestamp');
-
-                $table->index('serial_number', 'diskreport_serial_number');
-                $table->index('MountPoint', 'diskreport_MountPoint');
-                $table->index('media_type', 'diskreport_media_type');
-                $table->index('VolumeName', 'diskreport_VolumeName');
-                $table->index('VolumeType', 'diskreport_VolumeType');
+            $table->index('serial_number', 'diskreport_serial_number');
+            $table->index('MountPoint', 'diskreport_MountPoint');
+            $table->index('media_type', 'diskreport_media_type');
+            $table->index('VolumeName', 'diskreport_VolumeName');
+            $table->index('VolumeType', 'diskreport_VolumeType');
 
 //            $table->timestamps();
-            });
-        }
+        });
+
     }
 
     public function down()
