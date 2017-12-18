0fbf46aea07b5e7a4b0dc131e9dbcf11103f7251
abn290 <abn290@vu.nl>
2017-12-18 21:09:25 +0100

    Simplify indexes for managed installs

--- app/modules/managedinstalls/migrations/2017_02_09_234433_managedinstalls.php
+++ app/modules/managedinstalls/migrations/2017_02_09_234433_managedinstalls.php
@@ -21,13 +21,13 @@ class Managedinstalls extends Migration
             $table->string('status');
             $table->string('type');
 
-            $table->index('display_name', 'managedinstalls_display_name');
-            $table->index('name', 'managedinstalls_name');
-            $table->index(['name', 'version'], 'managedinstalls_name_version');
-            $table->index('serial_number', 'managedinstalls_serial_number');
-            $table->index('status', 'managedinstalls_status');
-            $table->index('type', 'managedinstalls_type');
-            $table->index('version', 'managedinstalls_version');
+            $table->index('display_name');
+            $table->index('name');
+            $table->index(['name', 'version']);
+            $table->index('serial_number');
+            $table->index('status');
+            $table->index('type');
+            $table->index('version');
 //            $table->timestamps();
         });
     }
