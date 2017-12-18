8bef6236cdf1387b514a886b213fb24d2d635f91
abn290 <abn290@vu.nl>
2017-12-18 22:17:55 +0100

    Add indexes for warranty

--- app/modules/warranty/migrations/2017_02_09_234959_warranty.php
+++ app/modules/warranty/migrations/2017_02_09_234959_warranty.php
@@ -10,13 +10,14 @@ class Warranty extends Migration
         $capsule = new Capsule();
         $capsule::schema()->create('warranty', function (Blueprint $table) {
             $table->increments('id');
-
             $table->string('serial_number')->unique();
             $table->string('purchase_date')->nullable();
             $table->string('end_date')->nullable();
             $table->string('status')->nullable();
-
-//            $table->timestamps();
+            
+            $table->index('purchase_date');
+            $table->index('end_date');
+            $table->index('status');
         });
     }
     
