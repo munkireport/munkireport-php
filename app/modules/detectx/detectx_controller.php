fdb1473773e9342fba2696eb37b4978a4962b340
Zack McCauley <wpgraphix@gmail.com>
2017-12-18 08:27:31 -0700

    More Tab/Space Fixes!

--- app/modules/detectx/detectx_controller.php
+++ app/modules/detectx/detectx_controller.php
@@ -9,22 +9,22 @@
 class Detectx_controller extends Module_controller
 {
 
-	/*** Protect methods with auth! ****/
-	function __construct()
-	{
-		// Store module path
-		$this->module_path = dirname(__FILE__);
-	}
+  /*** Protect methods with auth! ****/
+  function __construct()
+  {
+    // Store module path
+    $this->module_path = dirname(__FILE__);
+  }
 
-	/**
-	 * Default method
-	 * @author tuxudo
-	 *
-	 **/
-	function index()
-	{
-		echo "You've loaded the detectx module!";
-	}
+  /**
+  * Default method
+  * @author tuxudo
+  *
+  **/
+  function index()
+  {
+    echo "You've loaded the detectx module!";
+  }
   public function get_stats()
   {
       $obj = new View();
@@ -41,26 +41,23 @@ class Detectx_controller extends Module_controller
       $obj->view('json', array('msg' => current($queryobj->query($sql))));
   }
 
-	/**
-     * Retrieve data in json format
-     *
-     **/
-	public function get_data($serial_number = '')
-	{
-        $obj = new View();
-
-        if (! $this->authorized()) {
-            $obj->view('json', array('msg' => 'Not authorized'));
-            return;
-        }
-
-        $queryobj = new Detectx_model;
-        $detectx_tab = array();
-        foreach($queryobj->retrieve_records($serial_number) as $detectxEntry) {
-            $detectx_tab[] = $detectxEntry->rs;
-        }
-
-        $obj->view('json', array('msg' => $detectx_tab));
-	}
+  /**
+  * Retrieve data in json format
+  *
+  **/
+  public function get_data($serial_number = '')
+  {
+    $obj = new View();
+    if (! $this->authorized()) {
+        $obj->view('json', array('msg' => 'Not authorized'));
+        return;
+    }
+    $queryobj = new Detectx_model;
+    $detectx_tab = array();
+    foreach($queryobj->retrieve_records($serial_number) as $detectxEntry) {
+      $detectx_tab[] = $detectxEntry->rs;
+    }
+    $obj->view('json', array('msg' => $detectx_tab));
+}
 
 } // END class Detectx_controller
