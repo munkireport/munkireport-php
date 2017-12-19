dfe79f17f96d941d813fe684a6e8e7d909fa03f2
tuxudo <John@Thunderchild.local>
2017-12-19 15:07:51 -0500

    Update v3 migration

--- app/modules/homebrew/Illuminate Migration/2017_12_17_0000001_homebrew.php
+++ app/modules/homebrew/Illuminate Migration/2017_12_17_0000001_homebrew.php
@@ -11,24 +11,24 @@ class Homebrew extends Migration
         $capsule::schema()->create('homebrew', function (Blueprint $table) {
             $table->increments('id');
             $table->string('serial_number')->unique();
-            $table->text('name')->nullable();
-            $table->text('full_name')->nullable();
-            $table->text('oldname')->nullable();
-            $table->text('aliases')->nullable();
-            $table->text('desc')->nullable();
-            $table->text('homepage')->nullable();
-            $table->text('installed_versions')->nullable();
-            $table->text('linked_keg')->nullable();
-            $table->text('dependencies')->nullable();
-            $table->text('build_dependencies')->nullable();
-            $table->text('recommended_dependencies')->nullable();
-            $table->text('runtime_dependencies')->nullable();
-            $table->text('optional_dependencies')->nullable();
-            $table->text('requirements')->nullable();
-            $table->text('options')->nullable();
-            $table->text('used_options')->nullable();
-            $table->text('caveats')->nullable();
-            $table->text('conflicts_with')->nullable();
+            $table->text('name');
+            $table->text('full_name');
+            $table->text('oldname');
+            $table->text('aliases');
+            $table->text('desc');
+            $table->text('homepage');
+            $table->text('installed_versions');
+            $table->text('linked_keg');
+            $table->text('dependencies');
+            $table->text('build_dependencies');
+            $table->text('recommended_dependencies');
+            $table->text('runtime_dependencies');
+            $table->text('optional_dependencies');
+            $table->text('requirements');
+            $table->text('options');
+            $table->text('used_options');
+            $table->text('caveats');
+            $table->text('conflicts_with');
             $table->boolean('built_as_bottle')->default(0);
             $table->boolean('installed_as_dependency')->default(0);
             $table->boolean('installed_on_request')->default(0);
@@ -40,16 +40,16 @@ class Homebrew extends Migration
             $table->boolean('versions_devel')->default(0);
             $table->boolean('versions_head')->default(0);
 
-            $table->index('built_as_bottle', 'homebrew_built_as_bottle');
-            $table->index('installed_as_dependency', 'homebrew_installed_as_dependency');
-            $table->index('installed_on_request', 'homebrew_installed_on_request');
-            $table->index('poured_from_bottle', 'homebrew_poured_from_bottle');
-            $table->index('keg_only', 'homebrew_keg_only');
-            $table->index('outdated', 'homebrew_outdated');
-            $table->index('pinned', 'homebrew_pinned');
-            $table->index('versions_devel', 'homebrew_versions_devel');
-            $table->index('versions_bottle', 'homebrew_versions_bottle');
-            $table->index('versions_head', 'homebrew_versions_head');
+            $table->index('built_as_bottle');
+            $table->index('installed_as_dependency'):
+            $table->index('installed_on_request'):
+            $table->index('poured_from_bottle'):
+            $table->index('keg_only'):
+            $table->index('outdated'):
+            $table->index('pinned'):
+            $table->index('versions_devel'):
+            $table->index('versions_bottle'):
+            $table->index('versions_head'):
         });
     }
     
