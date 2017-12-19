8b62af451f8bc7a42ee523284841e6eb65b5d321
Mosen <mosen@users.noreply.github.com>
2017-12-19 19:55:32 +1100

    network_shares migration

---
+++ app/modules/network_shares/migrations/2017_12_19_195401_network_shares.php
@@ -0,0 +1,28 @@
+<?php
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class NetworkShares extends Migration
+{
+    public function up()
+    {
+        $capsule = new Capsule();
+        $capsule::schema()->create('network_shares', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('serial_number')->nullable();
+            $table->string('name')->nullable();
+            $table->string('mntfromname')->nullable();
+            $table->string('fstypename')->nullable();
+            $table->string('fsmtnonname')->nullable();
+            $table->boolean('automounted')->nullable();
+        });
+
+    }
+
+    public function down()
+    {
+        $capsule = new Capsule();
+        $capsule::schema()->dropIfExists('network_shares');
+    }
+}
