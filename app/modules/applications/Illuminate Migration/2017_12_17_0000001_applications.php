c6ec2d8bcb4cf49a96733530c556804fac560171
tuxudo <John@Thunderchild.local>
2017-12-19 15:05:32 -0500

    Updated v3 migration file

--- app/modules/applications/Illuminate Migration/2017_12_17_0000001_applications.php
+++ app/modules/applications/Illuminate Migration/2017_12_17_0000001_applications.php
@@ -11,23 +11,23 @@ class Applications extends Migration
         $capsule::schema()->create('applications', function (Blueprint $table) {
             $table->increments('id');
             $table->string('serial_number')->unique();
-            $table->string('name')->nullable();
-            $table->text('path')->nullable();
-            $table->bigInteger('last_modified')->nullable();
-            $table->string('obtained_from')->nullable();
-            $table->string('runtime_environment')->nullable();
-            $table->string('version')->nullable();
-            $table->text('info')->nullable();
-            $table->string('signed_by')->nullable();
+            $table->string('name');
+            $table->text('path');
+            $table->bigInteger('last_modified');
+            $table->string('obtained_from');
+            $table->string('runtime_environment');
+            $table->string('version');
+            $table->text('info');
+            $table->string('signed_by');
             $table->boolean('has64bit')->default(0);
 
-            $table->index('name', 'applications_name');
-            $table->index('last_modified', 'applications_last_modified');
-            $table->index('obtained_from', 'applications_obtained_from');
-            $table->index('runtime_environment', 'applications_runtime_environment');
-            $table->index('version', 'applications_version');
-            $table->index('signed_by', 'applications_signed_by');
-            $table->index('has64bit', 'applications_has64bit');
+            $table->index('name');
+            $table->index('last_modified');
+            $table->index('obtained_from');
+            $table->index('runtime_environment');
+            $table->index('version');
+            $table->index('signed_by');
+            $table->index('has64bit');
         });
     }
     
