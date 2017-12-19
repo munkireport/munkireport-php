e28c03f4149aacc109c9b972269f1f4499fc9344
Mosen <mosen@users.noreply.github.com>
2017-12-19 20:57:18 +1100

    no need for special index names

--- app/modules/ard/migrations/2017_02_09_234100_ard.php
+++ app/modules/ard/migrations/2017_02_09_234100_ard.php
@@ -10,18 +10,16 @@ class Ard extends Migration
         $capsule = new Capsule();
         $capsule::schema()->create('ard', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('serial_number');
+            $table->string('serial_number')->unique();
             $table->string('Text1')->nullable();
             $table->string('Text2')->nullable();
             $table->string('Text3')->nullable();
             $table->string('Text4')->nullable();
 
-            $table->index('Text1', 'ard_Text1');
-            $table->index('Text2', 'ard_Text2');
-            $table->index('Text3', 'ard_Text3');
-            $table->index('Text4', 'ard_Text4');
-
-            $table->unique('serial_number', 'ard_serial_number_unique');
+            $table->index('Text1');
+            $table->index('Text2');
+            $table->index('Text3');
+            $table->index('Text4');
             // $table->timestamps();
         });
     }
