a3aa496fd2b1d6c893eaee5672c03d34df6fb468
tuxudo <John@Thunderchild.local>
2017-12-19 15:12:43 -0500

    Update v3 migration

--- app/modules/devtools/Illuminate Migration/2017_12_17_0000001_devtools.php
+++ app/modules/devtools/Illuminate Migration/2017_12_17_0000001_devtools.php
@@ -11,29 +11,29 @@ class Devtools extends Migration
         $capsule::schema()->create('devtools', function (Blueprint $table) {
             $table->increments('id');
             $table->string('serial_number')->unique();
-            $table->string('cli_tools')->nullable();
-            $table->string('dashcode_version')->nullable();
-            $table->text('devtools_path')->nullable();
-            $table->string('devtools_version')->nullable();
-            $table->string('instruments_version')->nullable();
-            $table->string('interface_builder_version')->nullable();
-            $table->text('ios_sdks')->nullable();
-            $table->text('ios_simulator_sdks')->nullable();
-            $table->text('macos_sdks')->nullable();
-            $table->text('tvos_sdks')->nullable();
-            $table->text('tvos_simulator_sdks')->nullable();
-            $table->text('watchos_sdks')->nullable();
-            $table->text('watchos_simulator_sdks')->nullable();
-            $table->string('xcode_version')->nullable();
-            $table->string('xquartz')->nullable();
+            $table->string('cli_tools');
+            $table->string('dashcode_version');
+            $table->text('devtools_path');
+            $table->string('devtools_version');
+            $table->string('instruments_version');
+            $table->string('interface_builder_version');
+            $table->text('ios_sdks');
+            $table->text('ios_simulator_sdks');
+            $table->text('macos_sdks');
+            $table->text('tvos_sdks');
+            $table->text('tvos_simulator_sdks');
+            $table->text('watchos_sdks');
+            $table->text('watchos_simulator_sdks');
+            $table->string('xcode_version');
+            $table->string('xquartz');
 
-            $table->index('cli_tools', 'devtools_cli_tools');
-            $table->index('dashcode_version', 'devtools_dashcode_version');
-            $table->index('devtools_version', 'devtools_devtools_version');
-            $table->index('instruments_version', 'devtools_instruments_version');
-            $table->index('interface_builder_version', 'devtools_interface_builder_version');
-            $table->index('xcode_version', 'devtools_xcode_version');
-            $table->index('xquartz', 'devtools_xquartz');
+            $table->index('cli_tools');
+            $table->index('dashcode_version');
+            $table->index('devtools_version');
+            $table->index('instruments_version');
+            $table->index('interface_builder_version');
+            $table->index('xcode_version');
+            $table->index('xquartz');
         });
     }
     
