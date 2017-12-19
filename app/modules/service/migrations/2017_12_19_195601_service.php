319c4d40364c152c3b9eb0ff199deb6296ece10e
Mosen <mosen@users.noreply.github.com>
2017-12-19 19:57:45 +1100

    service migration

---
+++ app/modules/service/migrations/2017_12_19_195601_service.php
@@ -0,0 +1,26 @@
+<?php
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class Service extends Migration
+{
+    public function up()
+    {
+        $capsule = new Capsule();
+        $capsule::schema()->create('service', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('serial_number')->nullable();
+            $table->string('service_name');
+            $table->string('service_state');
+            $table->bigInteger('timestamp');
+        });
+
+    }
+
+    public function down()
+    {
+        $capsule = new Capsule();
+        $capsule::schema()->dropIfExists('service');
+    }
+}
