4f032c3c50588c237748d296528ef5114dd22bf1
tuxudo <John@Thunderchild.local>
2017-12-19 15:22:23 -0500

    Remove unique

--- app/modules/caching/Illuminate Migration/2017_12_17_0000001_caching.php
+++ app/modules/caching/Illuminate Migration/2017_12_17_0000001_caching.php
@@ -10,7 +10,7 @@ class Caching extends Migration
         $capsule = new Capsule();
         $capsule::schema()->create('caching', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('serial_number')->unique();
+            $table->string('serial_number');
             $table->string('collectiondate');
             $table->string('expirationdate');
             $table->bigInteger('collectiondateepoch');
