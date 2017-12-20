830a6048ca5998d8a96bba921fd8bb4b5317e587
abn290 <abn290@vu.nl>
2017-12-20 12:45:45 +0100

    Remove sample script and fix install.sh for fv_escrow

--- app/modules/filevault_escrow/scripts/install.sh
+++ app/modules/filevault_escrow/scripts/install.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-setreportpref "filevault_escrow" "/private/var/root/crypt_output.plist"
-=======
 # crypt output file
 CRYPT_OUTPUT_PLIST="/var/root/crypt_output.plist"
 
 # Set preference to include this file in the preflight check
 setreportpref "filevault_escrow" "${CRYPT_OUTPUT_PLIST}"
->>>>>>> feature/illuminate_database_schema
