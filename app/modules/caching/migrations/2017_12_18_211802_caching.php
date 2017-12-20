6b31a979f912b646dd90acf6ba8caea7c5f3d701
tuxudo <John@Thunderchild.local>
2017-12-20 12:35:55 -0500

    Updated for latest v3 changes

--- app/modules/caching/migrations/2017_12_18_211802_caching.php
+++ app/modules/caching/migrations/2017_12_18_211802_caching.php
@@ -10,8 +10,7 @@ class Caching extends Migration
         $capsule = new Capsule();
         $capsule::schema()->create('caching', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('serial_number')->nullable();
-
+            $table->string('serial_number');
             $table->string('collectiondate')->nullable();
             $table->string('expirationdate')->nullable();
             $table->bigInteger('collectiondateepoch')->nullable();
@@ -36,6 +35,42 @@ class Caching extends Migration
             $table->bigInteger('bytesimportedbyhttp')->nullable();
             $table->bigInteger('importsbyxpc')->nullable();
             $table->bigInteger('importsbyhttp')->nullable();
+            $table->integer('activated')->nullable();
+            $table->integer('active')->nullable();
+            $table->string('cachestatus')->nullable();
+            $table->bigInteger('appletvsoftware')->nullable();
+            $table->bigInteger('macsoftware')->nullable();
+            $table->bigInteger('iclouddata')->nullable();
+            $table->bigInteger('iossoftware')->nullable();
+            $table->bigInteger('booksdata')->nullable();
+            $table->bigInteger('itunesudata')->nullable();
+            $table->bigInteger('moviesdata')->nullable();
+            $table->bigInteger('musicdata')->nullable();
+            $table->bigInteger('otherdata')->nullable();
+            $table->bigInteger('cachefree')->nullable();
+            $table->bigInteger('cachelimit')->nullable();
+            $table->bigInteger('cacheused')->nullable();
+            $table->bigInteger('personalcachefree')->nullable();
+            $table->bigInteger('personalcachelimit')->nullable();
+            $table->bigInteger('personalcacheused')->nullable();
+            $table->integer('port')->nullable();
+            $table->text('publicaddress')->nullable();
+            $table->text('privateaddresses')->nullable();
+            $table->integer('registrationstatus')->nullable();
+            $table->string('registrationerror')->nullable();
+            $table->string('registrationresponsecode')->nullable();
+            $table->integer('restrictedmedia')->nullable();
+            $table->string('serverguid')->nullable();
+            $table->string('startupstatus')->nullable();
+            $table->bigInteger('totalbytesdropped')->nullable();
+            $table->bigInteger('totalbytesimported')->nullable();
+            $table->bigInteger('totalbytesreturnedtochildren')->nullable();
+            $table->bigInteger('totalbytesreturnedtoclients')->nullable();
+            $table->bigInteger('totalbytesreturnedtopeers')->nullable();
+            $table->bigInteger('totalbytesstoredfromorigin')->nullable();
+            $table->bigInteger('totalbytesstoredfromparents')->nullable();
+            $table->bigInteger('totalbytesstoredfrompeers')->nullable();
+            $table->text('reachability')->nullable();
 
             $table->index('serial_number');
             $table->index('collectiondate');
@@ -43,10 +78,18 @@ class Caching extends Migration
             $table->index('bytesfromcachetoclients');
             $table->index('bytesfrompeerstoclients');
             $table->index('bytesfromorigintoclients');
-            // $table->timestamps();
+            $table->index('activated');
+            $table->index('active');
+            $table->index('cachestatus');
+            $table->index('totalbytesreturnedtoclients');
+            $table->index('totalbytesreturnedtochildren');
+            $table->index('totalbytesreturnedtopeers');
+            $table->index('totalbytesstoredfromorigin');
+            $table->index('totalbytesstoredfromparents');
+            $table->index('totalbytesstoredfrompeers');
         });
     }
-
+    
     public function down()
     {
         $capsule = new Capsule();
