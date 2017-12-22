9fe19a09ec335c669d02d056dd4336fb054059e6
abn290 <abn290@vu.nl>
2017-12-22 14:34:30 +0100

    Clean up security migration

--- app/modules/security/migrations/2017_02_09_234938_security.php
+++ app/modules/security/migrations/2017_02_09_234938_security.php
@@ -12,25 +12,22 @@ class Security extends Migration
         $capsule::schema()->create('security', function (Blueprint $table) {
             $table->increments('id');
 
-            $table->string('serial_number')->nullable();
-            $table->string('gatekeeper')->nullable();
-            $table->string('sip')->nullable();
-            $table->string('ssh_users')->nullable();
-            $table->string('ard_users')->nullable();
-            $table->string('firmwarepw')->nullable();
-            $table->string('firewall_state')->nullable();
-            $table->string('skel_state')->nullable();
+            $table->string('serial_number')->unique();
+            $table->string('gatekeeper');
+            $table->string('sip');
+            $table->string('ssh_users');
+            $table->string('ard_users');
+            $table->string('firmwarepw');
+            $table->string('firewall_state');
+            $table->string('skel_state');
 
-//            $table->timestamps();
-
-            $table->index('ard_users');
-            $table->index('firewall_state');
-            $table->index('firmwarepw');
             $table->index('gatekeeper');
-            $table->index('serial_number');
             $table->index('sip');
-            $table->index('skel_state');
             $table->index('ssh_users');
+            $table->index('ard_users');
+            $table->index('firmwarepw');
+            $table->index('firewall_state');
+            $table->index('skel_state');
         });
     }
     
