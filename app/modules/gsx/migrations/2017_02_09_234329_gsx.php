6db6937f3468f71d4af472e4c9450f462d492977
abn290 <abn290@vu.nl>
2017-12-23 15:25:06 +0100

    FIx gsx migration

--- app/modules/gsx/migrations/2017_02_09_234329_gsx.php
+++ app/modules/gsx/migrations/2017_02_09_234329_gsx.php
@@ -63,7 +63,7 @@ class Gsx extends Migration
                     warrantystatus,
                     coverageenddate,
                     coveragestartdate,
-                    'daysremaining,
+                    daysremaining,
                     estimatedpurchasedate,
                     purchasecountry,
                     registrationdate,
