6245a2c0462ea0b59be304ced82ec106f3221832
abn290 <abn290@vu.nl>
2017-12-23 15:38:04 +0100

    Upgrade data for location

--- app/modules/location/migrations/2017_02_09_234352_location.php
+++ app/modules/location/migrations/2017_02_09_234352_location.php
@@ -6,10 +6,25 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 
 class Location extends Migration
 {
+    private $tableName = 'location';
+    private $tableNameV2 = 'location_v2';
+
     public function up()
     {
         $capsule = new Capsule();
-        $capsule::schema()->create('location', function (Blueprint $table) {
+        $migrateData = false;
+
+        if ($capsule::schema()->hasTable($this->tableNameV2)) {
+            // Migration already failed before, but didnt finish
+            throw new Exception("previous failed migration exists");
+        }
+
+        if ($capsule::schema()->hasTable($this->tableName)) {
+            $capsule::schema()->rename($this->tableName, $this->tableNameV2);
+            $migrateData = true;
+        }
+
+        $capsule::schema()->create($this->tableName, function (Blueprint $table) {
             $table->increments('id');
 
             $table->string('serial_number')->unique();
@@ -28,13 +43,35 @@ class Location extends Migration
             $table->index('address');
             $table->index('currentstatus');
 
-            $table->timestamps();
+            if ($migrateData) {
+                $capsule::select("INSERT INTO 
+                    $this->tableName
+                SELECT
+                    id,
+                    serial_number,
+                    address,
+                    altitude,
+                    currentstatus,
+                    ls_enabled,
+                    lastlocationrun,
+                    lastrun,
+                    latitude,
+                    latitudeaccuracy,
+                    longitude,
+                    longitudeaccuracy,
+                    stalelocation
+                FROM
+                    $this->tableNameV2");
+            }
         });
     }
 
     public function down()
     {
         $capsule = new Capsule();
-        $capsule::schema()->dropIfExists('location');
+        $capsule::schema()->dropIfExists($this->tableName);
+        if ($capsule::schema()->hasTable($this->tableNameV2)) {
+            $capsule::schema()->rename($this->tableNameV2, $this->tableName);
+        }
     }
 }
