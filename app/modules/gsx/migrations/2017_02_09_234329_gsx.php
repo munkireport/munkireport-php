bcc24f6f3faa0ff4b74967accb2e0f20a0f2e268
abn290 <abn290@vu.nl>
2017-12-23 11:17:14 +0100

    Upgrade data for gsx

--- app/modules/gsx/migrations/2017_02_09_234329_gsx.php
+++ app/modules/gsx/migrations/2017_02_09_234329_gsx.php
@@ -5,10 +5,25 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 
 class Gsx extends Migration
 {
+    private $tableName = 'gsx';
+    private $tableNameV2 = 'gsx_v2';
+
     public function up()
     {
         $capsule = new Capsule();
-        $capsule::schema()->create('gsx', function (Blueprint $table) {
+        $migrateData = false;
+
+        if ($capsule::schema()->hasTable($this->tableNameV2)) {
+            // Migration already failed before, but didnt finish
+            throw new Exception("previous failed migration exists");
+        }
+
+        if ($capsule::schema()->hasTable($this->tableName)) {
+            $capsule::schema()->rename($this->tableName, $this->tableNameV2);
+            $migrateData = true;
+        }
+
+        $capsule::schema()->create($this->tableName, function (Blueprint $table) {
             $table->increments('id');
 
             $table->string('serial_number')->unique()->nullable();
@@ -38,13 +53,44 @@ class Gsx extends Migration
             $table->index('estimatedpurchasedate');
             $table->index('isvintage');
             $table->index('warrantystatus');
-//            $table->timestamps();
+            
+            if ($migrateData) {
+                $capsule::select("INSERT INTO 
+                    $this->tableName
+                SELECT
+                    id,
+                    serial_number,
+                    warrantystatus,
+                    coverageenddate,
+                    coveragestartdate,
+                    'daysremaining,
+                    estimatedpurchasedate,
+                    purchasecountry,
+                    registrationdate,
+                    productdescription,
+                    configdescription,
+                    contractcoverageenddate,
+                    contractcoveragestartdate,
+                    contracttype,
+                    laborcovered,
+                    partcovered,
+                    warrantyreferenceno,
+                    isloaner,
+                    warrantymod,
+                    isvintage,
+                    isobsolete'
+                FROM
+                    $this->tableNameV2");
+            }
         });
     }
     
     public function down()
     {
         $capsule = new Capsule();
-        $capsule::schema()->dropIfExists('gsx');
+        $capsule::schema()->dropIfExists($this->tableName);
+        if ($capsule::schema()->hasTable($this->tableNameV2)) {
+            $capsule::schema()->rename($this->tableNameV2, $this->tableName);
+        }
     }
 }
