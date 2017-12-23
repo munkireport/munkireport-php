4b2a42f5271ba01f2c9283b84fc6b69625b499fa
abn290 <abn290@vu.nl>
2017-12-23 15:29:42 +0100

    Upgrade data for inventory, localadmin

--- app/modules/localadmin/migrations/2017_02_09_234346_localadmin.php
+++ app/modules/localadmin/migrations/2017_02_09_234346_localadmin.php
@@ -5,23 +5,51 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 
 class Localadmin extends Migration
 {
+    private $tableName = 'localadmin';
+    private $tableNameV2 = 'localadmin_v2';
+
     public function up()
     {
         $capsule = new Capsule();
-        $capsule::schema()->create('localadmin', function (Blueprint $table) {
+        $migrateData = false;
+
+        if ($capsule::schema()->hasTable($this->tableNameV2)) {
+            // Migration already failed before, but didnt finish
+            throw new Exception("previous failed migration exists");
+        }
+
+        if ($capsule::schema()->hasTable($this->tableName)) {
+            $capsule::schema()->rename($this->tableName, $this->tableNameV2);
+            $migrateData = true;
+        }
+
+        $capsule::schema()->create($this->tableName, function (Blueprint $table) {
             $table->increments('id');
 
             $table->string('serial_number')->unique();
             $table->string('users');
 
             $table->index('users');
-
+            
+            if ($migrateData) {
+                $capsule::select("INSERT INTO 
+                    $this->tableName
+                SELECT
+                    id,
+                    serial_number,
+                    users
+                FROM
+                    $this->tableNameV2");
+            }
         });
     }
     
     public function down()
     {
         $capsule = new Capsule();
-        $capsule::schema()->dropIfExists('localadmin');
+        $capsule::schema()->dropIfExists($this->tableName);
+        if ($capsule::schema()->hasTable($this->tableNameV2)) {
+            $capsule::schema()->rename($this->tableNameV2, $this->tableName);
+        }
     }
 }
