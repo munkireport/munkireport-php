1b367c98baa5e58fb51293fa9b2a6349924e1213
abn290 <abn290@vu.nl>
2017-12-24 09:50:47 +0100

    Upgrade data for service

--- app/modules/service/migrations/2017_12_19_195601_service.php
+++ app/modules/service/migrations/2017_12_19_195601_service.php
@@ -5,10 +5,25 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 
 class Service extends Migration
 {
+    private $tableName = 'service';
+    private $tableNameV2 = 'service_v2';
+
     public function up()
     {
         $capsule = new Capsule();
-        $capsule::schema()->create('service', function (Blueprint $table) {
+        $migrateData = false;
+
+        if ($capsule::schema()->hasTable($this->tableNameV2)) {
+            // Migration already failed before, but didnt finish
+            throw new Exception("previous failed migration exists");
+        }
+
+        if ($capsule::schema()->hasTable($this->tableName)) {
+            $capsule::schema()->rename($this->tableName, $this->tableNameV2);
+            $migrateData = true;
+        }
+
+        $capsule::schema()->create($this->tableName, function (Blueprint $table) {
             $table->increments('id');
             $table->string('serial_number')->nullable();
             $table->string('service_name');
@@ -19,11 +34,27 @@ class Service extends Migration
             $table->index('service_name');
             $table->index('service_state');
         });
+
+        if ($migrateData) {
+            $capsule::select("INSERT INTO 
+                $this->tableName
+            SELECT
+                id,
+                serial_number,
+                service_name,
+                service_state,
+                timestamp
+            FROM
+                $this->tableNameV2");
+        }
     }
 
     public function down()
     {
         $capsule = new Capsule();
-        $capsule::schema()->dropIfExists('service');
+        $capsule::schema()->dropIfExists($this->tableName);
+        if ($capsule::schema()->hasTable($this->tableNameV2)) {
+            $capsule::schema()->rename($this->tableNameV2, $this->tableName);
+        }
     }
 }
