b375f8a32acb164693379a41cf583f9776214826
abn290 <abn290@vu.nl>
2018-01-17 10:42:10 +0100

    Bump build version

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -3,7 +3,7 @@
 use munkireport\models\Machine_group, munkireport\lib\Modules;
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '3.0.0.2796';
+$GLOBALS['version'] = '3.0.0.3219';
 
 // Return version without commit count
 function get_version()
