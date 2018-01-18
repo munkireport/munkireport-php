06d8e040603dda3469a2671b48e1e652c1049a2a
Arjen van Bochoven <abn290@vu.nl>
2018-01-18 11:10:37 +0100

    Limit event.module to 50 chars

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -3,7 +3,7 @@
 use munkireport\models\Machine_group, munkireport\lib\Modules;
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '3.0.0.3227';
+$GLOBALS['version'] = '3.0.0.3228';
 
 // Return version without commit count
 function get_version()
