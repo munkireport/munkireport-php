a194334b01f74967f40031cdae594d082ecf2038
eholtam <poundbangbash@users.noreply.github.com>
2018-01-19 16:10:17 -0600

    Commit correct migration code

--- app/modules/security/migrations/2018_01_19_234938_security.php
+++ app/modules/security/migrations/2018_01_19_234938_security.php
@@ -1,25 +1,32 @@
 <?php
+
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Capsule\Manager as Capsule;
-use Illuminate\Support\Facades\Schema;
-
 
 class Security extends Migration
-{
+{    
     private $tableName = 'security';
-	
+
     public function up()
     {
-		Schema::table($tableName, function($table) {
+    
+		$capsule = new Capsule();
+
+        $capsule::schema()->table($this->tableName, function (Blueprint $table) {
 			$table->string('ssh_groups')->after('sip');
+						
         });
     }
 
     public function down()
     {
-		Schema::table($tableName, function($table) {
-			$table->dropColumn($columnName);
+
+		$capsule = new Capsule();
+
+        $capsule::schema()->table($this->tableName, function (Blueprint $table) {
+			$table->dropColumn('ssh_groups');
         });
     }
 }
