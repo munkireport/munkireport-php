545edb6283da73b3525c8ca84edc1442ab3a8713
abn290 <abn290@vu.nl>
2018-01-24 14:46:42 +0100

    FIx AuthLDAP #944

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -3,7 +3,7 @@
 use munkireport\models\Machine_group, munkireport\lib\Modules;
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '3.0.0.3233';
+$GLOBALS['version'] = '3.0.0.3236';
 
 // Return version without commit count
 function get_version()
