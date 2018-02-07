5e574fa6f75c5b094c46d6297ba4fc41a53b6961
tuxudo <John@Thunderchild.local>
2018-02-07 17:42:01 -0500

    Changed class name

--- app/modules/wifi/migrations/2018_2_8_000001_wifi.php
+++ app/modules/wifi/migrations/2018_2_8_000001_wifi.php
@@ -3,7 +3,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Capsule\Manager as Capsule;
 
-class Wifi extends Migration
+class Wifi201828000001 extends Migration
 {
     private $tableName = 'wifi';
 
