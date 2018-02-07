431814906b393a8819207bc85b00266a3a6412cc
tuxudo <John@Thunderchild.local>
2018-02-07 14:24:01 -0500

    Fixed migrations
    Tested with MR v3.0 final

--- app/modules/supported_os/provides.php
+++ app/modules/supported_os/provides.php
@@ -2,7 +2,7 @@
 
 return array(
     'listings' => array(
-        'supported_os' => array('view' => 'supported_os_listing', 'i18n' => 'supported_os.listingtitle'),
+        'supported_os' => array('view' => 'supported_os_listing', 'i18n' => 'supported_os.highest_supported'),
     ),
     'widgets' => array(
         'supported_os' => array('view' => 'supported_os_widget'),
