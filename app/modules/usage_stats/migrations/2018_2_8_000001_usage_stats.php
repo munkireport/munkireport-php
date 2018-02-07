d10c0ec5063392db31ec0e4795a71eadc2a9b8d7
tuxudo <John@Thunderchild.local>
2018-02-07 16:52:31 -0500

    Fixed migrations

--- app/modules/usage_stats/migrations/2018_2_8_000001_usage_stats.php
+++ app/modules/usage_stats/migrations/2018_2_8_000001_usage_stats.php
@@ -5,71 +5,256 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 
 class UsageStats extends Migration
 {
+    
     private $tableName = 'usage_stats';
-
+    private $tableNamebackup = 'usage_stats_backup';
+    
     public function up()
     {
         $capsule = new Capsule();
-    
-        $capsule::schema()->table($this->tableName, function (Blueprint $table) {
-                        
-            $table->double('ibyte_rate')->nullable()->change();
-            $table->bigInteger('ibytes')->nullable()->change();
-            $table->double('ipacket_rate')->nullable()->change();
-            $table->bigInteger('ipackets')->nullable()->change();
-            $table->double('obyte_rate')->nullable()->change();
-            $table->bigInteger('obytes')->nullable()->change();
-            $table->double('opacket_rate')->nullable()->change();
-            $table->bigInteger('opackets')->nullable()->change();
-            $table->double('rbytes_per_s')->nullable()->change();
-            $table->double('rops_per_s')->nullable()->change();
-            $table->double('wbytes_per_s')->nullable()->change();
-            $table->double('wops_per_s')->nullable()->change();
-            $table->bigInteger('rbytes_diff')->nullable()->change();
-            $table->bigInteger('rops_diff')->nullable()->change();
-            $table->bigInteger('wbytes_diff')->nullable()->change();
-            $table->bigInteger('wops_diff')->nullable()->change();
-            $table->double('package_watts')->nullable()->change();
-            $table->double('package_joules')->nullable()->change();
-            $table->bigInteger('freq_hz')->nullable()->change(); // CPU
-            $table->double('freq_ratio')->nullable()->change(); // CPU
-            $table->bigInteger('gpu_freq_hz')->nullable()->change();
-            $table->double('gpu_freq_mhz')->nullable()->change();
-            $table->double('gpu_freq_ratio')->nullable()->change();
-            $table->double('gpu_busy')->nullable()->change();
-         
+        $migrateData = false;
+        
+        if ($capsule::schema()->hasTable($this->tableName)) {
+            $capsule::schema()->rename($this->tableName, $this->tableNamebackup);
+            $migrateData = true;
+        }
+                
+        $capsule::schema()->create($this->tableName, function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('serial_number')->unique();
+            $table->bigInteger('timestamp');
+            $table->string('thermal_pressure')->nullable();
+            $table->integer('backlight_max')->nullable();
+            $table->integer('backlight_min')->nullable();
+            $table->integer('backlight')->nullable();
+            $table->integer('keyboard_backlight')->nullable();
+            $table->double('ibyte_rate')->nullable();
+            $table->bigInteger('ibytes')->nullable();
+            $table->double('ipacket_rate')->nullable();
+            $table->bigInteger('ipackets')->nullable();
+            $table->double('obyte_rate')->nullable();
+            $table->bigInteger('obytes')->nullable();
+            $table->double('opacket_rate')->nullable();
+            $table->bigInteger('opackets')->nullable();
+            $table->double('rbytes_per_s')->nullable();
+            $table->double('rops_per_s')->nullable();
+            $table->double('wbytes_per_s')->nullable();
+            $table->double('wops_per_s')->nullable();
+            $table->bigInteger('rbytes_diff')->nullable();
+            $table->bigInteger('rops_diff')->nullable();
+            $table->bigInteger('wbytes_diff')->nullable();
+            $table->bigInteger('wops_diff')->nullable();
+            $table->double('package_watts')->nullable();
+            $table->double('package_joules')->nullable();
+            $table->bigInteger('freq_hz')->nullable(); // CPU
+            $table->double('freq_ratio')->nullable(); // CPU
+            $table->string('gpu_name')->nullable();
+            $table->bigInteger('gpu_freq_hz')->nullable();
+            $table->double('gpu_freq_mhz')->nullable();
+            $table->double('gpu_freq_ratio')->nullable();
+            $table->double('gpu_busy')->nullable();
+            $table->string('kern_bootargs')->nullable();
+            
+            $table->index('backlight_max');
+            $table->index('backlight_min');
+            $table->index('backlight');
+            $table->index('keyboard_backlight');
+            $table->index('ibyte_rate');
+            $table->index('ibytes');
+            $table->index('ipacket_rate');
+            $table->index('ipackets');
+            $table->index('obyte_rate');
+            $table->index('obytes');
+            $table->index('opacket_rate');
+            $table->index('opackets');
+            $table->index('rbytes_per_s');
+            $table->index('rops_per_s');
+            $table->index('wbytes_per_s');
+            $table->index('wops_per_s');
+            $table->index('rbytes_diff');
+            $table->index('rops_diff');
+            $table->index('wbytes_diff');
+            $table->index('wops_diff');
+            $table->index('thermal_pressure');
+            $table->index('package_watts');
+            $table->index('package_joules');
+            $table->index('freq_hz');
+            $table->index('freq_ratio');
+            $table->index('gpu_name');
+            $table->index('gpu_freq_hz');
+            $table->index('gpu_freq_mhz');
+            $table->index('gpu_freq_ratio');
+            $table->index('gpu_busy');
+            $table->index('kern_bootargs');
         });
-     }
+        
+        if ($migrateData) {
+            $capsule::select("INSERT INTO 
+                $this->tableName
+            SELECT
+                            id,
+                serial_number,
+                timestamp,
+                thermal_pressure,
+                backlight_max,
+                backlight_min,
+                backlight,
+                keyboard_backlight,
+                ibyte_rate,
+                ibytes,
+                ipacket_rate,
+                ipackets,
+                obyte_rate,
+                obytes,
+                opacket_rate,
+                opackets,
+                rbytes_per_s,
+                rops_per_s,
+                wbytes_per_s,
+                wops_per_s,
+                rbytes_diff,
+                rops_diff,
+                wbytes_diff,
+                wops_diff,
+                package_watts,
+                package_joules,
+                freq_hz,
+                freq_ratio,
+                gpu_name,
+                gpu_freq_hz,
+                gpu_freq_mhz,
+                gpu_freq_ratio,
+                gpu_busy,
+                kern_bootargs
+            FROM
+                $this->tableNamebackup");
+            
+        $capsule::schema()->dropIfExists($this->tableNamebackup);
+        }
+        
+    }
     
     public function down()
     {
-        $capsule = new Capsule();
-        $capsule::schema()->table($this->tableName, function (Blueprint $table) {
-            $table->float('ibyte_rate')->nullable()->change();
-            $table->float('ibytes')->nullable()->change();
-            $table->float('ipacket_rate')->nullable()->change();
-            $table->float('ipackets')->nullable()->change();
-            $table->float('obyte_rate')->nullable()->change();
-            $table->float('obytes')->nullable()->change();
-            $table->float('opacket_rate')->nullable()->change();
-            $table->float('opackets')->nullable()->change();
-            $table->float('rbytes_per_s')->nullable()->change();
-            $table->float('rops_per_s')->nullable()->change();
-            $table->float('wbytes_per_s')->nullable()->change();
-            $table->float('wops_per_s')->nullable()->change();
-            $table->float('rbytes_diff')->nullable()->change();
-            $table->float('rops_diff')->nullable()->change();
-            $table->float('wbytes_diff')->nullable()->change();
-            $table->float('wops_diff')->nullable()->change();
-            $table->float('package_watts')->nullable()->change();
-            $table->float('package_joules')->nullable()->change();
-            $table->float('freq_hz')->nullable()->change(); // CPU
-            $table->float('freq_ratio')->nullable()->change(); // CPU
-            $table->float('gpu_freq_hz')->nullable()->change();
-            $table->float('gpu_freq_mhz')->nullable()->change();
-            $table->float('gpu_freq_ratio')->nullable()->change();
-            $table->float('gpu_busy')->nullable()->change();
-           
+$capsule = new Capsule();
+        $migrateData = false;
+        
+        if ($capsule::schema()->hasTable($this->tableName)) {
+            $capsule::schema()->rename($this->tableName, $this->tableNamebackup);
+            $migrateData = true;
+        }
+                
+        $capsule::schema()->create($this->tableName, function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('serial_number')->unique();
+            $table->bigInteger('timestamp');
+            $table->string('thermal_pressure')->nullable();
+            $table->integer('backlight_max')->nullable();
+            $table->integer('backlight_min')->nullable();
+            $table->integer('backlight')->nullable();
+            $table->integer('keyboard_backlight')->nullable();
+            $table->float('ibyte_rate')->nullable();
+            $table->float('ibytes')->nullable();
+            $table->float('ipacket_rate')->nullable();
+            $table->float('ipackets')->nullable();
+            $table->float('obyte_rate')->nullable();
+            $table->float('obytes')->nullable();
+            $table->float('opacket_rate')->nullable();
+            $table->float('opackets')->nullable();
+            $table->float('rbytes_per_s')->nullable();
+            $table->float('rops_per_s')->nullable();
+            $table->float('wbytes_per_s')->nullable();
+            $table->float('wops_per_s')->nullable();
+            $table->float('rbytes_diff')->nullable();
+            $table->float('rops_diff')->nullable();
+            $table->float('wbytes_diff')->nullable();
+            $table->float('wops_diff')->nullable();
+            $table->float('package_watts')->nullable();
+            $table->float('package_joules')->nullable();
+            $table->float('freq_hz')->nullable(); // CPU
+            $table->float('freq_ratio')->nullable(); // CPU
+            $table->string('gpu_name')->nullable();
+            $table->float('gpu_freq_hz')->nullable();
+            $table->float('gpu_freq_mhz')->nullable();
+            $table->float('gpu_freq_ratio')->nullable();
+            $table->float('gpu_busy')->nullable();
+            $table->string('kern_bootargs')->nullable();
+            
+            $table->index('backlight_max');
+            $table->index('backlight_min');
+            $table->index('backlight');
+            $table->index('keyboard_backlight');
+            $table->index('ibyte_rate');
+            $table->index('ibytes');
+            $table->index('ipacket_rate');
+            $table->index('ipackets');
+            $table->index('obyte_rate');
+            $table->index('obytes');
+            $table->index('opacket_rate');
+            $table->index('opackets');
+            $table->index('rbytes_per_s');
+            $table->index('rops_per_s');
+            $table->index('wbytes_per_s');
+            $table->index('wops_per_s');
+            $table->index('rbytes_diff');
+            $table->index('rops_diff');
+            $table->index('wbytes_diff');
+            $table->index('wops_diff');
+            $table->index('thermal_pressure');
+            $table->index('package_watts');
+            $table->index('package_joules');
+            $table->index('freq_hz');
+            $table->index('freq_ratio');
+            $table->index('gpu_name');
+            $table->index('gpu_freq_hz');
+            $table->index('gpu_freq_mhz');
+            $table->index('gpu_freq_ratio');
+            $table->index('gpu_busy');
+            $table->index('kern_bootargs');
         });
+        
+        if ($migrateData) {
+            $capsule::select("INSERT INTO 
+                $this->tableName
+            SELECT
+                            id,
+                serial_number,
+                timestamp,
+                thermal_pressure,
+                backlight_max,
+                backlight_min,
+                backlight,
+                keyboard_backlight,
+                ibyte_rate,
+                ibytes,
+                ipacket_rate,
+                ipackets,
+                obyte_rate,
+                obytes,
+                opacket_rate,
+                opackets,
+                rbytes_per_s,
+                rops_per_s,
+                wbytes_per_s,
+                wops_per_s,
+                rbytes_diff,
+                rops_diff,
+                wbytes_diff,
+                wops_diff,
+                package_watts,
+                package_joules,
+                freq_hz,
+                freq_ratio,
+                gpu_name,
+                gpu_freq_hz,
+                gpu_freq_mhz,
+                gpu_freq_ratio,
+                gpu_busy,
+                kern_bootargs
+            FROM
+                $this->tableNamebackup");
+            
+        $capsule::schema()->dropIfExists($this->tableNamebackup);
+        }
     }
 }
