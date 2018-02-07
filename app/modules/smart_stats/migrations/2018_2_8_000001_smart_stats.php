b47ed56e3bd39e891b6ead41fba88a0beeab0e30
tuxudo <John@Thunderchild.local>
2018-02-07 17:42:32 -0500

    Changed class name

--- app/modules/smart_stats/migrations/2018_2_8_000001_smart_stats.php
+++ app/modules/smart_stats/migrations/2018_2_8_000001_smart_stats.php
@@ -3,7 +3,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Capsule\Manager as Capsule;
 
-class SmartStats extends Migration
+class SmartStats201828000001 extends Migration
 {
     private $tableName = 'smart_stats';
 
