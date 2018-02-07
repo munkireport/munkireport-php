4993cb291f2806f2aa4fd33a4984f2ed9ac63dae
tuxudo <John@Thunderchild.local>
2018-02-07 17:46:15 -0500

    Changed class name

--- app/modules/timemachine/migrations/2018_2_8_000001_timemachine.php
+++ app/modules/timemachine/migrations/2018_2_8_000001_timemachine.php
@@ -3,7 +3,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Capsule\Manager as Capsule;
 
-class Timemachine extends Migration
+class Timemachine201828000001 extends Migration
 {
     private $tableName = 'timemachine';
 
