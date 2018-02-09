42543c60dc1c1e35c937b8e27bd7dfa6797d557e
abn290 <abn290@vu.nl>
2018-02-09 17:54:22 +0100

    Rename power migration

--- app/modules/power/migrations/2018_02_09_165342_power_fix_nullable_columns.php
+++ app/modules/power/migrations/2018_02_09_165342_power_fix_nullable_columns.php
@@ -3,7 +3,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Capsule\Manager as Capsule;
 
-class Power201828000001 extends Migration
+class PowerFixNullableColumns extends Migration
 {
     private $tableName = 'power';
 
