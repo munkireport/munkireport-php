4e22c0a28b1bd7f62a1dd45ffe2afe72ef69a09f
tuxudo <John@Thunderchild.local>
2018-02-10 12:09:50 -0500

    Cleaned up client tab

--- app/modules/memory/views/memory_tab.php
+++ app/modules/memory/views/memory_tab.php
@@ -7,49 +7,62 @@ $(document).on('appReady', function(){
 		// Set count of memory devices
 		var skipThese = ['name'];
         var memorycapacity = 0
+        var infokeep = ''
 
 		$.each(data, function(i,d){
 			
 			// Generate rows from data
 			var rows = ''
+			var inforows = ''
 			for (var prop in d){
 				// Skip skipThese
 				if(skipThese.indexOf(prop) == -1){
-					if(prop == 'global_ecc_state' && d[prop] == 1){
-					   rows = rows + '<tr><th>'+i18n.t('memory.'+prop)+'</th><td>'+i18n.t('memory.ecc_enabled')+'</td></tr>';
-					}
-					else if(prop == 'global_ecc_state' && d[prop] == 0){
-					   rows = rows + '<tr><th>'+i18n.t('memory.'+prop)+'</th><td>'+i18n.t('memory.ecc_disabled')+'</td></tr>';
-					} 
-					else if(prop == 'global_ecc_state' && d[prop] == 2){
-					   rows = rows + '<tr><th>'+i18n.t('memory.'+prop)+'</th><td>'+i18n.t('memory.ecc_errors')+'</td></tr>';
-					} 
-					else if(prop == 'is_memory_upgradeable' && d[prop] == 1){
-					   rows = rows + '<tr><th>'+i18n.t('memory.'+prop)+'</th><td>'+i18n.t('yes')+'</td></tr>';
-					} 
-					else if(prop == 'is_memory_upgradeable' && d[prop] == 0){
-					   rows = rows + '<tr><th>'+i18n.t('memory.'+prop)+'</th><td>'+i18n.t('no')+'</td></tr>';
-					} 
-					else if(prop == 'dimm_size'){
+					if(prop == 'global_ecc_state' && d[prop] == 1 && infokeep.indexOf("global_ecc_state") === -1){
+					   inforows = inforows + '<div style="max-width:500px;"><table class="table-striped table-condensed"><tr><th>'+i18n.t('memory.'+prop)+'</th><td>'+i18n.t('memory.ecc_enabled')+'</td></tr>';
+					   infokeep = infokeep + 'global_ecc_state'
+                    }
+					else if(prop == 'global_ecc_state' && d[prop] == 0 && infokeep.indexOf("global_ecc_state") === -1){
+					   inforows = inforows + '<div style="max-width:500px;"><table class="table-striped table-condensed"><tr><th>'+i18n.t('memory.'+prop)+'</th><td>'+i18n.t('memory.ecc_disabled')+'</td></tr>';
+					   infokeep = infokeep + 'global_ecc_state'
+                    } 
+					else if(prop == 'global_ecc_state' && d[prop] == 2 && infokeep.indexOf("global_ecc_state") === -1){
+					   inforows = inforows + '<div style="max-width:500px;"><table class="table-striped table-condensed"><tr><th>'+i18n.t('memory.'+prop)+'</th><td>'+i18n.t('memory.ecc_errors')+'</td></tr>';
+					   infokeep = infokeep + 'global_ecc_state'
+                    } 
+                    else if(prop == 'global_ecc_state'&& infokeep.indexOf("global_ecc_state") !== -1){
+					   rows = rows  
+                    }
+                    else if(prop == 'is_memory_upgradeable' && d[prop] == 1 && infokeep.indexOf("is_memory_upgradeable") === -1){
+					   inforows = inforows + '<tr><th>'+i18n.t('memory.'+prop)+'</th><td>'+i18n.t('yes')+'</td></tr></table></br></div>';
+					   infokeep = infokeep + 'is_memory_upgradeable'
+                    }
+                    else if(prop == 'is_memory_upgradeable' && d[prop] == 0 && infokeep.indexOf("is_memory_upgradeable") === -1){
+					   inforows = inforows + '<tr><th>'+i18n.t('memory.'+prop)+'</th><td>'+i18n.t('no')+'</td></tr></table></br></div>';
+					   infokeep = infokeep + 'is_memory_upgradeable'
+                    } 
+                    else if(prop == 'is_memory_upgradeable' && infokeep.indexOf("is_memory_upgradeable") !== -1){
+					   rows = rows  
+                    } 
+                    else if(prop == 'dimm_size'){
 					   rows = rows + '<tr><th>'+i18n.t('memory.'+prop)+'</th><td>'+d[prop]+'</td></tr>';
-					   if(d[prop].indexOf("GB") !== -1 ){
+					   if(d[prop].indexOf("GB") !== -1){
 					        memorycapacity = memorycapacity + +d[prop].replace(/\D/g,'');
-					   }
-					}
-					else if(prop == 'dimm_status' && d[prop] == "empty"){
+					   }                    
+                    }
+                    else if(prop == 'dimm_status' && d[prop] == "empty"){
 					   rows = rows + '<tr><th>'+i18n.t('memory.'+prop)+'</th><td>'+i18n.t('memory.empty')+'</td></tr>';
-					} 
-					else if(prop == 'dimm_status' && d[prop] == "ok"){
+                    } 
+                    else if(prop == 'dimm_status' && d[prop] == "ok"){
 					   rows = rows + '<tr><th>'+i18n.t('memory.'+prop)+'</th><td>'+i18n.t('memory.ok')+'</td></tr>';
-					} 
-					else if(prop == 'dimm_status' && d[prop] == "unknown"){
+                    } 
+                    else if(prop == 'dimm_status' && d[prop] == "unknown"){
 					   rows = rows + '<tr><th>'+i18n.t('memory.'+prop)+'</th><td>'+i18n.t('unknown')+'</td></tr>';
-					} 
-					else if(d[prop] == ''){
-					   rows = rows
-					}
-					else {
-					   rows = rows + '<tr><th>'+i18n.t('memory.'+prop)+'</th><td>'+d[prop]+'</td></tr>';
+                    } 
+                    else if(d[prop] == ''){
+                        rows = rows
+                    }
+                    else {
+                        rows = rows + '<tr><th>'+i18n.t('memory.'+prop)+'</th><td>'+d[prop]+'</td></tr>';
 					}
 				}
 			}
@@ -57,12 +70,14 @@ $(document).on('appReady', function(){
             $('#memory-cnt').text(memorycapacity+" GB");
 
 			$('#memory-tab')
+
+            .append(inforows)
+
             .append($('<h4>')
                 .append($('<i>')
                     .addClass('fa fa-microchip'))
             .append(' '+d.name))
-            .append($('<div style="max-width:500px;">')
-                .addClass('table-responsive')
+            .append($('<div style="max-width:450px;">')
                 .append($('<table>')
                     .addClass('table table-striped table-condensed')
                     .append($('<tbody>')
