ba8c3310e5019addb1c105be4e82d70d75bf456e
eholtam <poundbangbash@users.noreply.github.com>
2018-02-12 11:34:25 -0600

    Extensions migration name fix (#968)
    * Fix migration file for adding teamid
    
    * migration fixup
    
    * Update 2018_02_12_164431_extensions_add_teamid.php
    
    remove drop index

---
+++ app/modules/extensions/migrations/2018_02_12_164431_extensions_add_teamid.php
@@ -0,0 +1,32 @@
+<?php
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class ExtensionsAddTeamid extends Migration
+{
+    private $tableName = 'extensions';
+
+    public function up()
+    {
+        $capsule = new Capsule();
+        $capsule::schema()->table($this->tableName, function (Blueprint $table) {
+            $table->string('teamid')->after('codesign');
+            $table->renameColumn('codesign', 'developer');
+
+            $table->index('teamid');
+            $table->index('developer');
+        });
+    }
+    
+    public function down()
+    {
+        $capsule = new Capsule();
+        $capsule::schema()->table($this->tableName, function (Blueprint $table) {
+            $table->dropColumn('teamid');
+            $table->renameColumn('developer', 'codesign');
+
+            $table->index('codesign');
+        });
+    }
+}
