e856f0357e05585654c22b6a2d821e044f4f13cb
eholtam <poundbangbash@users.noreply.github.com>
2018-02-12 13:39:49 -0600

    Rename migration to be more explicit. Add default values to columns.

--- app/modules/mdm_status/migrations/2018_02_06_000001_mdm_status_initialize.php
+++ app/modules/mdm_status/migrations/2018_02_06_000001_mdm_status_initialize.php
@@ -4,19 +4,19 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Capsule\Manager as Capsule;
 
-class MDMStatus extends Migration
+class MDMStatusInitialize extends Migration
 {    
 
     public function up()
     {
     
-		$capsule = new Capsule();
+        $capsule = new Capsule();
 
         $capsule::schema()->create('mdm_status', function (Blueprint $table) {
             $table->increments('id');
             $table->string('serial_number')->unique();
-			$table->string('mdm_enrolled');
-			$table->string('mdm_enrollment_type');
+            $table->string('mdm_enrolled')->default('');
+            $table->string('mdm_enrollment_type')->default('');
 
             $table->index('mdm_enrolled');
             $table->index('mdm_enrollment_type');
