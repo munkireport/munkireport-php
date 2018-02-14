7acf2bb5483e5a20b1ab8605d9952c80b8bee39a
abn290 <abn290@vu.nl>
2018-02-14 23:57:03 +0100

    Fix migrations
    #971

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -3,7 +3,7 @@
 use munkireport\models\Machine_group, munkireport\lib\Modules;
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '3.0.2.3312';
+$GLOBALS['version'] = '3.0.2.3314';
 
 // Return version without commit count
 function get_version()
