4762e7dac61e5f976e51ad5b9c5cb493f2a90a2d
abn290 <abn290@vu.nl>
2018-02-14 23:00:32 +0100

    Add missing indexes

---
+++ app/modules/tag/migrations/2018_02_14_215000_tag_add_index.php
@@ -0,0 +1,25 @@
+<?php
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class TagAddIndex extends Migration
+{
+    private $tableName = 'tag';
+
+    public function up()
+    {
+        $capsule = new Capsule();
+        $capsule::schema()->table($this->tableName, function (Blueprint $table) {
+            $table->index('serial_number');
+        });
+    }
+    
+    public function down()
+    {
+        $capsule = new Capsule();
+        $capsule::schema()->table($this->tableName, function (Blueprint $table) {
+            $table->dropIndex('serial_number');
+        });
+    }
+}
