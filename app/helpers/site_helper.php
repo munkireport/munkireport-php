5318244b1260fedd84ab79bdd6f3fc66fa3ab7d4
abn290 <abn290@vu.nl>
2018-02-14 23:08:56 +0100

    Fix indexes
    #971

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -3,7 +3,7 @@
 use munkireport\models\Machine_group, munkireport\lib\Modules;
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '3.0.2.3306';
+$GLOBALS['version'] = '3.0.2.3308';
 
 // Return version without commit count
 function get_version()
