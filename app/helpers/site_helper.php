bc253ef574613ccb1fc990dcce648a993e153288
abn290 <abn290@vu.nl>
2018-02-14 14:21:58 +0100

    Update version for master

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -3,7 +3,7 @@
 use munkireport\models\Machine_group, munkireport\lib\Modules;
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '3.0.2.3294';
+$GLOBALS['version'] = '3.0.1.3295';
 
 // Return version without commit count
 function get_version()
