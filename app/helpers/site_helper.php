3ca8904a5148e3f7e51976f60f9f7e90e7f392f4
abn290 <abn290@vu.nl>
2018-02-15 21:25:01 +0100

    Bump version back to 3.0.3

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -3,7 +3,7 @@
 use munkireport\models\Machine_group, munkireport\lib\Modules;
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '3.0.2.3318';
+$GLOBALS['version'] = '3.0.3.3321';
 
 // Return version without commit count
 function get_version()
