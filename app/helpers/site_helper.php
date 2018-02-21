96522adc18f1e3ccb04086cddd7bbfc9b7d439cb
abn290 <abn290@vu.nl>
2018-02-21 10:36:24 +0100

    Update README to point to adldap2 project

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -3,7 +3,7 @@
 use munkireport\models\Machine_group, munkireport\lib\Modules;
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '3.0.3.3326';
+$GLOBALS['version'] = '3.0.3.3327';
 
 // Return version without commit count
 function get_version()
