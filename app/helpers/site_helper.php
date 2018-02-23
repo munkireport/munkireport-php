ecb2bb3cf718d327b03d1335ad8574fcf46d632d
abn290 <abn290@vu.nl>
2018-02-23 19:06:05 +0100

    Remove old migrations

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -3,7 +3,7 @@
 use munkireport\models\Machine_group, munkireport\lib\Modules;
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '3.0.3.3331';
+$GLOBALS['version'] = '3.0.3.3337';
 
 // Return version without commit count
 function get_version()
