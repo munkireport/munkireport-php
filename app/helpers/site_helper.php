84da508db9a725a07a50677975c07c5cfef09176
abn290 <abn290@vu.nl>
2018-03-12 19:04:03 +0100

    Add fv key encryption migration
    Fixes #1012

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -3,7 +3,7 @@
 use munkireport\models\Machine_group, munkireport\lib\Modules;
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '3.0.3.3350';
+$GLOBALS['version'] = '3.0.3.3353';
 
 // Return version without commit count
 function get_version()
