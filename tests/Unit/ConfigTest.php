f9fc9af4c40728b2f35ba37cf873ebef6a5f049d
Mosen <mosen@users.noreply.github.com>
2018-03-28 23:16:23 +1100

    Config tests pass

--- tests/Unit/ConfigTest.php
+++ tests/Unit/ConfigTest.php
@@ -10,29 +10,29 @@ require_once __DIR__ . '/../../config_default.php';
 class ConfigTest extends TestCase
 {
     protected function setUp() {
-        $_ENV['STRINGVALUE'] = 'STRING';
-        $_ENV['BOOLVALUE'] = 'TRUE';
-        $_ENV['INTVALUE'] = '1234';
-        $_ENV['ARRAYVALUE'] = 'ONE,TWO,THREE,FOUR';
+        putenv("STRINGVALUE=STRING");
+        putenv("BOOLVALUE=TRUE");
+        putenv("INTVALUE=1234");
+        putenv("ARRAYVALUE=ONE,TWO,THREE,FOUR");
     }
 
     public function testGetEnvDefault() {
         $v = getenv_default('STRINGVALUE', 'NOPE');
-        $this->assertEquals($v, 'STRINGVALUE');
+        $this->assertEquals($v, 'STRING');
     }
 
     public function testGetEnvBool() {
-        $v = getenv_default('BOOLVALUE', false);
+        $v = getenv_default('BOOLVALUE', false, 'bool');
         $this->assertEquals(true, $v);
     }
 
     public function testGetEnvInt() {
-        $v = getenv_default('INTVALUE', 1);
+        $v = getenv_default('INTVALUE', 1, 'int');
         $this->assertEquals(1234, $v);
     }
 
     public function testGetEnvArray() {
-        $v = getenv_default('ARRAYVALUE', ['ONE','TWO']);
+        $v = getenv_default('ARRAYVALUE', ['ONE','TWO'], 'array');
         $this->assertEquals(4, count($v));
     }
 }
