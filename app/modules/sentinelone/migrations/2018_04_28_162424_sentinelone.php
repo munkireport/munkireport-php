5e65631d297c9bc106c3377bd8d84aac3d4f7711
eholtam <poundbangbash@users.noreply.github.com>
2018-04-29 01:34:47 -0500

    Migration db setup fixes.

--- app/modules/sentinelone/migrations/2018_04_28_162424_sentinelone.php
+++ app/modules/sentinelone/migrations/2018_04_28_162424_sentinelone.php
@@ -6,42 +6,36 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 class Sentinelone extends Migration
 {
 	public function up()
-	{ 
+	{
 		$capsule = new Capsule();
 		$capsule::schema()->create('sentinelone', function (Blueprint $table) {
 			$table->increments('id');
 			$table->string('serial_number');
-			$table->boolean('active-threats-present')->nullable();
-			$table->string('agent-id')->nullable();
-			$table->string('agent-install-time')->nullable();
-			$table->boolean('agent-running')->nullable();
-			$table->string('agent-version')->nullable();
-			$table->boolean('enforcing-security')->nullable();
-			$table->string('last-seen')->nullable();
-			$table->string('mgmt-url')->nullable();
-			$table->boolean('self-protection-enabled')->nullable();
+			$table->boolean('active_threats_present')->nullable();
+			$table->string('agent_id')->nullable();
+			$table->string('agent_install_time')->nullable();
+			$table->boolean('agent_running')->nullable();
+			$table->string('agent_version')->nullable();
+			$table->boolean('enforcing_security')->nullable();
+			$table->string('last_seen')->nullable();
+			$table->string('mgmt_url')->nullable();
+			$table->boolean('self_protection_enabled')->nullable();
 
-            $table->index('serial_number');
-			$table->index('active-threats-present');
-			$table->index('agent-id');
-			$table->index('agent-install-time');
-			$table->index('agent-running');
-			$table->index('agent-version');
-			$table->index('enforcing-security');
-			$table->index('last-seen');
-			$table->index('mgmt-url');
-			$table->index('self-protection-enabled');
-
-		  });
+			$table->index('serial_number');
+			$table->index('active_threats_present');
+			$table->index('agent_id');
+			$table->index('agent_install_time');
+			$table->index('agent_running');
+			$table->index('agent_version');
+			$table->index('enforcing_security');
+			$table->index('last_seen');
+			$table->index('mgmt_url');
+			$table->index('self_protection_enabled');
+	});
 	}
-
-  public function down()
-  {
-
-  $capsule = new Capsule();
-
-        $capsule = new Capsule();
-        $capsule::schema()->dropIfExists('sentinelone');
-      });
-  }
+	public function down()
+	{
+		$capsule = new Capsule();
+		$capsule::schema()->dropIfExists('sentinelone');
+	}
 }
