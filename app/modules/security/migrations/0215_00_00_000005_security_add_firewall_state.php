184c3e7fb5a969773544f1b750060a17b81c0974
Mosen <mosen@users.noreply.github.com>
2018-05-26 21:03:58 +1000

    More migrations from v2.15

--- app/modules/security/migrations/0215_00_00_000005_security_add_firewall_state.php
+++ app/modules/security/migrations/0215_00_00_000005_security_add_firewall_state.php
@@ -7,11 +7,11 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Capsule\Manager as Capsule;
 
-class SecurityAddIndexes extends Migration {
+class SecurityAddFirewallState extends Migration {
 
     use \munkireport\lib\LegacyMigrationSupport;
 
-    public static $legacySchemaVersion = 4;
+    public static $legacySchemaVersion = 5;
     public static $legacyTableName = 'security';
 
     public function up() {
@@ -20,12 +20,8 @@ class SecurityAddIndexes extends Migration {
 
         if ($legacyVersion < static::$legacySchemaVersion) {
             $capsule::schema()->table('security', function (Blueprint $table) {
-                $table->index('serial_number', 'security_serial_number');
-                $table->index('gatekeeper', 'security_gatekeeper');
-                $table->index('sip', 'security_sip');
-                $table->index('ssh_users', 'security_ssh_users');
-                $table->index('ard_users', 'security_ard_users');
-                $table->index('firmwarepw', 'security_firmwarepw');
+                $table->integer('firewall_state')->nullable();
+                $table->index('firewall_state', 'security_firewall_state');
             });
 
             $this->markLegacyMigrationRan();
@@ -39,14 +35,8 @@ class SecurityAddIndexes extends Migration {
             $capsule::schema()->table(
                 'security',
                 function (Blueprint $table) {
-                    $table->dropIndex([
-                        'security_serial_number',
-                        'security_gatekeeper',
-                        'security_sip',
-                        'security_ssh_users',
-                        'security_ard_users',
-                        'security_firmwarepw'
-                    ]);
+                    $table->dropIndex('security_firewall_state');
+                    $table->dropColumn('firewall_state');
                 }
             );
 
