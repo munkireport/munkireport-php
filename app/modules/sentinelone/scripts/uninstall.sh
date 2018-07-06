8189af6ab187da4fdd059c8f6c7105d6a1afca8f
Arjen van Bochoven <abn290@vu.nl>
2018-07-06 13:20:17 +0200

    Fix sentinel one uninstallers

--- app/modules/sentinelone/scripts/uninstall.sh
+++ app/modules/sentinelone/scripts/uninstall.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-MODULE_NAME="sentinelone"
-MODULESCRIPT="sentinelone.py"
-MODULE_CACHE_FILE="sentinelone.plist"
-
 # Remove preflight script
 rm -f "${MUNKIPATH}preflight.d/sentinelone.py"
 
