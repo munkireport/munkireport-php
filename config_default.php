9fc177d3930c61399ae57ab7584919e7e2c8883e
tuxudo <tuxudo@users.noreply.github.com>
2018-07-17 13:44:13 -0400

    Added config option for custom inactive

--- config_default.php
+++ config_default.php
@@ -530,6 +530,18 @@
 
 	/*
 	|===============================================
+	| Client inactivity
+	|===============================================
+	|
+	| Set this to be the number of days until a client shows as
+	| being inactive in the round Client Activity widget, defaul is
+	| 30 days.
+	|
+	*/
+	$conf['days_inactive'] = getenv_default(30);
+
+	/*
+	|===============================================
 	| Client passphrases
 	|===============================================
 	|
