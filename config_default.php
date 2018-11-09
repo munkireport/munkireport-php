f6ccfe0694bb9fc2ac0d6afd4b78fffa91b3ffa1
tuxudo <John@Thunderchild.local>
2018-11-09 15:10:16 -0500

    Add user_sessions filtering config

--- config_default.php
+++ config_default.php
@@ -334,6 +334,25 @@
 
 	/*
 	|===============================================
+	| User Sessions Events
+	|===============================================
+	|
+	| User Sessions will log all events. To skip some events, set
+	| the events that you want to skip to be false. By default the
+	| module also saves historical data. To disable this, set the 
+	| user_sessions_keep_historical key to false. 
+	|
+	*/
+	$conf['user_sessions_save_remote_ssh'] = TRUE;
+	$conf['user_sessions_save_login'] = TRUE;
+	$conf['user_sessions_save_logout'] = TRUE;
+	$conf['user_sessions_save_reboot'] = TRUE;
+	$conf['user_sessions_save_shutdown'] = TRUE;
+	$conf['user_sessions_keep_historical'] = TRUE;
+	$conf['user_sessions_unique_users_only'] = FALSE;
+
+	/*
+	|===============================================
 	| USB Devices
 	|===============================================
 	|
