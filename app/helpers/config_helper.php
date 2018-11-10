035e67f2f3c3bae23496d3bb4202c55d356e41b2
Arjen van Bochoven <nejra@me.com>
2018-11-10 15:28:38 +0100

    Use env() instead
    Also add automatic typehinting

--- app/helpers/config_helper.php
+++ app/helpers/config_helper.php
@@ -20,17 +20,28 @@ function initDotEnv()
 
 }
 
+// Legacy function name
+function getenv_default($key, $default = null, $typehint = null)
+{
+    env($key, $default, $typehint);
+}
+
 // Get a value from $_ENV with fallback to default
 // typehint parameter processes env var as the suggested type.
-function getenv_default($key, $default = null, $typehint = null) {
+function env($key, $default = null, $typehint = null) {
     if (getenv($key)) {
         $v = getenv($key);
+        if ( $typehint === null && $default !== null) {
+            $typehint = gettype($default);
+        }
         switch ($typehint) {
             case 'array':
                 return explode(',', $v);
             case 'bool':
+            case 'boolean':
                 return $v == 'TRUE' || $v == '1';
             case 'int':
+            case 'integer':
                 return (int)$v;
             default:
                 return $v;
