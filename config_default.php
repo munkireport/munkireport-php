240d34391cd07267c6d93383f5fdfb1c3751d185
Arjen van Bochoven <nejra@me.com>
2018-11-21 21:40:36 +0100

    Move Inventory config to module

--- config_default.php
+++ config_default.php
@@ -287,55 +287,6 @@
 	$conf['vnc_link'] = env('VNC_LINK', "vnc://%s:5900");
 	$conf['ssh_link'] = env('SSH_LINK',"ssh://adminuser@%s");
 
-	/*
-	|===============================================
-	| Inventory - bundle ignore list
-	|===============================================
-	|
-	| List of bundle-id's to be ignored when processing inventory
-	| The list is processed using regex, examples:
-	|
-	| Skip  all virtual windows apps created by parallels and VMware
-	| $conf['bundleid_ignorelist'][] = ['com.parallels.winapp.*', 'com.vmware.proxyApp.*'];
-	|
-	| Skip all Apple apps, except iLife, iWork and Server
-	| 'com.apple.(?!iPhoto)(?!iWork)(?!Aperture)(?!iDVD)(?!garageband)(?!iMovieApp)(?!Server).*'
-	|
-	| Skip all apps with empty bundle-id's
-	| '^$'
-	|
-	*/
-	$default_ignorelist = [
-        'com.parallels.winapp.*',
-        'com.vmware.proxyApp.*',
-        'com.apple.print.PrinterProxy',
-        'com.google.Chrome.app.*',
-        ];
-	$conf['bundleid_ignorelist'] = env('BUNDLEID_IGNORELIST', $default_ignorelist);
-
-	/*
-	|===============================================
-	| Inventory - path ignore list
-	|===============================================
-	|
-	| List of bundle-paths to be ignored when processing inventory
-	| The list is processed using regex, examples:
-	|
-	| Skip all apps in /System/Library
-	| $conf['bundlepath_ignorelist'][] = '/System/Library/.*';
-	|
-	| Skip all apps that are contained in an app bundle
-	| $conf['bundlepath_ignorelist'][] = '.*\.app\/.*\.app';
-	|
-	*/
-	$default_path_ignorelist = [
-        '/System/Library/.*',
-        '.*/Library/AutoPkg.*',
-        '/.DocumentRevisions-V100/.*',
-        '/Library/Application Support/Adobe/Uninstall/.*',
-        '.*/Library/Application Support/Google/Chrome/Default/Web Applications/.*',
-        ];
-	$conf['bundlepath_ignorelist'] = env('BUNDLEPATH_IGNORELIST', $default_path_ignorelist);
 
 	/*
 	|===============================================
