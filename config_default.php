9eecafca887b96b89bab6e3c8032c42e400d9096
Kevin M. Cox <kevinmcox@users.noreply.github.com>
2018-11-21 11:15:47 -0600

    Spacing on "Whitelist Management Console Access"
    Tabs vs. Spaces

--- config_default.php
+++ config_default.php
@@ -875,30 +875,30 @@
 	*/
 	$conf['mysql_create_tbl_opts'] = env('MYSQL_CREATE_TBL_OPTS','ENGINE=InnoDB DEFAULT CHARSET=utf8');
 
-        /*
-        |===============================================
-        | Whitelist Management Console Access
-        |===============================================
-        |
-        | Whitelisting of IP addresses that can access the management interface 
-        |    (anything except for index.php?/report/ which is always allowed)
-        |  - You can provide either individual IP addresses (which will have /32 appended automatically)
-        |      or you can provide CIDR notation. See https://en.wikipedia.org/wiki/Classless_Inter-Domain_Routing for reference
-        |  - You can also provide a custom 403 page for traffic that does not have access to the management interface
-        |      Default: The default munkireport-php 403 client error page (no need to add this object if you 
-        |                 dont want the custom 403 page
-        |
-        */
+	/*
+	|===============================================
+	| Whitelist Management Console Access
+	|===============================================
+	|
+	| Whitelisting of IP addresses that can access the management interface 
+	|    (anything except for index.php?/report/ which is always allowed)
+	|  - You can provide either individual IP addresses (which will have /32 appended automatically)
+	|      or you can provide CIDR notation. See https://en.wikipedia.org/wiki/Classless_Inter-Domain_Routing for reference
+	|  - You can also provide a custom 403 page for traffic that does not have access to the management interface
+	|      Default: The default munkireport-php 403 client error page (no need to add this object if you 
+	|                 dont want the custom 403 page
+	|
+	*/
 
-        /*
-        | $conf['auth']['network'] = [
-        |     'whitelist_ipv4' => [
-        |         'xxx.xxx.xxx.xxx',
-        |         'xxx.xxx.xxx.xxx',
-        |     ],
-        |     'redirect_unauthorized' => 'http://fqdn/403.html',
-        | ]
-        */
+	/*
+	| $conf['auth']['network'] = [
+	|     'whitelist_ipv4' => [
+	|         'xxx.xxx.xxx.xxx',
+	|         'xxx.xxx.xxx.xxx',
+	|     ],
+	|     'redirect_unauthorized' => 'http://fqdn/403.html',
+	| ]
+	*/
 
 	/*
 	|===============================================
