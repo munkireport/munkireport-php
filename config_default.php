162f4039f91255be00a3b9c90fbe34ed9a8b17dc
Arjen van Bochoven <nejra@me.com>
2018-11-22 20:34:13 +0100

    Move localadmin config to module

--- config_default.php
+++ config_default.php
@@ -105,16 +105,6 @@
 	$conf['module_search_paths'] = env('MODULE_SEARCH_PATHS', []);
 
 	/*
-  |===============================================
-  | Local Admin Threshold Value
-  |===============================================
-  |
-	| This value specifies the minimum number of local admin accounts needed to
-	|	list the computer in the Local Admin Report.  Default is 2.
-	*/
-	$conf['local_admin_threshold'] = env('LOCALADMIN_THRESHOLD', 2);
-
-	/*
 	|===============================================
 	| Default Theme
 	|===============================================
