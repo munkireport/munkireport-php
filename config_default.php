1c3f7143af8d734776ffb3a1ba671f0c2d235d3c
Arjen van Bochoven <nejra@me.com>
2018-11-22 23:33:25 +0100

    Move disk report config to module

--- config_default.php
+++ config_default.php
@@ -553,22 +553,6 @@
 
 	/*
 	|===============================================
-	| Disk Report Widget Thresholds
-	|===============================================
-	|
-	| Thresholds for disk report widget. This array holds two values:
-	| free gigabytes below which the level is set to 'danger'
-	| free gigabytes below which the level is set as 'warning'
-	| If there are more free bytes, the level is set to 'success'
-	|
-	*/
-	$conf['disk_thresholds'] = [
-	    'danger' => env('DISK_REPORT_THRESHOLD_DANGER', 5),
-        'warning' => env('DISK_REPORT_THRESHOLD_WARNING', 10)
-        ];
-
-	/*
-	|===============================================
 	| App settings
 	|===============================================
 	|
