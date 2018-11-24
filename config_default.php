b0f457fbcf1a9409896d68442a7dd76ae34fe963
Kevin M. Cox <kevinmcox@users.noreply.github.com>
2018-11-24 13:20:26 -0600

    Fixed spaced on "Small horizontal widgets"
    Spaces vs. Tabs

--- config_default.php
+++ config_default.php
@@ -482,23 +482,23 @@
 	| This is a list of the current dashboard widgets
 	|
 	| Small horizontal widgets:
-        |       bound_to_ds
-        |       client (two items)
-        |       disk_report
-        |       external_displays_count
-        |       firmwarepw
-        |       gatekeeper
-        |       hardware_model
-        |       installed memory
-        |       localadmin
-        |       munki
-        |       power_battery_condition
-        |       power_battery_health
-        |       sip
-        |       smart_status
-        |       uptime
-        |       wifi_state
-        |       	|
+	|	bound_to_ds
+	|	client (two items)
+	|	disk_report
+	|	external_displays_count
+	|	firmwarepw
+	|	gatekeeper
+	|	hardware_model
+	|	installed memory
+	|	localadmin
+	|	munki
+	|	power_battery_condition
+	|	power_battery_health
+	|	sip
+	|	smart_status
+	|	uptime
+	|	wifi_state
+	|
 	| Small horizontal / medium vertical widgets:
 	|	network_location
 	|
