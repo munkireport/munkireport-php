e26e3d30cadd6ada0abf248765a9cc1e6e77e624
Kevin M. Cox <kevinmcox@users.noreply.github.com>
2018-11-24 13:07:33 -0600

    Fixed spacing in "MunkiWebAdmin2"
    Spaces vs. Tabs

--- config_default.php
+++ config_default.php
@@ -326,7 +326,7 @@
 	| To learn more about MWA2 visit: https://github.com/munki/mwa2
 	|
 	*/
-        $conf['mwa2_link'] = env('MWA2_LINK');
+	$conf['mwa2_link'] = env('MWA2_LINK');
 
 	/*
 	|===============================================
