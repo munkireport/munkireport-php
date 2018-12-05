fc86996d123c0a5348fd63e7331c8e7f021fc01d
Arjen van Bochoven <nejra@me.com>
2018-12-05 20:59:43 +0100

    Bump minor version to 4

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -3,7 +3,7 @@
 use munkireport\models\Machine_group, munkireport\lib\Modules;
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '3.3.2.3594';
+$GLOBALS['version'] = '3.4.0.3594';
 
 // Return version without commit count
 function get_version()
