91a978ed15ebf983f8695ebcea93b26361c0923e
Arjen van Bochoven <nejra@me.com>
2018-12-05 23:44:46 +0100

    Remove warranty_helper from core

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -3,7 +3,7 @@
 use munkireport\models\Machine_group, munkireport\lib\Modules;
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '3.4.0.3617';
+$GLOBALS['version'] = '3.4.0.3618';
 
 // Return version without commit count
 function get_version()
