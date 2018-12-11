0dd58b97c611d5e158a3dac87a11245bc87aa58c
Arjen van Bochoven <abn290@vu.nl>
2018-12-11 16:10:05 +0100

    Initial config_to_env script

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -3,7 +3,7 @@
 use munkireport\models\Machine_group, munkireport\lib\Modules, munkireport\lib\Dashboard;
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '3.4.0.3628';
+$GLOBALS['version'] = '3.4.0.3630';
 
 // Return version without commit count
 function get_version()
