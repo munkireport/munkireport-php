889853052490775d7336421d3371ec959e7d24d9
Arjen van Bochoven <nejra@me.com>
2018-12-12 19:08:07 +0100

    Add convert dashboard and fix mode

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -3,7 +3,7 @@
 use munkireport\models\Machine_group, munkireport\lib\Modules, munkireport\lib\Dashboard;
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '3.4.0.3635';
+$GLOBALS['version'] = '3.4.0.3636';
 
 // Return version without commit count
 function get_version()
