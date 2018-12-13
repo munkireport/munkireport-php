68df96691d62db80251e9fb8cb9a746a03c652d5
Arjen van Bochoven <abn290@vu.nl>
2018-12-13 13:54:41 +0100

    Use hosts for adldap2

--- app/config/auth/ad.php
+++ app/config/auth/ad.php
@@ -3,9 +3,7 @@
 return [
   'account_suffix'           => env('AUTH_AD_ACCOUNT_SUFFIX', NULL),
   'base_dn'                  => env('AUTH_AD_BASE_DN', 'dc=mydomain,dc=local'),
-  'domain_controllers'       => env('AUTH_AD_DOMAIN_CONTROLLERS', []),
-  'admin_username'           => env('AUTH_AD_ADMIN_USERNAME', ''),
-  'admin_password'           => env('AUTH_AD_ADMIN_PASSWORD', ''),
+  'hosts'                    => env('AUTH_AD_HOSTS', []),
   'schema'                   => env('AUTH_AD_SCHEMA', 'ActiveDirectory'),
   'mr_allowed_users'         => env('AUTH_AD_ALLOWED_USERS', []),
   'mr_allowed_groups'        => env('AUTH_AD_ALLOWED_GROUPS', []),
