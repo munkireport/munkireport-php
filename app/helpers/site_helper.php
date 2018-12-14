fa9a86325dd6217c1af31c25f9e31430587e3556
Arjen van Bochoven <nejra@me.com>
2018-12-14 18:35:45 +0100

    Add more conversion items

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -3,7 +3,7 @@
 use munkireport\models\Machine_group, munkireport\lib\Modules, munkireport\lib\Dashboard;
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '3.4.0.3658';
+$GLOBALS['version'] = '3.4.0.3661';
 
 // Return version without commit count
 function get_version()
