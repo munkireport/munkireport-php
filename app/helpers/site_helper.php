cfdfb6600cae2814c265b29d97d8def488a29908
Arjen van Bochoven <nejra@me.com>
2019-01-11 17:36:42 +0100

    Update dotenv to v3.1 add exception catching for bad .env

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -3,7 +3,7 @@
 use munkireport\models\Machine_group, munkireport\lib\Modules, munkireport\lib\Dashboard;
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '4.0.2.3686';
+$GLOBALS['version'] = '4.0.2.3689';
 
 // Return version without commit count
 function get_version()
