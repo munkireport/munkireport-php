f5415dfbc342bd92f9d022655f59264b55e90861
tuxudo <tuxudo@users.noreply.github.com>
2019-06-12 18:48:44 -0400

    Clean up and modernize model template

--- build/templates/model.php
+++ build/templates/model.php
@@ -7,22 +7,17 @@ class MODULE_model extends \Model {
 	{
 		parent::__construct('id', 'MODULE'); //primary key, tablename
 		$this->rs['id'] = '';
-		$this->rs['serial_number'] = $serial; $this->rt['serial_number'] = 'VARCHAR(255) UNIQUE';
-        $this->rs['item1'] = '';
-        $this->rs['item2'] = 0;
+		$this->rs['serial_number'] = $serial;
+		$this->rs['item1'] = '';
+		$this->rs['item2'] = 0;
 
-        // Array with fields that can't be set by process
-        $this->restricted = array('id', 'serial_number');
+		// Array with fields that can't be set by process
+		$this->restricted = array('id', 'serial_number');
 
-		// Schema version, increment when creating a db migration
-		$this->schema_version = 0;
-
-		// Create table if it does not exist
-		//$this->create_table();
-
-		if ($serial)
+		if ($serial){
 			$this->retrieveOne('serial_number=?', $serial);
-
+		}
+		
 		$this->serial = $serial;
 
 	}
@@ -37,26 +32,26 @@ class MODULE_model extends \Model {
 	 **/
 	function process($data)
 	{
-
-        // This array will hold fields that can be populated
-        $fillable = array();
+		// This array will hold fields that can be populated
+		$fillable = array();
 
 		//clear any previous data we had
 		foreach($this->rs as $field => $value) {
 			if( ! in_array($field, $this->restricted)){
-                $fillable[] = $field;
-                $this->$field = '';
-            }
+				$fillable[] = $field;
+				$this->$field = '';
+			}
 		}
+		
 		// Parse data
-        $sep = ' = ';
+		$sep = ' = ';
 		foreach(explode("\n", $data) as $line) {
-            echo $line;
-            list($key, $val) = explode($sep, $line);
+		    echo $line;
+		    list($key, $val) = explode($sep, $line);
 
-            if( in_array($key, $fillable)){
-                $this->$key = $val;
-            }
+		    if( in_array($key, $fillable)){
+			$this->$key = $val;
+		    }
 
 		} //end foreach explode lines
 
