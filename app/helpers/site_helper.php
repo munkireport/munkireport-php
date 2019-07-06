168b9d971cdae362165f6cced53bf526f9eaa246
Arjen van Bochoven <nejra@me.com>
2019-07-06 14:32:52 +0200

    Network module 2.0

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -3,7 +3,7 @@
 use munkireport\models\Machine_group, munkireport\lib\Modules, munkireport\lib\Dashboard;
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '4.2.3.3810';
+$GLOBALS['version'] = '4.2.3.3813';
 
 // Return version without commit count
 function get_version()
