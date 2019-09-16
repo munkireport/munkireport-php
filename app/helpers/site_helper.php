317abec7e98a735f3710ea23c4d160c282e3aca3
Arjen van Bochoven <nejra@me.com>
2019-09-16 20:20:16 +0200

    Bump version to 5.0.0

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -3,7 +3,7 @@
 use munkireport\models\Machine_group, munkireport\lib\Modules, munkireport\lib\Dashboard;
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '4.4.0.3857';
+$GLOBALS['version'] = '5.0.0.3857';
 
 // Return version without commit count
 function get_version()
