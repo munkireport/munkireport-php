ce40d662b9cebe965319e25f5b7f77d16abae4be
Arjen van Bochoven <nejra@me.com>
2019-09-18 19:31:48 +0200

    Remove storage plot from core

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -3,7 +3,7 @@
 use munkireport\models\Machine_group, munkireport\lib\Modules, munkireport\lib\Dashboard;
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '5.0.0.3892';
+$GLOBALS['version'] = '5.0.0.3893';
 
 // Return version without commit count
 function get_version()
