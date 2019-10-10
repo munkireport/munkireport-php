ed5e0235049c55f9a7fbaa34c81a3248d99e3061
Luke Sturgis (Student Employee) <lcsees@rit.edu>
2019-10-10 15:57:39 -0400

    made *black*. also removed dependency on some global variables so we can appropriately write unit tests, if desired.

--- build/mr_upgrade.py
+++ build/mr_upgrade.py
@@ -4,6 +4,7 @@
 
 import argparse
 import datetime
+from dotenv import load_dotenv
 from distutils.dir_util import copy_tree
 import coloredlogs
 import json
@@ -15,21 +16,24 @@ import subprocess
 import urllib.request
 
 
-NOW = datetime.datetime.now()
-
 # log output to console
 log = logging.getLogger()
 coloredlogs.install(
     fmt="[%(asctime)s] - [%(levelname)-8s] - %(message)s", level="INFO", logger=log
 )
 
+# load environment variables
+load_dotenv()
+
 
 def get_current_version(install_path):
     """Return current build version"""
     helper = install_path + "app/helpers/site_helper.php"
     if os.path.exists(helper):
-        version = [re.findall(r"(?<=GLOBALS\['version'\])?[0-9].*(?=';)", line)
-                for line in open(helper)]
+        version = [
+            re.findall(r"(?<=GLOBALS\['version'\])?[0-9].*(?=';)", line)
+            for line in open(helper)
+        ]
         return [x for x in version]
 
     return None
@@ -37,95 +41,89 @@ def get_current_version(install_path):
 
 def database_type():
     """Return the database type."""
-    return os.getenv('CONNECTION_DRIVER') or 'sqlite'
+    return os.getenv("CONNECTION_DRIVER") or "sqlite"
 
 
 def set_maintenance_mode(install_path, value):
     """Set maintenance mode to down or remove"""
     log.debug(f"Setting maintenance mode to '{value}'...")
     if value == "down":
-        open(install_path + 'storage/framework/' + value, 'a').close()
+        open(install_path + "storage/framework/" + value, "a").close()
     else:
-        os.remove(install_path + 'storage/framework/down')
+        os.remove(install_path + "storage/framework/down")
 
 
-def backup_database(database_type, backup_dir, install_path):
+def backup_database(database_type, backup_dir, install_path, current_time):
     """Backup a MunkiReport database."""
     if database_type == "mysql":
-        username = os.getenv('CONNECTION_USERNAME')
-        password = os.getenv('CONNECTION_PASSWORD')
-        database = os.getenv('CONNECTION_DATABASE')
-        backup_file = backup_dir + database + NOW.strftime("%Y%m%d%H%M") + '.bak'
+        username = os.getenv("CONNECTION_USERNAME")
+        password = os.getenv("CONNECTION_PASSWORD")
+        database = os.getenv("CONNECTION_DATABASE")
+        backup_file = backup_dir + database + current_time + ".bak"
         cmd = f"/usr/local/opt/mysql-client/bin/mysqldump --user={username} --password={password} {database} > {backup_file}"
         log.info("Backing up database to '{}'...".format(backup_file))
         subprocess.Popen(cmd, shell=True)
 
-    elif database_type == 'sqlite':
+    elif database_type == "sqlite":
         shutil.copyfile(
-            install_path + 'app/db/db.sqlite',
-            backup_dir + 'db' + NOW.strftime("%Y%m%d%H%M") + '.sqlite.bak'
+            install_path + "app/db/db.sqlite",
+            backup_dir + "db" + current_time + ".sqlite.bak",
         )
 
 
-def backup_files(backup_dir, install_path):
+def backup_files(backup_dir, install_path, current_time):
     """Create file backup of install."""
-    final_dir = os.path.join(backup_dir, "munkireport", NOW.strftime("%Y%m%d%H%M"))
+    final_dir = os.path.join(backup_dir, "munkireport", current_time)
     log.info(f"Backing up files to '{final_dir}'...")
     os.mkdir(final_dir)
     copy_tree(install_path, final_dir)
-       
+
 
 def get_latest_version():
     """Return MR latest version"""
     mr_api = "https://api.github.com/repos/munkireport/munkireport-php/releases/latest"
     with urllib.request.urlopen(mr_api) as response:
         data = json.loads(response.read())
-    return data['tag_name'].strip('v')
+    return data["tag_name"].strip("v")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-            description='Manage a MunkiReport install.')
+    parser = argparse.ArgumentParser(description="Manage a MunkiReport install.")
     group = parser.add_mutually_exclusive_group()
     group.add_argument(
-            '-i', '--info',
-            action='store_true',
-            help='Print info on the MunkiReport install.'
-            )
+        "-i",
+        "--info",
+        action="store_true",
+        help="Print info on the MunkiReport install.",
+    )
     parser.add_argument(
-            '--no-backup',
-            help='Do not take any backups before upgrading.',
-            default=False
-            )
+        "--no-backup", help="Do not take any backups before upgrading.", default=False
+    )
+    parser.add_argument("--backup-dir", help="Directory to back up to.", default="/tmp")
     parser.add_argument(
-            '--backup-dir',
-            help='Directory to back up to.',
-            default='/tmp'
-            )
+        "--install-path",
+        help="Install path for MunkiReport.",
+        default=os.path.dirname(os.path.realpath(__file__)).strip("build"),
+    )
     parser.add_argument(
-            '--install-path',
-            help='Install path for MunkiReport.',
-            default=os.path.dirname(os.path.realpath(__file__)).strip('build')
-            )
+        "--upgrade", help="Attempt to upgrade MunkiReport", default="False"
+    )
     parser.add_argument(
-            '--upgrade',
-            help='Attempt to upgrade MunkiReport',
-            default='False'
-            )
-    parser.add_argument(
-            '--upgrade-version',
-            help='Version of MunkiReport to upgrade to.',
-            default='latest'
-            )
+        "--upgrade-version",
+        help="Version of MunkiReport to upgrade to.",
+        default="latest",
+    )
     args = parser.parse_args()
 
     install_path = args.install_path
     current_version = get_current_version(install_path)
     latest_version = get_latest_version()
     database_type = database_type()
-    
+
     if not current_version:
-        log.error(f"The directory '{install_path}' does not appear to be a valid MunkiReport install.")
+        log.error(
+            f"The directory '{install_path}' does not appear to be a valid MunkiReport install."
+        )
         exit()
 
     log.info(f"Current version: {current_version}")
@@ -133,22 +131,22 @@ if __name__ == "__main__":
     log.info(f"Install path:    {install_path}")
     log.info(f"Database type:   {database_type}")
 
-    if args.info:    
+    if args.info:
         exit()
 
     if current_version < latest_version:
         log.info(f"Starting upgrade of {install_path}...")
         set_maintenance_mode(install_path, "down")
 
+        current_time = datetime.datetime.now().strftime("%Y%m%d%H%M")
         # backup database
-        backup_database(database_type, args.backup_dir, install_path)
+        backup_database(database_type, args.backup_dir, install_path, current_time)
 
         # backup files
-        backup_files(args.backup_dir, install_path)
+        backup_files(args.backup_dir, install_path, current_time)
 
-        # Update
+        # attempt git pull for update
         try:
-            # do git pull
             log.info("Starting Git pull...")
             proc = subprocess.check_output(["git", "pull"])
         except subprocess.CalledProcessError as e:
@@ -160,7 +158,9 @@ if __name__ == "__main__":
         log.info("Running composer...")
         os.chdir(install_path)
         try:
-            proc = subprocess.check_output(["/usr/local/bin/composer", "update", "--no-dev"])
+            proc = subprocess.check_output(
+                ["/usr/local/bin/composer", "update", "--no-dev"]
+            )
         except subprocess.CalledProcessError as e:
             log.error(f"Composer failed with the following output: '{e}'. Exiting...")
             exit(1)
