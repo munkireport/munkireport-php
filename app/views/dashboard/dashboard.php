76ac0bf5b8247314d9e847e4d80bee40c29db243
bochoven <arjen@bochoven.net>
2020-02-08 08:11:04 +0100

    Enable multiple widgets Override widget name by specifying widget: name in the widget variables

--- app/views/dashboard/dashboard.php
+++ app/views/dashboard/dashboard.php
@@ -8,7 +8,15 @@
 
 		<?php foreach($row as $item => $data):?>
 
-		<?php $widget->view($this, $item, $data); ?>
+			<?php if(array_key_exists('widget', $data)):?>
+
+				<?php $widget->view($this, $data['widget'], $data); ?>
+
+			<?php else:?>
+
+				<?php $widget->view($this, $item, $data); ?>
+
+			<?php endif?>
 
 		<?php endforeach?>
 
