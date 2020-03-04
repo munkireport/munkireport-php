e6aa7ff0ce74652f189f8a78661aa237fc12a4eb
bochoven <arjen@bochoven.net>
2020-03-04 07:32:13 +0100

    Add bargraph_widget

---
+++ app/views/widgets/bargraph_widget.php
@@ -0,0 +1,58 @@
+<div class="col-lg-6 col-md-6">
+    <div class="panel panel-default" id="<?=$widget_id?>">
+        <div 
+            class="panel-heading" 
+            <?php if(isset($i18n_tooltip)):?> 
+            data-i18n="[title]<?=$i18n_tooltip?>"
+            <?php endif?>
+            >
+            <h3 class="panel-title"><i class="fa <?=$icon?>"></i>
+                <span data-i18n="<?=$i18n_title?>"></span>
+                <span class="counter badge"></span>
+                <list-link data-url="<?=$listing_link?>"></list-link>
+            </h3>
+        </div>
+		<div class="panel-body">
+
+			<svg style="width:100%"></svg>
+
+		</div>
+    </div><!-- /panel -->
+</div><!-- /col -->
+
+<script>
+$(document).on('appReady', function(e, lang) {
+    
+    var widgetId = "<?=$widget_id?>"
+    var box = $('#<?=$widget_id?> div.scroll-box');
+    var showCounter = <?php if(isset($show_counter) && $showcounter):?>true<?php else:?>false<?php endif?>; 
+    var apiUrl = "<?=$api_url?>"
+    var searchKey = "<?=$search_key?>"
+    var listingLink = "<?=$listing_link?>"
+    var i18nEmptyResult = "<?php echo isset($i18n_empty_result)?$i18n_empty_result:''?>"
+    var badgeType = "<?php echo isset($badge)?$badge:'default'?>"
+    var urlType = "<?php echo isset($url_type)?$url_type:'listing'?>"
+
+	var conf = {
+		url: appUrl + apiUrl, // Url for json
+        widget: widgetId, // Widget id
+        <?php if(isset($margin)):?>
+        margin: <?=$margin?>,
+        <?php endif?>
+        <?php if(isset($search_component)):?>
+		elementClickCallback: function(e){
+			var label = e.data.label;
+			window.location.href = appUrl + listingLink + '#' + <?=$search_component?> ;
+        },
+        <?php endif?>
+        <?php if(isset($label_modifier)):?>
+		labelModifier: function(label){
+			return <?=$label_modifier?>;
+        }
+        <?php endif?>
+	};
+
+	mr.addGraph(conf);
+
+});	
+</script>
