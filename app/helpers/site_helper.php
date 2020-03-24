860d989de27d0ed99c3d2b46443d0d75d0ccf5db
bochoven <arjen@bochoven.net>
2020-03-24 20:31:57 +0100

    Add support for admin pages

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -3,7 +3,7 @@
 use munkireport\models\Machine_group, munkireport\lib\Modules, munkireport\lib\Dashboard;
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '5.3.2.4037';
+$GLOBALS['version'] = '5.3.2.4038';
 
 // Return version without commit count
 function get_version()
