4dab0562e5b249bf8e381a39904d348a3d28b35c
bochoven <arjen@bochoven.net>
2020-04-15 23:11:46 +0200

    Add User.php

---
+++ app/lib/munkireport/User.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace munkireport\lib;
+
+/**
+* User class
+*
+* Store and retrieve information about the logged in user
+*
+*/
+class User
+{
+    private $conf, $session;
+
+    public function __construct($conf = [])
+    {
+        $this->conf = $conf;
+        $this->session = $_SESSION;
+    }
+
+    public function isAdmin()
+    {
+        return $this->_getRole() == 'admin';
+    }
+
+    public function isManager()
+    {
+        return $this->_getRole() == 'manager';
+    }
+
+    public function canArchive()
+    {
+        return $this->isAdmin() || $this->isManager();
+    }
+
+    public function canAccessMachineGroup($id)
+    {
+        if ($this->isAdmin()) {
+            return true;
+        }
+    
+        return in_array($id, $this->machineGroups());
+    }
+
+    public function machineGroups()
+    {
+        return $this->session['machine_groups'] ?? [];
+    }
+
+    private function _getRole()
+    {
+        return $this->session['role'] ?? 'nobody';
+    }
+}
