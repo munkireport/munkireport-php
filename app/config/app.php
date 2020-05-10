0aa4dadac350edbc3e0b94ed26c5bfc81d6ca4e3
Arjen van Bochoven <arjen@bochoven.net>
2020-05-11 00:46:18 +0200

    Add archive to authorization

--- app/config/app.php
+++ app/config/app.php
@@ -181,6 +181,7 @@ return [
     |
     */
     'authorization' => [
+        'archive' => env('AUTHORIZATION_ARCHIVE', ['admin', 'manager', 'archiver']),
         'delete_machine' => env('AUTHORIZATION_DELETE_MACHINE', ['admin', 'manager']),
         'global' => env('AUTHORIZATION_GLOBAL', ['admin']),
     ],
