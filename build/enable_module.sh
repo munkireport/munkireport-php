b44bf395111ea9d3296cfd17a88907b10a7806d8
Jon Crain <joncrain@users.noreply.github.com>
2020-05-21 14:53:51 -0400

    Update enable_module.sh
    Hiding the composer mess.

--- build/enable_module.sh
+++ build/enable_module.sh
@@ -17,6 +17,7 @@ MODULES=$(grep -i "MODULES=" "$ENV")
 LINE_NUMBER=$(grep -ni "MODULES=" "$ENV" | cut -f1 -d:)
 
 #### Check for Composer
+{
 if [ ! "$COMPOSER_PATH" ]; then
     EXPECTED_CHECKSUM="$(curl --silent https://composer.github.io/installer.sig)"
     php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
@@ -34,22 +35,30 @@ fi
 
 #### Install Module via Composer
 COMPOSER=composer.local.json "$COMPOSER_PATH" require "$MODULE" 
+} &> /dev/null
 
 if [ $? -eq 1 ]; then
     echo "$MODULE not found"
     exit
 fi
+echo "Installing newest version of $MODULE"
 
 #### Run Composer and Database Migration
-"$COMPOSER_PATH" update "$MODULE" --no-dev
+{ 
+"$COMPOSER_PATH" update "$MODULE" --no-dev 
+} &> /dev/null
+echo "Performing database migrations:"
 php "$PARENT_DIRECTORY/database/migrate.php"
 
 #### Clean up
+{
 cp -Rf "$PARENT_DIRECTORY/vendor/$MODULE" "$PARENT_DIRECTORY/local/modules/"
 COMPOSER=composer.local.json composer remove "$MODULE"
 "$COMPOSER_PATH" update "$MODULE" --no-dev
+} &> /dev/null
 
 #### Add Module to .env
+echo "Enabling module in .env file"
 if [ ! "$MODULES" ]; then
         echo "Modules not configured. Configuring base modules"
         echo "MODULES='munkireport, managedinstalls, disk_report, $MODULE_BASE'" >> "$PARENT_DIRECTORY/.env"
