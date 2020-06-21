f20813de59ec553b649cdd43927f396907715f48
Mosen <mosen@users.noreply.github.com>
2020-06-21 23:09:39 +1000

    Copy AdminController.php from controllers/Admin.php and adapt for Laravel Ignore admin permissions check to get admin functionality working without working auth required. Copy ModuleMarketplaceController.php from controllers/Module_marketplace.php and adjust for Laravel Copy SystemController.php from controllers/System.php and adjust for Laravel Add routes for newly added controllers and sort alphabetically (kinda)

--- routes/web.php
+++ routes/web.php
@@ -15,28 +15,50 @@ use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/show/dashboard/default');
 
+Route::get('/admin/get_bu_data', 'AdminController@get_bu_data');
+Route::get('/admin/get_mg_data', 'AdminController@get_mg_data');
+Route::post('/admin/save_business_unit', 'AdminController@save_business_unit');
+Route::post('/admin/save_machine_group', 'AdminController@save_machine_group');
+Route::get('/admin/show/{which}', 'AdminController@show');
+
 Route::get('/clients/detail/{sn?}', 'ClientsController@detail');
 Route::get('/clients/get_data/{serial_number?}', 'ClientsController@get_data');
 Route::get('/clients/get_links', 'ClientsController@get_links');
 
-Route::get('/show/dashboard/{dashboard?}', 'ShowController@dashboard');
-Route::get('/show/listing/{module}/{name?}', 'ShowController@listing');
-Route::get('/show/report/{report}/{action}', 'ShowController@report');
 Route::post('/datatables/data', 'DatatablesController@data');
+
 Route::get('/install', 'InstallController@index');
 Route::get('/install/get_paths', 'InstallController@get_paths');
+
+Route::get('/locale/get/{lang?}', 'LocaleController@get');
+Route::get('/locale/get/{lang}/{load}', 'LocaleController@get');
+
+Route::get('/module_marketplace/get_module_data', 'ModuleMarketplaceController@get_module_data');
 
 Route::post('/report/hash_check', 'ReportController@hash_check');
 Route::post('/report/check_in', 'ReportController@check_in');
 
 Route::any('/settings/theme', 'SettingsController@theme');
 
-Route::get('/locale/get/{lang?}', 'LocaleController@get');
+Route::get('/show/dashboard/{dashboard?}', 'ShowController@dashboard');
+Route::get('/show/listing/{module}/{name?}', 'ShowController@listing');
+Route::get('/show/report/{report}/{action}', 'ShowController@report');
+
+Route::get('/system/show/{which?}', 'SystemController@show');
+Route::get('/system/DataBaseInfo', 'SystemController@DataBaseInfo');
+Route::get('/system/phpInfo', 'SystemController@phpInfo');
+
+
+
+
+
+
 
 Route::get('/filter/get_filter', 'FilterController@get_filter');
 Route::get('/unit/get_machine_groups', 'UnitController@get_machine_groups');
 
 Route::get('/module/{module}/{action}/{params?}', 'ModuleController@invoke')->where('params', '.*');
+
 
 
 Auth::routes();
