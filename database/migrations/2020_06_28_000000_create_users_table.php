fad91bdc8cb4daf29bd5e7784d2266d46ad0b18f
Mosen <mosen@users.noreply.github.com>
2020-06-28 23:54:03 +1000

    Made example env more readable Add role to users table Make the imported models from the old rewrite not reference unavailable relationships

--- database/migrations/2020_06_28_000000_create_users_table.php
+++ database/migrations/2020_06_28_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'manager', 'user'])->default('user');
             $table->rememberToken();
             $table->timestamps();
 
