94fe2742f21b14201bd028bef51d08d21bbcf639
Arjen <arjen@bochoven.net>
2020-07-05 22:11:23 +0200

    Fix migrations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(\Illuminate\Database\Migrations\Migrator::class, function ($app) {
+            return $app['migrator'];
+        });
     }
 
     /**
