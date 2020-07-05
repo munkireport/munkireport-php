b7184e7e759ab03a497e903d3ad562748c4bdc06
Mosen <mosen@users.noreply.github.com>
2020-07-05 22:48:30 +1000

    try registering extra factories with EloquentFactory in the seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use munkireport\lib\Modules as ModuleMgr;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+        $factory = app(\Illuminate\Database\Eloquent\Factory::class);
+
+        $moduleMgr = new ModuleMgr;
+        $moduleMgr->loadinfo(true);
+
+        foreach($moduleMgr->getInfo() as $moduleName => $info) {
+            // print("Finding model factories in " . $moduleMgr->getPath($moduleName). "\n");
+            $factorypath = $moduleMgr->getPath($moduleName, "/${moduleName}_factory.php");
+
+            if(is_file($factorypath)) {
+                print($factorypath);
+                $factory->load($factorypath);
+                $factory_models[] = ucfirst($moduleName . "_model");
+            }
+        }
+
+
+        $reportData = factory(\Reportdata_model::class)->times(10)->create();
+
+//
+//        $this->call(ReportDataSeeder::class);
         // $this->call(UserSeeder::class);
     }
 }
