a6d12f68a13bbdd4c4b363a3f95c9ad5cde7ee1c
Mosen <mosen@users.noreply.github.com>
2020-07-05 20:50:06 +1000

    Use Schema facade instead of Capsule for core migrations because Capsule isnt needed there anymore. Add some more config var fallbacks

--- database/migrations/2017_02_20_085316_machine_group.php
+++ database/migrations/2017_02_20_085316_machine_group.php
@@ -1,7 +1,7 @@
 <?php
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Support\Facades\Schema;
 
 class MachineGroup extends Migration
 {
@@ -10,20 +10,19 @@ class MachineGroup extends Migration
 
     public function up()
     {
-        $capsule = new Capsule();
         $migrateData = false;
 
-        if ($capsule::schema()->hasTable($this->tableNameV2)) {
+        if (Schema::hasTable($this->tableNameV2)) {
             // Migration already failed before, but didnt finish
             throw new Exception("previous failed migration exists");
         }
 
-        if ($capsule::schema()->hasTable($this->tableName)) {
-            $capsule::schema()->rename($this->tableName, $this->tableNameV2);
+        if (Schema::hasTable($this->tableName)) {
+            Schema::rename($this->tableName, $this->tableNameV2);
             $migrateData = true;
         }
 
-        $capsule::schema()->create($this->tableName, function (Blueprint $table) {
+        Schema::create($this->tableName, function (Blueprint $table) {
             $table->increments('id');
             $table->integer('groupid')->nullable();
             $table->string('property');
@@ -31,7 +30,7 @@ class MachineGroup extends Migration
         });
 
         if ($migrateData) {
-            $capsule::unprepared("INSERT INTO 
+            Schema::unprepared("INSERT INTO 
                 $this->tableName
             SELECT
                 id,
@@ -45,10 +44,9 @@ class MachineGroup extends Migration
 
     public function down()
     {
-        $capsule = new Capsule();
-        $capsule::schema()->dropIfExists($this->tableName);
-        if ($capsule::schema()->hasTable($this->tableNameV2)) {
-            $capsule::schema()->rename($this->tableNameV2, $this->tableName);
+        Schema::dropIfExists($this->tableName);
+        if (Schema::hasTable($this->tableNameV2)) {
+            Schema::rename($this->tableNameV2, $this->tableName);
         }
     }
 }
