5c796eb7982a13d2ed29aa266417c85ebaaebaa0
Mosen <mosen@users.noreply.github.com>
2020-07-07 20:43:01 +1000

    use Schema facade in cleanup_hash_table add mock Capsule into shims/ remove dependency on Illuminate/Database, its already included w laravel

--- app/Console/Commands/MigrateCommand.php
+++ app/Console/Commands/MigrateCommand.php
@@ -4,9 +4,8 @@ namespace App\Console\Commands;
 
 use Illuminate\Database\Migrations\Migrator;
 use Illuminate\Database\Console\Migrations\MigrateCommand as BaseCommand;
-use Illuminate\Support\Facades\Schema;
 use munkireport\lib\Modules as ModuleMgr;
-use Illuminate\Database\Capsule\Manager as Capsule;
+
 
 class MigrateCommand extends BaseCommand
 {
@@ -15,46 +14,23 @@ class MigrateCommand extends BaseCommand
     /**
      * Execute the console command.
      *
-     * @return void
+     * @return int
      */
     public function handle()
     {
+        $this->info("Using MunkiReport Version of Migrate");
         $this->addModuleMigrationPaths($this->migrator);
-        $this->setUpDbConnection();
-        parent::handle();
+        return parent::handle();
     }
 
     protected function addModuleMigrationPaths(Migrator $migrator)
     {
-      $moduleMgr = new ModuleMgr;
-      $moduleMgr->loadinfo(true);
-      foreach($moduleMgr->getInfo() as $moduleName => $info){
-          if($moduleMgr->getModuleMigrationPath($moduleName, $migrationPath)){
-            $migrator->path($migrationPath);
-          }
-      }
-    }
-
-    protected function setUpDbConnection()
-    {
-      $connection = conf('connection');
-        
-      if(has_sqlite_db($connection)){
-        $this->ensure_sqlite_db_exists($connection);
-      }
-      
-      if(has_mysql_db($connection)){
-        add_mysql_opts($connection);
-      }
-
-      $capsule = new Capsule();
-      $capsule->setContainer($this->getLaravel());
-      // $capsule->addConnection($connection);
-      // $capsule->setAsGlobal();
-    }
-
-    protected function ensure_sqlite_db_exists($connection)
-    {
-      touch($connection['database']);
+        $moduleMgr = new ModuleMgr;
+        $moduleMgr->loadinfo(true);
+        foreach ($moduleMgr->getInfo() as $moduleName => $info) {
+            if ($moduleMgr->getModuleMigrationPath($moduleName, $migrationPath)) {
+                $migrator->path($migrationPath);
+            }
+        }
     }
 }
