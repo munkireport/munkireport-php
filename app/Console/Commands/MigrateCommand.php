0a466ebfb47b5eed71f8a13df1e768a26b70c953
Mosen <mosen@users.noreply.github.com>
2020-07-07 20:43:01 +1000

    use Schema facade in cleanup_hash_table add mock Capsule into shims/ remove dependency on Illuminate/Database, its already included w laravel

--- app/Console/Commands/MigrateCommand.php
+++ app/Console/Commands/MigrateCommand.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Database\Migrations\Migrator;
 use Illuminate\Database\Console\Migrations\MigrateCommand as BaseCommand;
 use munkireport\lib\Modules as ModuleMgr;
-use Illuminate\Database\Capsule\Manager as Capsule;
+
 
 class MigrateCommand extends BaseCommand
 {
@@ -20,44 +20,17 @@ class MigrateCommand extends BaseCommand
     {
         $this->info("Using MunkiReport Version of Migrate");
         $this->addModuleMigrationPaths($this->migrator);
-        $this->setUpDbConnection();
         return parent::handle();
     }
 
     protected function addModuleMigrationPaths(Migrator $migrator)
     {
-      $moduleMgr = new ModuleMgr;
-      $moduleMgr->loadinfo(true);
-      foreach($moduleMgr->getInfo() as $moduleName => $info){
-          if($moduleMgr->getModuleMigrationPath($moduleName, $migrationPath)){
-            $migrator->path($migrationPath);
-          }
-      }
-    }
-
-    protected function setUpDbConnection()
-    {
-      $connection = conf('connection');
-        
-      if(has_sqlite_db($connection)){
-        $this->ensure_sqlite_db_exists($connection);
-      }
-      
-      if(has_mysql_db($connection)){
-        add_mysql_opts($connection);
-      }
-
-      $default = config("database.default"); // This becomes overridden by capsule
-      $capsule = new Capsule($this->getLaravel());
-
-      // Capsule doesnt know about database.default indirection, it just looks for a connection named database.default
-      $connections = app('config')['database.connections'];
-      $capsule->addConnection($connections[$default]);
-      $capsule->setAsGlobal();
-    }
-
-    protected function ensure_sqlite_db_exists($connection)
-    {
-      touch($connection['database']);
+        $moduleMgr = new ModuleMgr;
+        $moduleMgr->loadinfo(true);
+        foreach ($moduleMgr->getInfo() as $moduleName => $info) {
+            if ($moduleMgr->getModuleMigrationPath($moduleName, $migrationPath)) {
+                $migrator->path($migrationPath);
+            }
+        }
     }
 }
