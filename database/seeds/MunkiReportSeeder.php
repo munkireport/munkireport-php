85eb8ce296e448697f726e7ad1139717d0942a4e
Mosen <mosen@users.noreply.github.com>
2020-07-08 20:18:04 +1000

    Add seeders for all available factories

---
+++ database/seeds/MunkiReportSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MunkiReportSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('machine')->get('serial_number')->each(function ($m) {
+            factory(Munkireport_model::class)->times(3)->create(['serial_number' => $m->serial_number]);
+        });
+    }
+}
