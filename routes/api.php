e72d25e10e45c30f4510bd7a8dca5b2b8bfd006c
Mosen <mosen@users.noreply.github.com>
2020-07-10 19:41:39 +1000

    Backed out the API middleware on GraphQL for now

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
 });
 
