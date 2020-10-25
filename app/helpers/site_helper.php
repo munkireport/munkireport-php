b382bd71e0ebe3cbc47d521ef207c960b28a4bda
Arjen <arjen@bochoven.net>
2020-10-25 14:29:08 +0100

    Update site_helper.php
    Fix get_filtered_groups

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -324,12 +324,14 @@ function get_filtered_groups()
     $out = array(0);
 
     // Get filter
-//    if (isset($_SESSION['filter']['machine_group']) && $_SESSION['filter']['machine_group']) {
-//        $filter = $_SESSION['filter']['machine_group'];
-//        $out = array_diff($_SESSION['machine_groups'], $filter);
-//    } else {
-//        $out = $_SESSION['machine_groups'];
-//    }
+    if(session()->has('filter.machine_group')){
+        $out = array_diff(
+            session()->get('machine_groups', []),
+            session()->get('filter.machine_group')
+        );
+    }else{
+        $out = session()->get('machine_groups', []);
+    }
 
     // If out is empty, signal no groups
     if (! $out) {
