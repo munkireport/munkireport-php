60c0ce1461b1b6eee4fc1c8808da55bcde503ad9
Mosen <mosen@users.noreply.github.com>
2021-03-28 11:14:52 +1100

    Only seed database with Fake data if you specifically call the FakeSeeder

---
+++ database/seeds/DatabaseFakeSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use munkireport\lib\Modules as ModuleMgr;
+
+class DatabaseFakeSeeder extends Seeder
+{
+    /**
+     * Seed the application's database with made up data.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call([
+            // These must be called first
+            UserSeeder::class,
+            MachineSeeder::class,
+            ReportDataSeeder::class,
+
+            // Modules
+            BluetoothSeeder::class,
+            DiskReportSeeder::class,
+            EventSeeder::class,
+            ManagedInstallsSeeder::class,
+//            FirewallSeeder::class,
+//            MunkiReportInfoSeeder::class,
+//            MunkiReportSeeder::class,
+            WarrantySeeder::class,
+        ]);
+
+    }
+}
