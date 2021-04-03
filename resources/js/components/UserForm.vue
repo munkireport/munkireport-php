3ccceae0e26e78346749c83376c21b1578b2ab9f
Mosen <mosen@users.noreply.github.com>
2021-04-03 23:31:19 +1100

    Add component UserForm.vue for user editing Wire up UsersList.vue to UserForm.vue in Users.vue top level view Discarded UsersTable.vue because it did not need such large real estate. Switched to list group (UsersList.vue).

---
+++ resources/js/components/UserForm.vue
@@ -0,0 +1,50 @@
+<template>
+  <form>
+    <div class="form-group">
+      <label for="username">Username</label>
+      <input type="text" class="form-control" id="username" v-model="data.name" />
+    </div>
+    <div class="form-group">
+      <label for="displayName">Display Name</label>
+      <input type="text" class="form-control" id="displayName" v-model="data.display_name" />
+    </div>
+    <div class="form-group">
+      <label for="email">E-mail</label>
+      <input type="text" class="form-control" id="email" v-model="data.email" />
+    </div>
+    <div class="form-group">
+      <label for="locale">Preferred Locale</label>
+      <select id="locale" v-model="data.locale">
+        <option disabled value="">Please select one</option>
+        <option value="en-US">en-US</option>
+      </select>
+    </div>
+    <div class="form-group">
+      <button type="submit" class="btn btn-primary" @click.prevent="update">Update</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "UserForm",
+  data() {
+    return {
+      id: null,
+      name: "",
+      display_name: "",
+      email: "",
+      locale: "",
+    }
+  },
+  methods: {
+    update: () => {
+      console.log('update user');
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/UsersList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="list-group">
+      <a v-for="item in items" href="#" class="list-group-item" @click.stop="select(item.id)">
+        <h4 class="list-group-item-heading" v-text="item.name"></h4>
+        <p class="list-group-item-text">
+          {{ item.email }}
+        </p>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UsersList",
+  props: [
+      'items'
+  ],
+  data() {
+    return {
+    }
+  },
+  methods: {
+    select(userId) {
+      this.$emit('selected', userId);
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
