c2feeea9377b87b7c538e4327f4cb9d02e27d003
Mosen <mosen@users.noreply.github.com>
2021-04-09 12:38:20 +1000

    Remove MultiAuthGuard.php/NoAuthGuard.php which were earlier attempts to mix NOAUTH with authentication

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use App\Auth\MultiAuthGuard;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
