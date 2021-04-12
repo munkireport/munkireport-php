dd1c1630e6a1cc764d2ef1790764b3ef39d61e94
Mosen <mosen@users.noreply.github.com>
2021-04-12 13:34:56 +1000

    Core widgets are back. Module widgets were being registered twice, and the second time was with addModuleWidgets which adds a fixed path infix to every module (even core ones). Removed widgets from Modules class and everything is fine again.

--- app/lib/munkireport/Modules.php
+++ app/lib/munkireport/Modules.php
@@ -56,6 +56,9 @@ class Modules
      *
      * This is a temporary workaround for the fact that listings and processors can't be discovered inside
      * the core package. When theres another registration mechanism, this can be removed.
+     *
+     * Widgets have been omitted because addModuleWidgets() always assumes views are in the same path as the module.
+     * There is a function called addCoreWidgets() which adds them separately.
      */
     protected function addCoreModules()
     {
@@ -66,11 +69,11 @@ class Modules
             'listings' => array(
                 'clients' => array('view' => 'clients_listing', 'i18n' => 'client.clients'),
             ),
-            'widgets' => array(
-                'client' => array('view' => 'client_widget'),
-                'registered_clients' => array('view' => 'registered_clients_widget'),
-                'uptime' => array('view' => 'uptime_widget'),
-            ),
+//            'widgets' => array(
+//                'client' => array('view' => 'client_widget'),
+//                'registered_clients' => array('view' => 'registered_clients_widget'),
+//                'uptime' => array('view' => 'uptime_widget'),
+//            ),
             'reports' => array(
                 'clients' => array('view' => 'clients', 'i18n' => 'client.report'),
             ),
@@ -88,17 +91,17 @@ class Modules
             'listings' => [
                 'hardware' => ['view' => 'hardware_listing', 'i18n' => 'machine.hardware'],
             ],
-            'widgets' => [
-                'duplicated_computernames' => ['view' => 'duplicated_computernames_widget'],
-                'hardware_basemodel' => ['view' => 'hardware_basemodel_widget'],
-                'hardware_model' => ['view' => 'hardware_model_widget'],
-                'hardware_type' => ['view' => 'hardware_type_widget'],
-                'installed_memory' => ['view' => 'installed_memory_widget'],
-                'memory' => ['view' => 'memory_widget'],
-                'new_clients' => ['view' => 'new_clients_widget'],
-                'os' => ['view' => 'os_widget'],
-                'osbuild' => ['view' => 'osbuild_widget'],
-            ],
+//            'widgets' => [
+//                'duplicated_computernames' => ['view' => 'duplicated_computernames_widget'],
+//                'hardware_basemodel' => ['view' => 'hardware_basemodel_widget'],
+//                'hardware_model' => ['view' => 'hardware_model_widget'],
+//                'hardware_type' => ['view' => 'hardware_type_widget'],
+//                'installed_memory' => ['view' => 'installed_memory_widget'],
+//                'memory' => ['view' => 'memory_widget'],
+//                'new_clients' => ['view' => 'new_clients_widget'],
+//                'os' => ['view' => 'os_widget'],
+//                'osbuild' => ['view' => 'osbuild_widget'],
+//            ],
             'reports' => [
                 'hardware' => ['view' => 'hardware', 'i18n' => 'machine.hardware_report'],
             ],
