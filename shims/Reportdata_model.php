677e3d0e24fed1cacb89ce4704a73893b96d41a1
Mosen <mosen@users.noreply.github.com>
2022-01-25 10:34:15 +1100

    Remove munkireport\models\Reportdata_model and replace with App\ReportData alias \Reportdata_model to App\ReportData

--- shims/Reportdata_model.php
+++ shims/Reportdata_model.php
@@ -4,4 +4,4 @@
  * This class alias provided for backwards compatibility when 3rd party modules access reportdata via
  * \Reportdata_model without the namespace.
  */
-class_alias('munkireport\models\Reportdata_model', '\Reportdata_model');
+class_alias('App\ReportData', '\Reportdata_model');
