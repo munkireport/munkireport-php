7af70dd8711c661da4257189a4aa42d34760fff0
Mosen <mosen@users.noreply.github.com>
2022-12-08 00:35:22 +1100

    #1491: changes in the way php handles array_key_exists() with null values caused an error with yaml based dashboards

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -6,11 +6,10 @@
 
 @section('content')
 <div class="container-fluid">
-
     @foreach($dashboard_layout as $row)
 	<div class="row pt-4">
         @foreach($row as $item => $data)
-            @if(array_key_exists('widget', $data))
+            @if(is_array($data) && array_key_exists('widget', $data))
                 <?php
                 $obj = new View();
                 $widget->view($obj, $data['widget'], $data)
