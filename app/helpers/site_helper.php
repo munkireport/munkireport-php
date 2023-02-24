c6b0f49703abd2a10adf7174765583854eefb125
tuxudo <tuxudo@users.noreply.github.com>
2023-02-23 21:43:45 -0500

    Update MR version

--- app/helpers/site_helper.php
+++ app/helpers/site_helper.php
@@ -4,7 +4,7 @@ use munkireport\models\Machine_group, munkireport\lib\Modules, munkireport\lib\D
 use munkireport\lib\User;
 
 // Munkireport version (last number is number of commits)
-$GLOBALS['version'] = '5.7.2.4265';
+$GLOBALS['version'] = '5.8.0.4284';
 
 // Return version without commit count
 function get_version()
