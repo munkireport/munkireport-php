08587a07de9dbb984e2db2e27bd8b842603624d9
Mosen <mosen@users.noreply.github.com>
2023-04-25 21:59:38 +1000

    Removed laravel/ui routes. Add FortifyServiceProvider to app

--- config/app.php
+++ config/app.php
@@ -185,6 +185,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\FortifyServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\ModuleGraphQLSchemaProvider::class,
         App\Providers\ModulesServiceProvider::class,
